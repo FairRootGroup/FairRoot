f709b6a856e6909eae9f3f042e0d12ba748bd2ef
Mohammad Al-Turany <m.al-turany@gsi.de>
2009-02-03 15:57:19 +0000

    adapt the code by ilse
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@4484 0381ead4-6506-0410-b988-94b70fbc4730

--- ora/FairGenericParOraIo.h
+++ ora/FairGenericParOraIo.h
@@ -6,17 +6,10 @@
 class FairOraConn;
 class FairParSet;
 class FairParGenericSet;
-class FairParamBinObj;
+class FairParamObj;
 
 class FairGenericParOraIo : public FairDetParOraIo {
 private:
-  class FairParOraBlob : public TObject {
-    public:
-      FairParamBinObj* binaryParam;   // Pointer to object in parameter list
-      Int_t           blobId;        // Id of BinaryLargeObject
-      FairParOraBlob(FairParamBinObj* o=0,Int_t id=0) { binaryParam=o; blobId=id; }
-      ~FairParOraBlob() {}
-  };
 public:
   FairGenericParOraIo(FairOraConn* p=0);
   ~FairGenericParOraIo() {;}
@@ -26,10 +19,10 @@ public:
   Int_t writeSet(FairParGenericSet*);
   Bool_t readFromLoadingTable(FairParGenericSet*,Int_t);
 private:
-  Bool_t readBlob(FairParamBinObj* obj,Int_t lobId);
+  Bool_t readBlob(FairParamObj*,Int_t,Bool_t);
   Int_t createParamVers(FairParGenericSet*);
-  Int_t storeBlob(UChar_t*, Int_t);
-  Bool_t readLoadBlob(FairParamBinObj*,Int_t);
+  Int_t storeBlob(UChar_t*,Int_t,Bool_t);
+  Bool_t readLoadBlob(FairParamObj*,Int_t,Bool_t);
   ClassDef(FairGenericParOraIo,0) // I/O from Oracle for gemeric parameter containers
 };
 
