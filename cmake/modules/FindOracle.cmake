5311902b3698813010aabf8fb09be333467ff629
Florian Uhlig <F.Uhlig@gsi.de>
2009-02-04 13:55:18 +0000

    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@4500 0381ead4-6506-0410-b988-94b70fbc4730

--- cmake/modules/FindOracle.cmake
+++ cmake/modules/FindOracle.cmake
@@ -22,6 +22,15 @@ FIND_PROGRAM(
     NO_DEFAULT_PATH
 )
 
+MESSAGE("CMAKE_SYSTEM_INCLUDE_PATH: ${CMAKE_SYSTEM_INCLUDE_PATH}")
+MESSAGE("CMAKE_INCLUDE_PATH: ${CMAKE_INCLUDE_PATH}")
+
+
+FIND_PATH(IOMANIP_INCLUDE_DIR iomanip
+)
+
+MESSAGE("IOMANIP: ${IOMANIP_INCLUDE_DIR}")
+
 IF(ORACLE_PREPROCESSOR)
   SET(ORACLE_FOUND "YES")
 ENDIF(ORACLE_PREPROCESSOR)
@@ -41,7 +50,7 @@ ENDIF (ORACLE_FOUND)
 
 MACRO (PREPROCESS_ORACLE_FILES INFILES INCLUDE_DIRS_IN)
  
-  set(SYS_INCLUDE "'sys_include=(${ORACLE_HOME}/precomp/public,/usr/include/,/usr/local/gcc3.2.3/lib/gcc-lib/i686-pc-linux-gnu/3.2.3/include,/usr/local/gcc3.2.3/lib/gcc-lib/i686-pc-linux-gnu,/usr/include/g++-3,/usr/include/c++/3.2/backward,/usr/include/c++/3.2)'")
+  set(SYS_INCLUDE "'sys_include=(/usr/include/,/usr/lib/gcc/i486-linux-gnu/4.1.2/include/,/usr/include/c++/4.1.2/backward/,/usr/include/c++/4.1.2,/usr/include/c++/4.1.2/i486-linux-gnu/)'")
 
 #  set(INCLUDE_DIRS "
 #      include=${ORACLE_HOME}/precomp/public
@@ -57,8 +66,7 @@ MACRO (PREPROCESS_ORACLE_FILES INFILES INCLUDE_DIRS_IN)
     set(INCLUDE_DIRS ${INCLUDE_DIRS} include=${_current_FILE})   
   endforeach (_current_FILE ${INCLUDE_DIRS_IN})
 
-  SET(PROCFLAGS oraca=yes code=cpp parse=partial sqlcheck=semantics ireclen=130 oreclen=130 ${INCLUDE_DIRS})
-# ${SYS_INCLUDE} ${INCLUDE_DIRS})
+  SET(PROCFLAGS oraca=yes code=cpp parse=partial sqlcheck=semantics ireclen=130 oreclen=130 ${SYS_INCLUDE} ${INCLUDE_DIRS})
  
 
 #  MESSAGE("PROCFLAGS: ${PROCFLAGS}")
