53333f083c5bfa3bb41840706ff78dc34558b3b5
Florian Uhlig <F.Uhlig@gsi.de>
2009-02-19 15:42:27 +0000

    Remove libEGPythia6 from root libraries.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@4708 0381ead4-6506-0410-b988-94b70fbc4730

--- cmake/modules/FindROOT.cmake
+++ cmake/modules/FindROOT.cmake
@@ -117,7 +117,7 @@ IF (ROOT_FOUND)
 #  STRING(REGEX MATCHALL "-L([^ ])+"  root_library ${root_flags})
 #  REMOVE_FROM_LIST(root_flags "${root_libs_all}" "${root_library}")
 
-  SET(ROOT_LIBRARIES "${root_flags} -lEGPythia6")
+  SET(ROOT_LIBRARIES ${root_flags})
 
   # Make variables changeble to the advanced user
   MARK_AS_ADVANCED( ROOT_LIBRARY_DIR ROOT_INCLUDE_DIR ROOT_DEFINITIONS)
