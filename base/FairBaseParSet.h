c82b5730956f6c8d39782042d091febc4760d47b
Mohammad Al-Turany <m.al-turany@gsi.de>
2009-02-26 12:13:54 +0000

    add the beam mom. to the parameters
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@4783 0381ead4-6506-0410-b988-94b70fbc4730

--- base/FairBaseParSet.h
+++ base/FairBaseParSet.h
@@ -19,20 +19,22 @@ public:
   
   void SetDetList(TObjArray *array){fDetList=array;}
   void SetGeoNodes(TObjArray *array){fGeoNodes=array;}
-
   void SetGen(FairPrimaryGenerator *gen){fPriGen=gen;}
-  
+  void SetBeamMom(Double_t  BMom){fBeamMom = BMom;}
+
   TObjArray             *GetDetList(){return fDetList;}    
   TObjArray             *GetGeoNodes(){return fGeoNodes;}
   FairPrimaryGenerator   *GetPriGen(){return fPriGen; }
+  Double_t 		GetBeamMom(){return fBeamMom; }
   
 
 protected:
 
-  TObjArray             *fDetList;  // Detectors used in the simulation
-  TObjArray            *fGeoNodes; // List of FairGeoNodes for sensitive volumes
+  TObjArray              *fDetList;  // Detectors used in the simulation
+  TObjArray              *fGeoNodes; // List of FairGeoNodes for sensitive volumes
   FairPrimaryGenerator   *fPriGen;   //Generator used for simulation
-  ClassDef(FairBaseParSet,1)
+  Double_t 		 fBeamMom;  //Beam momentum
+  ClassDef(FairBaseParSet,2)
 private:  
   FairBaseParSet(const FairBaseParSet &L);
   FairBaseParSet& operator= (const FairBaseParSet&) {return *this;}
