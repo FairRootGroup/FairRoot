1495d37495491fd6605a55946628b7a589c8dc35
Florian Uhlig <F.Uhlig@gsi.de>
2011-02-23 16:54:33 +0000

    Small changes to test the new pre-commit hook.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@10943 0381ead4-6506-0410-b988-94b70fbc4730

--- base/FairVolumeList.cxx
+++ base/FairVolumeList.cxx
@@ -13,66 +13,70 @@ using std::endl;
 
 //_____________________________________________________________________________
 
-FairVolumeList::FairVolumeList() 
- :fData(new TObjArray())
+FairVolumeList::FairVolumeList()
+  :fData(new TObjArray())
 {
-    //
-   
+  //
+
 }
 
 //_____________________________________________________________________________
-FairVolumeList::~FairVolumeList() {
-    if (fData) {
-        fData->Delete();
-        delete fData;
-    }
+FairVolumeList::~FairVolumeList()
+{
+  if (fData) {
+    fData->Delete();
+    delete fData;
+  }
 }
 
 //_____________________________________________________________________________
-FairVolume * FairVolumeList::getVolume(TString * name) {
-
-    TObject * obj = findObject(* name);
-    if (obj) cout << "-I FairVolume getVolume " << name->Data() << "found" << endl;
-    return (FairVolume *) obj;
-}
-
-//_____________________________________________________________________________
-Int_t FairVolumeList::getVolumeId(TString * name) {
-
-    TObject * obj = findObject(* name);
-    if (obj) cout << "-I FairVolume getVolume " << name->Data() << "found" << endl;
-    FairVolume * vol = (FairVolume *) obj;
-    return vol->getVolumeId();
-}
-
-
-
-//_____________________________________________________________________________
-FairVolume * FairVolumeList::findObject(TString name) {
-    FairVolume * obj = NULL;
-    for (int i = 0; i < fData->GetEntriesFast(); i++) {
-            obj = (FairVolume *) fData->At(i);
-        if (obj ) 
+FairVolume* FairVolumeList::getVolume(TString* name)
 {
 
-            if (obj->getName() == name) return (FairVolume *) obj;
-        }
-
-    }
-    return NULL;
+  TObject* obj = findObject(* name);
+  if (obj) { cout << "-I FairVolume getVolume " << name->Data() << "found" << endl; }
+  return (FairVolume*) obj;
 }
 
 //_____________________________________________________________________________
-void FairVolumeList::addVolume(FairVolume * elem) {
+Int_t FairVolumeList::getVolumeId(TString* name)
+{
 
-    FairVolume *v= (FairVolume *)findObject(elem->getName());
+  TObject* obj = findObject(* name);
+  if (obj) { cout << "-I FairVolume getVolume " << name->Data() << "found" << endl; }
+  FairVolume* vol = (FairVolume*) obj;
+  return vol->getVolumeId();
+}
 
-    if (v) {
-    if(gDebug>0)   cerr << "-I FairVolumeList element: " << elem->getName() <<  " VolId : " << elem->getVolumeId() << " already defined " << v->getVolumeId()<< endl;
-    }else{ 	
+
+
+//_____________________________________________________________________________
+FairVolume* FairVolumeList::findObject(TString name)
+{
+  FairVolume* obj = NULL;
+  for (int i = 0; i < fData->GetEntriesFast(); i++) {
+    obj = (FairVolume*) fData->At(i);
+    if (obj ) {
+
+      if (obj->getName() == name) { return (FairVolume*) obj; }
+    }
+
+  }
+  return NULL;
+}
+
+//_____________________________________________________________________________
+void FairVolumeList::addVolume(FairVolume* elem)
+{
+
+  FairVolume* v= (FairVolume*)findObject(elem->getName());
+
+  if (v) {
+    if(gDebug>0) { cerr << "-I FairVolumeList element: " << elem->getName() <<  " VolId : " << elem->getVolumeId() << " already defined " << v->getVolumeId()<< endl; }
+  } else {
 
     fData->Add(elem);
-    }
+  }
 }
 
 ClassImp(FairVolumeList)
