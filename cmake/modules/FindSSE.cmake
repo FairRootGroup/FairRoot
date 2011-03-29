cfdd96f078b56764aaeaae5054304018f2d83cb9
Florian Uhlig <F.Uhlig@gsi.de>
2011-03-29 11:17:01 +0000

    New module to check if SSE is available.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@11291 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ cmake/modules/FindSSE.cmake
@@ -0,0 +1,20 @@
+# Check if SSE instructions are available on the machine where 
+# the project is compiled.
+
+If(CMAKE_SYSTEM_NAME MATCHES Linux)
+  Exec_Program(cat ARGS "/proc/cpuinfo" OUTPUT_VARIABLE CPUINFO)
+  String(REGEX REPLACE "^.*(sse).*$" "\\1" SSE_THERE ${CPUINFO})
+  String(COMPARE EQUAL "sse" "${SSE_THERE}" SSE_TRUE)
+ElseIf(CMAKE_SYSTEM_NAME MATCHES Darwin)
+  Exec_Program("/usr/sbin/sysctl -n machdep.cpu.features" OUTPUT_VARIABLE CPUINFO)
+  String(REGEX REPLACE "^.*(SSE).*$" "\\1" SSE_THERE ${CPUINFO})
+  String(COMPARE EQUAL "SSE" "${SSE_THERE}" SSE_TRUE)
+EndIf(CMAKE_SYSTEM_NAME MATCHES Linux)
+
+If(SSE_TRUE)
+  Set(SSE_FOUND true) 
+  MESSAGE(STATUS "SSE extensions available")
+Else(SSE_TRUE)
+  Set(SSE_FOUND false)
+  MESSAGE(STATUS "NO SSE extensions available.")
+EndIf(SSE_TRUE)
