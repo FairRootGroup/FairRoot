67b2e9c70937a1eb757a573f786ee09f70a0c5ac
Florian Uhlig <F.Uhlig@gsi.de>
2011-03-31 18:44:21 +0000

    Rename functions GetTime and SetTime to GetTimeStamp and SetTimeStamp to make the meaning more clear.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@11340 0381ead4-6506-0410-b988-94b70fbc4730

--- base/FairTimeStamp.h
+++ base/FairTimeStamp.h
@@ -16,9 +16,9 @@ class FairTimeStamp : public FairMultiLinkedData
     /** Destructor **/
     virtual ~FairTimeStamp();
     /** Accessors **/
-    Double_t GetTime()             const { return fTimeStamp; };
+    Double_t GetTimeStamp()             const { return fTimeStamp; };
     /** Modifiers **/
-    void SetTime(Double_t t) { fTimeStamp = t; }
+    void SetTimeStamp(Double_t t) { fTimeStamp = t; }
   protected:
     Double_t fTimeStamp;        /** Time of digit or Hit  [ns] */
     ClassDef(FairTimeStamp,1);
