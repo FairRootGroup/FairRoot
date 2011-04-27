da052b80af8c25b8120fc540e82f905707f680b5
Mohammad Al-Turany <m.al-turany@gsi.de>
2011-04-27 17:51:40 +0000

    correct wronge checked file from last commit
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@11735 0381ead4-6506-0410-b988-94b70fbc4730

--- base/FairTimeStamp.h
+++ base/FairTimeStamp.h
@@ -1,27 +1,46 @@
-#include "FairTimeStamp.h"
+#ifndef FAIRTIMESTAMP_H
+#define FAIRTIMESTAMP_H
+#include "FairMultiLinkedData.h"
+/**
+ * Base class for Time stamp information
+ ** Aug. 2010
+ **@author M.Al-Turany <m.al-turany@gsi.de>
+ */
+class FairTimeStamp : public FairMultiLinkedData
+{
+  public:
+    /** Default constructor **/
+    FairTimeStamp();
+    /** Constructor with time **/
+    FairTimeStamp(Double_t time);
+    /** Constructor with time and time error **/
+    FairTimeStamp(Double_t time, Double_t timeerror);
 
-// -----   Default constructor   -------------------------------------------
-FairTimeStamp::FairTimeStamp()
-  : FairMultiLinkedData(),
-    fTimeStamp(0), fTimeStampError(-1)
-{
-}
-// -----   Standard constructor   ------------------------------------------
-FairTimeStamp::FairTimeStamp(Double_t time)
-  :FairMultiLinkedData(),
-   fTimeStamp(time), fTimeStampError(-1)
-{
-}
+    /** Destructor **/
+    virtual ~FairTimeStamp();
+    /** Accessors **/
+    Double_t GetTimeStamp()             const { return fTimeStamp; };
+    Double_t GetTimeStampError()     const { return fTimeStampError;};
+    /** Modifiers **/
+    void SetTimeStamp(Double_t t) { fTimeStamp = t; }
+    void SetTimeStampError(Double_t t) {fTimeStampError = t;}
 
-FairTimeStamp::FairTimeStamp(Double_t time, Double_t timeerror)
-  :FairMultiLinkedData(),
-   fTimeStamp(time), fTimeStampError(timeerror)
-{
-}
-// -----   Destructor   ----------------------------------------------------
-FairTimeStamp::~FairTimeStamp()
-{
-}
+    Int_t Compare(const TObject* obj) const {
+      if (this == obj) { return 0; }
+      FairTimeStamp* tsobj = (FairTimeStamp*)obj;
+      Double_t ts = tsobj->GetTimeStamp();
+      if (fTimeStamp < ts) { return -1; }
+      else if (fTimeStamp == ts) { return 0; }
+      else { return 1; }
+    }
 
-// -------------------------------------------------------------------------
-ClassImp(FairTimeStamp)
+    Bool_t IsSortable() const { return kTRUE;};
+
+  protected:
+    Double_t fTimeStamp;        /** Time of digit or Hit  [ns] */
+    Double_t fTimeStampError;     /** Error on time stamp */
+
+    ClassDef(FairTimeStamp,1);
+};
+
+#endif
