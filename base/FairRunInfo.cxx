2055beddbb428d417d57e5c3ad5126a336d12724
Florian Uhlig <F.Uhlig@gsi.de>
2011-08-17 12:48:33 +0000

    Use more precise time information.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@13213 0381ead4-6506-0410-b988-94b70fbc4730

--- base/FairRunInfo.cxx
+++ base/FairRunInfo.cxx
@@ -31,20 +31,21 @@ FairRunInfo::~FairRunInfo()
 void FairRunInfo::StoreInfo()
 {
   // Extract the Information about the used memory from the system.
-  // Stores this infor together with the timedifference between now
+  // Stores this info together with the timedifference between now
   // and the last call in container arrays for later usage.
   //  gSystem->GetCpuInfo(&cpuInfo, 10);
   //  gSystem->GetMemInfo(&memInfo);
 
-  // Set the TimeStamp to the actual time
-  fTimeStamp.Set();
-  fTime.push_back(fTimeStamp.GetSec());
 
-  gSystem->GetProcInfo(&fProcInfo);
-  fResidentMemory.push_back(fProcInfo.fMemResident/1024);
-  fVirtualMemory.push_back(fProcInfo.fMemVirtual/1024);
+  fLogger->Info(MESSAGE_ORIGIN,"Before GetInfo.");
+
+  GetInfo();
+
+  fLogger->Info(MESSAGE_ORIGIN,"Before CalculateTimeDifference.");
 
   CalculateTimeDifference();
+
+  fLogger->Info(MESSAGE_ORIGIN,"Before PrintInfo.");
 
   PrintInfo();
 }
@@ -72,69 +73,51 @@ void FairRunInfo::CalculateTimeDifference()
 
 void FairRunInfo::WriteInfo()
 {
-  TList* histoList = NULL;
+  TList* histoList = new TList();
   CreateAndFillHistograms(histoList);
-  //  WriteHistosToFile(histoList);
+  //  CreateAndFillHistograms(histoList);
+  WriteHistosToFile(histoList);
 }
 
 void FairRunInfo::CreateAndFillHistograms(TList* histoList)
 {
-  fLogger->Info(MESSAGE_ORIGIN,"Size of fTime: %i",
-                fTime.size());
-  fLogger->Info(MESSAGE_ORIGIN,"Size of fTimeDiff: %i",
-                fTimeDiff.size());
-  fLogger->Info(MESSAGE_ORIGIN,"Size of fResidentMemory: %i",
-                fResidentMemory.size());
-  fLogger->Info(MESSAGE_ORIGIN,"Size of fVirtualMemory: %i",
-                fVirtualMemory.size());
-
   Int_t entries = fTime.size();
   Int_t timePeriod = fTime.back()-fTime.front();
 
-  TH1F* memResVsEvent = new TH1F("memResVsEvent","Resident Memory as function of Eventnumber;Event;Memory [MB]",entries, 0, entries);
-  TH1F* memVirtVsEvent = new TH1F("memVirtVsEvent","Virtual Memory as function of Eventnumber;Event;Memory [MB]",entries, 0, entries);
-  TH1F* memResVsTime = new TH1F("memResVsTime","Resident memory as function of Runtime;Time [s];Memory [MB]",timePeriod, 0, timePeriod);
-  TH1F* memVirtVsTime = new TH1F("memVirtVsTime","Virtual memory as function of Runtime;Time [s];Memory [MB]",timePeriod, 0, timePeriod);
-  TH1F* eventtimeVsEvent = new TH1F("eventtimeVsEvent","Runtime per Event as function of Event number;Event;Time [s]",entries-1, 1, entries);
+  TH1F* ResidentMemoryVsEvent = new TH1F("ResidentMemoryVsEvent","Resident Memory as function of Eventnumber;Event;Memory [MB]",entries, 0, entries);
+  TH1F* VirtualMemoryVsEvent = new TH1F("VirtualMemoryVsEvent","Virtual Memory as function of Eventnumber;Event;Memory [MB]",entries, 0, entries);
+  TH1F* ResidentMemoryVsTime = new TH1F("ResidentMemoryVsTime","Resident memory as function of Runtime;Time [s];Memory [MB]",timePeriod, 0, timePeriod);
+  TH1F* VirtualMemoryVsTime = new TH1F("VirtualMemoryVsTime","Virtual memory as function of Runtime;Time [s];Memory [MB]",timePeriod, 0, timePeriod);
+  TH1F* EventtimeVsEvent = new TH1F("EventtimeVsEvent","Runtime per Event as function of Event number;Event;Time [s]",(entries-1)*10, 1, entries);
 
-  std::vector<Int_t> timeDiffSorted(fTimeDiff);
-  std::vector<Int_t>::iterator it;
+  std::vector<Double_t> timeDiffSorted(fTimeDiff);
+  std::vector<Double_t>::iterator it;
   std::sort(timeDiffSorted.begin(), timeDiffSorted.end());
 
   Int_t minTime = timeDiffSorted.front();
   Int_t maxTime = timeDiffSorted.back();
   timePeriod = maxTime - minTime;
 
-  TH1F* timePerEvent = new TH1F("timePerEvent","Runtime;Time [s];Events",timePeriod+20, minTime-10, maxTime+10);
+  TH1F* TimePerEvent = new TH1F("TimePerEvent","Runtime;Time [s];Events",(timePeriod+20)*10, minTime-10, maxTime+10);
 
   Int_t counter = 0;
   std::vector<Long_t>::iterator lit;
   for(lit=fResidentMemory.begin(); lit<fResidentMemory.end(); lit++) {
-    memResVsEvent->Fill(counter, *lit);
+    ResidentMemoryVsEvent->Fill(counter, *lit);
     counter++;
   }
-  for(it=fTimeDiff.begin(); it<fTimeDiff.end(); it++) {
-    timePerEvent->Fill(*it);
-  }
+
   counter = 1;
   for(it=fTimeDiff.begin(); it<fTimeDiff.end(); it++) {
-    eventtimeVsEvent->Fill(counter, *it);
+    TimePerEvent->Fill(*it);
+    EventtimeVsEvent->Fill(counter, *it);
     counter++;
   }
-  //  histoList->AddLast(memVsEvent);
-  // create a new subdirectory in the output file and
-  // write all histograms
-  TDirectory* currentDir = gDirectory;
 
-  gDirectory->cd();
-  gDirectory->mkdir("FairRunInfo");
-  gDirectory->cd("FairRunInfo");
 
-  memResVsEvent->Write();
-  timePerEvent->Write();
-  eventtimeVsEvent->Write();
-
-  gDirectory=currentDir;
+  histoList->AddLast(ResidentMemoryVsEvent);
+  histoList->AddLast(TimePerEvent);
+  histoList->AddLast(EventtimeVsEvent);
 }
 
 void FairRunInfo::WriteHistosToFile(TList* histoList)
@@ -159,9 +142,15 @@ void FairRunInfo::WriteHistosToFile(TList* histoList)
 
 void FairRunInfo::Reset()
 {
+  GetInfo();
+}
+
+void FairRunInfo::GetInfo()
+{
   // Set the TimeStamp to the actual time and store it
   fTimeStamp.Set();
-  fTime.push_back(fTimeStamp.GetSec());
+  //  fTime.push_back(fTimeStamp.GetSec());
+  fTime.push_back(fTimeStamp.AsDouble());
 
   gSystem->GetProcInfo(&fProcInfo);
   fResidentMemory.push_back(fProcInfo.fMemResident/1024);
