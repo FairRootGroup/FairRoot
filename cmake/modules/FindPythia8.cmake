88550587e84ec1a275717ab82ef2d6789c615bba
Mohammad Al-Turany <m.al-turany@gsi.de>
2011-10-14 09:32:57 +0000

    new cmake modules  for glpk and Pythia8
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@13538 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ cmake/modules/FindPythia8.cmake
@@ -0,0 +1,36 @@
+# - Try to find PYTHIA8 instalation
+# Once done this will define
+#
+
+
+
+MESSAGE(STATUS "Looking for PYTHIA8 ...")
+
+FIND_PATH(PYTHIA8_INCLUDE_DIR NAMES Pythia.h PATHS
+  ${SIMPATH}/generators/pythia8/include  
+  ${SIMPATH}//include/pythia8  
+  NO_DEFAULT_PATH
+)
+
+FIND_PATH(PYTHIA8_LIB_DIR  NAMES libpythia8.so PATHS
+  ${SIMPATH}/generators/lib
+  ${SIMPATH}/lib
+  NO_DEFAULT_PATH
+)
+
+if (PYTHIA8_INCLUDE_DIR AND PYTHIA8_LIB_DIR)
+   set(PYTHIA8_FOUND TRUE)
+endif (PYTHIA8_INCLUDE_DIR AND PYTHIA8_LIB_DIR)
+
+if (PYTHIA8_FOUND)
+  if (NOT PYTHIA8_FOUND_QUIETLY)
+    MESSAGE(STATUS "Looking for PYTHIA8... - found ${PYTHIA8_LIB_DIR}")
+#    message(STATUS "Found PLUTO: ${PLUTO_LIBRARY_DIR}")
+    SET(LD_LIBRARY_PATH ${LD_LIBRARY_PATH} ${PYTHIA8_LIB_DIR})
+  endif (NOT PYTHIA8_FOUND_QUIETLY)
+else (PYTHIA8_FOUND)
+  if (PYTHIA8_FOUND_REQUIRED)
+    message(FATAL_ERROR "Looking for PYTHIA8... - Not found")
+  endif (PYTHIA8_FOUND_REQUIRED)
+endif (PYTHIA8_FOUND)
+
