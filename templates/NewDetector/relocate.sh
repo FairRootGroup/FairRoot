011d71a1ea91c8e610584a3e05b8702d83165634
Florian Uhlig <F.Uhlig@gsi.de>
2011-11-07 21:52:45 +0000

    New version.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@13639 0381ead4-6506-0410-b988-94b70fbc4730

--- templates/NewDetector/relocate.sh
+++ templates/NewDetector/relocate.sh
@@ -7,8 +7,13 @@
 
 #set -xv
 
-if [ $# -ne 1 ]; then
-  echo "Please call the script with the name of the detector."
+if [ $# -ne 3 ]; then
+  echo "Please call the script with three parameters. The first one is the"
+  echo "name of the detector. The second is the name of the project. This"
+  echo "name can be found in the main CMakeLists.txt as argument for"
+  echo "Project(<ProjectName>)). The third parameter is the prefix in front"
+  echo "the class names. For CBM this is for example Cbm, for Panda Pnd."
+  echo "If you're not sure check in already existing detectors."
   echo "The script will exchange all default names by the new name"
   exit 1
 fi  
@@ -16,13 +21,45 @@ fi
 DetectorName=$1 
 DetectorNameUpper=$(echo $DetectorName | tr [:lower:] [:upper:])
 
+ProjectName=$(echo $2 | tr [:lower:] [:upper:])
+ProjectSourceDir=${ProjectName}_SOURCE_DIR
+RelativeDir=$(basename $PWD)
+Prefix=$3
 for i in $(ls NewDetector*); do 
   oldfile=$i
   newfile=$(echo $oldfile | sed "s/NewDetector/$DetectorName/")
   mv $oldfile $newfile
 done 
 
-find . -name "*" -exec sed -e "s/NewDetector/$DetectorName/g" -i "{}" ";"
-find . -name "*" -exec sed -e "s/NEWDETECTOR/$DetectorNameUpper/g" -i "{}" ";"
+find . -name "*.h" -exec sed -e "s/NewDetector/$DetectorName/g" -i "{}" ";"
+find . -name "*.h" -exec sed -e "s/NEWDETECTOR/$DetectorNameUpper/g" -i "{}" ";"
+find . -name "*.cxx" -exec sed -e "s/NewDetector/$DetectorName/g" -i "{}" ";"
+find . -name "*.cxx" -exec sed -e "s/NEWDETECTOR/$DetectorNameUpper/g" -i "{}" ";"
+find . -name "*.cxx" -exec sed -e "s/FairDetectorList/${Prefix}DetectorList/g" -i "{}" ";"
+find . -name "*.cxx" -exec sed -e "s/FairStack/${Prefix}Stack/g" -i "{}" ";"
+find . -name "*.h" -exec sed -e "s/FairDetectorList/${Prefix}DetectorList/g" -i "{}" ";"
+find . -name "*.h" -exec sed -e "s/FairStack/${Prefix}Stack/g" -i "{}" ";"
 
-#set +xv
+sed -e "s#tutorial/NewDetector#$RelativeDir#g" -i CMakeLists.txt
+sed -e "s/NewDetector/$DetectorName/g" -i CMakeLists.txt
+sed -e "s/NEWDETECTOR/$DetectorNameUpper/g" -i CMakeLists.txt
+sed -e "s/FAIRROOT_SOURCE_DIR/$ProjectSourceDir/g" -i CMakeLists.txt
+
+if [ -d .svn ]; then
+  echo "Please remove the .svn directory"
+  echo "##"
+fi
+
+echo "Please add the directories which contain the Stack and"
+echo "DetectorList classes to the include directories in CMakeLists.txt"
+echo "##"
+
+echo "Please add the new detector to the detector list. This iist can be"
+echo "found in the DetectorList class. The name to be added is"
+echo "k${DetectorName}."
+
+echo "##"
+echo "edit ${DetectorName}}Geo::getModNumInMod according to the comment in"
+eche "the file."
+
+#set +xvx
