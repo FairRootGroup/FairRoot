3b732056b5f78ad42f739f75d1119b6edd9ee8a6
Florian Uhlig <F.Uhlig@gsi.de>
2011-11-17 13:31:26 +0000

    Use new cdash webserver for FairRoot tests.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@13712 0381ead4-6506-0410-b988-94b70fbc4730

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,7 +4,7 @@ set(CTEST_PROJECT_NAME "FairRoot")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "fairroot.gsi.de")
+set(CTEST_DROP_SITE "cdash.gsi.de")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=FairRoot")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
