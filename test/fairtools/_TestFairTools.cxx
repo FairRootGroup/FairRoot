88a949180fc6e01a16c1457f3cacfbbb42fa1f4a
Florian Uhlig <F.Uhlig@gsi.de>
2011-11-18 12:30:39 +0000

    First try to implement a parametrized test to test all log levels with only one test.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@13720 0381ead4-6506-0410-b988-94b70fbc4730

--- test/fairtools/_TestFairTools.cxx
+++ test/fairtools/_TestFairTools.cxx
@@ -9,31 +9,83 @@
 #include <fstream>
 #include <algorithm>
 
-TEST(FairToolsTest, callInstanceShouldWork)
+class LogLevelTest : public testing::TestWithParam<const char*>
 {
-  FairLogger* fLogger = FairLogger::GetLogger();
+  protected:
+    virtual void SetUp() {
+      logLevelSettingToTest=GetParam();
+    }
+    std::string logLevelSettingToTest;
+};
+
+
+static const char* const LogString[] = { "FATAL  ", "ERROR  ", "WARNING",
+                                       "INFO   ", "DEBUG  ", "DEBUG1 ",
+                                       "DEBUG2 ", "DEBUG3 ", "DEBUG4 "
+                                       };
+
+/*
+static const char* const LogString[] = { "FATAL", "ERROR", "WARNING",
+                                       "INFO", "DEBUG", "DEBUG1",
+                                       "DEBUG2", "DEBUG3", "DEBUG4"
+                                       };
+*/
+
+void LogNoArguments(FairLogger* fLogger, std::string OutputString)
+{
+  // Same message for all log levels
+  fLogger->Error(MESSAGE_ORIGIN, OutputString.c_str());
+  fLogger->Warning(MESSAGE_ORIGIN, OutputString.c_str());
+  fLogger->Info(MESSAGE_ORIGIN, OutputString.c_str());
+  fLogger->Debug(MESSAGE_ORIGIN, OutputString.c_str());
+  fLogger->Debug1(MESSAGE_ORIGIN, OutputString.c_str());
+  fLogger->Debug2(MESSAGE_ORIGIN, OutputString.c_str());
+  fLogger->Debug3(MESSAGE_ORIGIN, OutputString.c_str());
+  fLogger->Debug4(MESSAGE_ORIGIN, OutputString.c_str());
 }
 
-TEST(FairToolsTest, testScreenOutputwithoutArgument)
+std::vector<std::string> CreateExpectedOutputNoArguments(std::string loglevel,
+    std::string outString,
+    std::string filename="")
 {
-  FairLogger* fLogger = FairLogger::GetLogger();
 
-  FairCaptureOutput handler(true, false);
-  handler.BeginCapture();
+  //Put blanks to the string to have all strings the same length
+  int size = loglevel.size();
+  int maxsize = 7; // length of string "WARNING"
+  for (int i=size; i<maxsize; i++) {
+    loglevel += " ";
+  }
 
-  fLogger->Error(MESSAGE_ORIGIN,"I am here.");
-  fLogger->Info(MESSAGE_ORIGIN,"I am here.");
-  fLogger->Debug(MESSAGE_ORIGIN,"I am here.");
-  fLogger->Debug1(MESSAGE_ORIGIN,"I am here.");
-  fLogger->Debug2(MESSAGE_ORIGIN,"I am here.");
-  fLogger->Debug3(MESSAGE_ORIGIN,"I am here.");
-  fLogger->Debug4(MESSAGE_ORIGIN,"I am here.");
+  std::vector<std::string> levelNames(LogString, LogString+9);
+  std::vector<std::string>::iterator position =
+    std::find(levelNames.begin(), levelNames.end(), loglevel);
+  levelNames.erase(position+1, levelNames.end());
+  levelNames.erase(levelNames.begin()); // don't use FATAL loglevel in this test
 
-  handler.EndCapture();
+  std::vector<std::string> retVal;
+  std::string fullOutputString;
+  if ( filename.compare("") != 0) {
+    fullOutputString = "[INFO   ] Openning log file: " + filename;
+    retVal.push_back(fullOutputString);
+    retVal.push_back(" ");
+  }
 
+  std::vector<std::string>::iterator iter;
+  for ( iter = levelNames.begin();
+        iter < levelNames.end();
+        iter++) {
+    fullOutputString = "[" + *iter + "] " + outString + " ";
+    retVal.push_back(fullOutputString);
+  }
+
+  return retVal;
+}
+
+
+
+void CheckScreenOutput(FairCaptureOutput handler, std::vector<std::string> v)
+{
   int noLines = handler.GetNumberOfLines();
-  const char* expectedValue[] = {"[ERROR  ] I am here. ", "[INFO   ] I am here. "};
-  std::vector<std::string> v(expectedValue, expectedValue + 2);
   int expectedNumberOfLines = v.size();
   EXPECT_EQ(expectedNumberOfLines,noLines);
 
@@ -43,3 +95,111 @@ TEST(FairToolsTest, testScreenOutputwithoutArgument)
     EXPECT_EQ( v[i], handler.GetCaptureLine(i) );
   }
 }
+
+void CheckFileOutput(FairTestOutputHandler handler, std::vector<std::string> v)
+{
+  int noLines = handler.GetNumberOfLines();
+  int expectedNumberOfLines = v.size();
+  EXPECT_EQ(expectedNumberOfLines,noLines);
+
+  int minLines = std::min(expectedNumberOfLines,noLines);
+
+  for (int i = 0; i < minLines; ++i) {
+    EXPECT_EQ( v[i], handler.GetCaptureLine(i) );
+  }
+}
+
+TEST(FairToolsTest, callInstanceShouldWork)
+{
+  FairLogger* fLogger = FairLogger::GetLogger();
+}
+
+TEST(FairToolsTest, CheckDefaultSettings)
+{
+  // The default is to log everything from Info level upwards
+  // to screen, no logging to file
+  std::string OutputString = "I am here.";
+
+  FairLogger* fLogger = FairLogger::GetLogger();
+  FairCaptureOutput handler(true, false);
+  handler.BeginCapture();
+  LogNoArguments(fLogger, OutputString);
+  handler.EndCapture();
+
+  std::vector<std::string> v = CreateExpectedOutputNoArguments("INFO", OutputString);
+  CheckScreenOutput(handler, v);
+}
+
+
+TEST(FairToolsTest, testScreenAndFileOutputWithoutArgument)
+{
+
+  std::string OutputString = "I am here.";
+
+  FairLogger* fLogger = FairLogger::GetLogger();
+
+  char fileName[25];
+  tmpnam(fileName);
+  fLogger->SetLogFileName(fileName);
+  fLogger->SetLogToFile(true);
+
+  FairCaptureOutput handler(true, false);
+  handler.BeginCapture();
+  LogNoArguments(fLogger, OutputString);
+  handler.EndCapture();
+
+  //  std::string stringFileName(fileName);
+  std::vector<std::string> v = CreateExpectedOutputNoArguments("INFO", OutputString, fileName);
+  CheckScreenOutput(handler, v);
+
+  v = CreateExpectedOutputNoArguments("INFO", OutputString);
+  FairTestOutputHandler outputhandler(fileName);
+  CheckFileOutput(outputhandler, v);
+}
+
+TEST_P(LogLevelTest, testAllLogLevelsToScreenAndFile)
+{
+  std::string OutputString = "I am here.";
+
+  FairLogger* fLogger = FairLogger::GetLogger();
+
+  std::string bla = logLevelSettingToTest;
+  std::cout<<"Log level: "<<bla<<":"<<std::endl;
+
+  std::string::iterator iter;
+  std::string bla1;
+  for ( iter = bla.begin();
+        iter < bla.end();
+        iter++) {
+    bla1 = *iter;
+    if ( bla1.compare(" ") == 0) {
+      bla.erase(iter);
+    }
+  }
+  std::cout<<"Log level: "<<bla<<":"<<std::endl;
+
+  fLogger->SetLogFileLevel(bla.c_str());
+  fLogger->SetLogScreenLevel(bla.c_str());
+
+  char fileName[25];
+  tmpnam(fileName);
+  fLogger->SetLogFileName(fileName);
+  fLogger->SetLogToFile(true);
+
+  FairCaptureOutput handler(true, false);
+  handler.BeginCapture();
+  LogNoArguments(fLogger, OutputString);
+  handler.EndCapture();
+
+  //  std::string stringFileName(fileName);
+  //  std::vector<std::string> v = CreateExpectedOutputNoArguments(logLevelSettingToTest, OutputString, fileName);
+  //  CheckScreenOutput(handler, v);
+
+  std::vector<std::string> v = CreateExpectedOutputNoArguments(logLevelSettingToTest, OutputString);
+  FairTestOutputHandler outputhandler(fileName);
+  CheckFileOutput(outputhandler, v);
+}
+
+INSTANTIATE_TEST_CASE_P(TestAllLogLevels,
+                        LogLevelTest,
+                        ::testing::ValuesIn(LogString));
