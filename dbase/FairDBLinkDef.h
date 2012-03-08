d21a48c6b91f28f6cb9803c257c65c66b9254406
M.Babai <M.Babai@rug.nl>
2012-03-08 17:05:38 +0000

    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@14874 0381ead4-6506-0410-b988-94b70fbc4730

--- dbase/FairDBLinkDef.h
+++ dbase/FairDBLinkDef.h
@@ -101,6 +101,9 @@
 #pragma link C++ class FairRegistryItemXxx<const char*>-;
 #pragma link C++ function operator<<(ostream&, const FairRegistry&);
 
-
+// dbIO
+#pragma link C++ class FairParTSQLIo+;
+#pragma link C++ class FairDetParTSQLIo+;
+#pragma link C++ class FairGenericParTSQLIo+;
 #endif
 
