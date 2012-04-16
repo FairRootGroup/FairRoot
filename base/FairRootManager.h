ccce006e39643ff0fa38dd044420039ff01c4424
Mohammad Al-Turany <m.al-turany@gsi.de>
2012-04-16 10:36:25 +0000

    correct typo in last commit
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@15283 0381ead4-6506-0410-b988-94b70fbc4730

--- base/FairRootManager.h
+++ base/FairRootManager.h
@@ -192,7 +192,7 @@ class FairRootManager : public TObject
     */
     void                TruncateBranchNames(TTree* fTree, const char* folderName);
 
-    void                Write(const char* name=0, Int_t option=0, Int_t bufsize=0);
+    void                Write();
     /** Write the current TGeoManager to file*/
     void                WriteGeometry();
     /**Write the file header object to the output file*/
