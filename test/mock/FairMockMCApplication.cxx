2582ce67710d290d7150cb789030e742f72d4421
Florian Uhlig <F.Uhlig@gsi.de>
2012-05-02 09:22:47 +0000

    Restructure test directory. Create new subdirectories and move files there.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@15415 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ test/mock/FairMockMCApplication.cxx
@@ -0,0 +1,11 @@
+# include "FairMockMCApplication.h"
+
+FairMockMCApplication::FairMockMCApplication(const char* name, const char* title)
+  :TVirtualMCApplication(name,title)
+{
+}
+
+FairMockMCApplication::FairMockMCApplication()
+  :TVirtualMCApplication()
+{
+}
