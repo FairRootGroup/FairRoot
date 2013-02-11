2ee3045ed904394c5f8adb882dde26b7e60f7454
Florian Uhlig <F.Uhlig@gsi.de>
2013-02-11 12:57:37 +0000

    Initial version of a tutorial to develop alignment procedures for FairRoot.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/release@18511 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ example/Tutorial4/macros/run_tutorial4.C
@@ -0,0 +1,140 @@
+void run_tutorial4(Int_t nEvents = 10)
+{
+  
+  TString dir = getenv("VMCWORKDIR");
+  TString tutdir = dir + "/example/Tutorial4";
+
+  TString tut_geomdir = dir + "/example/geometry";
+  gSystem->Setenv("GEOMPATH",tut_geomdir.Data());
+
+  TString tut_configdir = dir + "/example/Tutorial4/macros/gconfig";
+  gSystem->Setenv("CONFIG_DIR",tut_configdir.Data());
+
+  Double_t momentum = 2.;
+
+  Double_t theta    = 0.;
+
+  TString outDir = "./";
+
+  // Output file name
+  TString  outFile     ="data/testrun.root";
+  TString  parFile     ="data/testparams.root";
+  
+
+  // In general, the following parts need not be touched
+  // ========================================================================
+
+  // ----    Debug option   -------------------------------------------------
+  gDebug = 0;
+  // ------------------------------------------------------------------------
+
+  // -----   Timer   --------------------------------------------------------
+  TStopwatch timer;
+  timer.Start();
+  // ------------------------------------------------------------------------
+
+  // ----  Load libraries   -------------------------------------------------
+  gROOT->LoadMacro("$VMCWORKDIR/example/gconfig/basiclibs.C");
+  basiclibs();
+  gSystem->Load("libFairTools");
+  gSystem->Load("libFairDB");
+  gSystem->Load("libGeoBase");
+  gSystem->Load("libParBase");
+  gSystem->Load("libBase");
+  gSystem->Load("libMCStack");
+  gSystem->Load("libGen");
+  gSystem->Load("libPassive");
+  gSystem->Load("libTutorial4");
+  // ------------------------------------------------------------------------
+
+  gLogger->SetLogScreenLevel("INFO");   
+ 
+  // -----   Create simulation run   ----------------------------------------
+  FairRunSim* run = new FairRunSim();
+  run->SetName("TGeant3");              // Transport engine
+  run->SetOutputFile(outFile);          // Output file
+  FairRuntimeDb* rtdb = run->GetRuntimeDb();
+  // ------------------------------------------------------------------------
+  
+  // -----   Create media   -------------------------------------------------
+  run->SetMaterials("media.geo");       // Materials
+  // ------------------------------------------------------------------------
+  
+  // -----   Create geometry   ----------------------------------------------
+  FairModule* cave= new FairCave("CAVE");
+  cave->SetGeometryFileName("cave_vacuum.geo"); 
+  run->AddModule(cave);
+
+  FairDetector* tutdet = new FairTutorialDet("TUTDET", kTRUE);
+  tutdet->SetGeometryFileName("tutorial4.root"); 
+  run->AddModule(tutdet);
+  // ------------------------------------------------------------------------
+
+  // -----   Create PrimaryGenerator   --------------------------------------
+  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+  FairBoxGenerator* boxGen = new FairBoxGenerator(2212, 1);
+
+  boxGen->SetThetaRange (   theta,   theta+0.1);
+  boxGen->SetPRange     (momentum,momentum+0.01);
+  boxGen->SetPhiRange   (0.,360.);
+  boxGen->SetBoxXYZ (-20.,-20.,20.,20., 0.);
+
+//  boxGen->SetDebug(kTRUE);
+
+  primGen->AddGenerator(boxGen);
+
+  
+  run->SetGenerator(primGen);
+  // ------------------------------------------------------------------------
+
+  // -----   Initialize simulation run   ------------------------------------
+  run->SetStoreTraj(kTRUE);
+
+  run->Init();
+
+
+ // -Trajectories Visualization (TGeoManager Only )
+ // -----------------------------------------------
+
+ // Set cuts for storing the trajectpries
+  /* FairTrajFilter* trajFilter = FairTrajFilter::Instance();
+     trajFilter->SetStepSizeCut(0.01); // 1 cm
+     trajFilter->SetVertexCut(-2000., -2000., 4., 2000., 2000., 100.);
+     trajFilter->SetMomentumCutP(10e-3); // p_lab > 10 MeV
+     trajFilter->SetEnergyCut(0., 1.02); // 0 < Etot < 1.04 GeV
+     trajFilter->SetStorePrimaries(kTRUE);
+     trajFilter->SetStoreSecondaries(kTRUE);
+   */
+
+  // ------------------------------------------------------------------------
+
+  // -----   Runtime database   ---------------------------------------------
+
+  Bool_t kParameterMerged = kTRUE;
+  FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+//  FairParAsciiFileIo* parOut = new FairParAsciiFileIo();
+  parOut->open(parFile.Data());
+  rtdb->setOutput(parOut);
+  rtdb->saveOutput();
+  rtdb->print();
+  // ------------------------------------------------------------------------
+   
+  // -----   Start run   ----------------------------------------------------
+  run->Run(nEvents);
+  run->CreateGeometryFile("geofile_full.root");
+  // ------------------------------------------------------------------------
+  
+  // -----   Finish   -------------------------------------------------------
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  cout << endl << endl;
+  cout << "Macro finished succesfully." << endl;
+  cout << "Output file is "    << outFile << endl;
+  cout << "Parameter file is " << parFile << endl;
+  cout << "Real time " << rtime << " s, CPU time " << ctime 
+       << "s" << endl << endl;
+  // ------------------------------------------------------------------------
+}
+
+
