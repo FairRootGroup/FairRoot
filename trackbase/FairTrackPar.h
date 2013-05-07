4972a68a91d71383a9a60eba6e298beb6b373137
Florian Uhlig <F.Uhlig@gsi.de>
2013-05-07 09:55:22 +0000

    Correct problem with undefined function. Copy constructor and assignment operator are now private.
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@19636 0381ead4-6506-0410-b988-94b70fbc4730

--- trackbase/FairTrackPar.h
+++ trackbase/FairTrackPar.h
@@ -26,48 +26,108 @@ class FairTrackPar : public TObject
     /** Output to screen **/
     virtual void  Print(Option_t* option = "") const;
     /** Accessors **/
-    virtual TVector3 GetPosition() const {return TVector3(fX, fY, fZ); };
+    virtual TVector3 GetPosition() const {
+      return TVector3(fX, fY, fZ);
+    };
 
-    virtual Double_t GetX()   { return fX; };
-    virtual Double_t GetY()   { return fY; };
-    virtual Double_t GetZ()   { return fZ; };
-    Int_t   GetQ()  const { return fq; };
+    virtual Double_t GetX()   {
+      return fX;
+    };
+    virtual Double_t GetY()   {
+      return fY;
+    };
+    virtual Double_t GetZ()   {
+      return fZ;
+    };
+    Int_t   GetQ()  const {
+      return fq;
+    };
 
-    virtual Double_t GetDX()  const { return fDX; };
-    virtual Double_t GetDY()  const { return fDY; };
-    virtual Double_t GetDZ()  const { return fDZ; };
+    virtual Double_t GetDX()  const {
+      return fDX;
+    };
+    virtual Double_t GetDY()  const {
+      return fDY;
+    };
+    virtual Double_t GetDZ()  const {
+      return fDZ;
+    };
 
-    virtual TVector3 GetMomentum() const {return TVector3(fPx, fPy, fPz); };
+    virtual TVector3 GetMomentum() const {
+      return TVector3(fPx, fPy, fPz);
+    };
 
-    virtual Double_t GetPx() const { return fPx; };
-    virtual Double_t GetPy() const { return fPy; };
-    virtual Double_t GetPz() const { return fPz; };
-    virtual Double_t GetQp() const { return fQp; };
+    virtual Double_t GetPx() const {
+      return fPx;
+    };
+    virtual Double_t GetPy() const {
+      return fPy;
+    };
+    virtual Double_t GetPz() const {
+      return fPz;
+    };
+    virtual Double_t GetQp() const {
+      return fQp;
+    };
 
-    virtual Double_t GetDPx() const { return fDPx; };
-    virtual Double_t GetDPy() const { return fDPy; };
-    virtual Double_t GetDPz() const { return fDPz; };
-    virtual Double_t GetDQp() const { return fDQp; };
+    virtual Double_t GetDPx() const {
+      return fDPx;
+    };
+    virtual Double_t GetDPy() const {
+      return fDPy;
+    };
+    virtual Double_t GetDPz() const {
+      return fDPz;
+    };
+    virtual Double_t GetDQp() const {
+      return fDQp;
+    };
 
 
 
-    virtual void SetX(Double_t x)   { fX  = x;  };
-    virtual void SetY(Double_t y)   { fY  = y;  };
-    virtual void SetZ(Double_t z)   { fZ  = z;  };
+    virtual void SetX(Double_t x)   {
+      fX  = x;
+    };
+    virtual void SetY(Double_t y)   {
+      fY  = y;
+    };
+    virtual void SetZ(Double_t z)   {
+      fZ  = z;
+    };
 
-    virtual void SetDX(Double_t dx)   { fDX  = dx;  };
-    virtual void SetDY(Double_t dy)   { fDY  = dy;  };
-    virtual void SetDZ(Double_t dz)   { fDZ  = dz;  };
+    virtual void SetDX(Double_t dx)   {
+      fDX  = dx;
+    };
+    virtual void SetDY(Double_t dy)   {
+      fDY  = dy;
+    };
+    virtual void SetDZ(Double_t dz)   {
+      fDZ  = dz;
+    };
 
-    virtual void SetPx(Double_t px) { fPx = px; };
-    virtual void SetPy(Double_t py) { fPy = py; };
-    virtual void SetPz(Double_t pz) { fPz = pz; };
+    virtual void SetPx(Double_t px) {
+      fPx = px;
+    };
+    virtual void SetPy(Double_t py) {
+      fPy = py;
+    };
+    virtual void SetPz(Double_t pz) {
+      fPz = pz;
+    };
 
-    virtual void SetDPx(Double_t dpx) { fDPx = dpx; };
-    virtual void SetDPy(Double_t dpy) { fDPy = dpy; };
-    virtual void SetDPz(Double_t dpz) { fDPz = dpz; };
+    virtual void SetDPx(Double_t dpx) {
+      fDPx = dpx;
+    };
+    virtual void SetDPy(Double_t dpy) {
+      fDPy = dpy;
+    };
+    virtual void SetDPz(Double_t dpz) {
+      fDPz = dpz;
+    };
 
-    void SetQp(Double_t qp) { fQp = qp; };
+    void SetQp(Double_t qp) {
+      fQp = qp;
+    };
 
 
   protected:
@@ -94,6 +154,8 @@ class FairTrackPar : public TObject
     Int_t fq;
 
     FairLogger* fLogger;  //! /// FairLogger
+
+  private:
 
     FairTrackPar(const FairTrackPar&);
     FairTrackPar& operator=(const FairTrackPar&);
