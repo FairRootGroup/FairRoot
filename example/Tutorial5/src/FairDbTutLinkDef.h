7854007c4559bf1da7e7618b04b85d2ab5bbe588
Denis Bertini <D.Bertini@gsi.de>
2013-06-17 13:21:53 +0000

    Generic Parameter IO
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@20296 0381ead4-6506-0410-b988-94b70fbc4730

--- example/Tutorial5/src/FairDbTutLinkDef.h
+++ example/Tutorial5/src/FairDbTutLinkDef.h
@@ -6,7 +6,10 @@
 
 #pragma link C++ class FairDbResultPtr<FairDbTutPar>+;
 #pragma link C++ class FairDbWriter<FairDbTutPar>+;
+#pragma link C++ class FairDbResultPtr<FairDbTutParBin>+;
+#pragma link C++ class FairDbWriter<FairDbTutParBin>+;
 #pragma link C++ class  FairDbTutPar+;
+#pragma link C++ class  FairDbTutParBin+;
 #pragma link C++ class  FairDbTutContFact+;
 #pragma link C++ class  FairDbTutAccessRtdbTask+;
 
