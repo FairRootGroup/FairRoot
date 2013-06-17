1e92719bf75931f750d3a13076b3fd0123b996d4
Denis Bertini <D.Bertini@gsi.de>
2013-06-17 13:23:12 +0000

    Generic Parameter IO
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@20299 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ example/Tutorial5/macros/sql_params_read_bin.C
@@ -0,0 +1,53 @@
+Int_t  sql_params_read_bin()
+{
+  // ----  Load libraries   -------------------------------------------------
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+  basiclibs();
+  gSystem->Load("libGenVector");
+  gSystem->Load("libGeoBase");
+  gSystem->Load("libFairDB");
+  gSystem->Load("libParBase");
+  gSystem->Load("libBase");
+  gSystem->Load("libMCStack");
+  gSystem->Load("libField");
+  gSystem->Load("libTutorial5");
+
+
+  // Create a Runtime Database singleton.
+  FairRuntimeDb* db = FairRuntimeDb::instance();
+
+  // Set the SQL IO as first input
+  FairParTSQLIo* inp = new FairParTSQLIo();
+  inp->open();
+  db->setFirstInput(inp);
+
+  // Create the container via the factory if not already created
+  FairDbTutParBin* p1 = (FairDbTutParBin*)(db->getContainer("TUTParBin"));
+
+  p1->Print();
+
+  // Create a dummy runID using date in UTC from which
+  // corresponding  parameters will be initialised
+
+  ValTimeStamp tStamp(2013,06,17,09,29,00);
+  UInt_t runId = tStamp.GetSec();
+  cout << "-I- looking for parameters at runID# " << runId << endl;
+  cout << "-I- corresponding  time in runID (UTC) " << tStamp.AsString("c") << endl;
+
+  // Use the generated RunID to initialised the parameter
+  // using the SQL-based IO input
+  db->initContainers(runId);
+
+
+  // Get the container after initialisation
+  // from the RuntimeDB
+  FairDbTutParBin* pp1 = (FairDbTutParBin*)(db->getContainer("TUTParBin"));
+  pp1->Print();
+
+  cout << endl;
+  cout << "\n  -I- SQLDB: RuntimeDB::init from SQL Database done.... \n" << endl;
+  cout << endl;
+
+
+  return 0;
+}
