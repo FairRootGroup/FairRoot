0bea32b4df14d32094f42b070f6eedb2a1498e4e
Denis Bertini <D.Bertini@gsi.de>
2013-06-19 13:46:31 +0000

    Generic Object IO
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@20338 0381ead4-6506-0410-b988-94b70fbc4730

--- dbase/dbInterface/FairUtilStream.h
+++ dbase/dbInterface/FairUtilStream.h
@@ -16,9 +16,6 @@ void eat_whitespace(std::istream& is);
 /// return a string of words.  Strings are delimited by single quotes.
 /// any leading white space is eaten.
 std::string read_quoted_string(std::istream& is);
-//void  string_to_binary(const std::string source,  char* destination,  int length);
-//void  binary_to_string(const  char* source,  int length, std::string& destination);
-
 void BinToHex(void* const data,  const size_t dataLength, std::string& dest);
 void BinFromHex(const std::string& in, void* const data );
 
