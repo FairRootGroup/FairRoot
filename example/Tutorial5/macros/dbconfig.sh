ac966d69ba5ca9e052eba6161ac77afa6ab3ea26
Denis Bertini <D.Bertini@gsi.de>
2013-07-03 10:13:38 +0000

    FairDb Logging Mechanism included
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@20594 0381ead4-6506-0410-b988-94b70fbc4730

--- example/Tutorial5/macros/dbconfig.sh
+++ example/Tutorial5/macros/dbconfig.sh
@@ -9,6 +9,11 @@ export ENV_TSQL_URL="mysql://localhost/r3b"
 export ENV_TSQL_USER="scott"
 export ENV_TSQL_PSWD="tiger"
 
+# Log File 
+# -- If the string is NULL the default file "fairdbinfo.log"
+# -- will be created in the working directory 
+export ENV_LOGFILE_DB=""
+
 # Local PgSQL server
 #export ENV_TSQL_URL="pgsql://localhost:5432/R3B"
 #export ENV_TSQL_USER="postgres"
