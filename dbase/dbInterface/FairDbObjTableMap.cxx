90ccc71c57a4edb9cf2c15332a79cd3637b39ef0
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:44:10 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22208 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbObjTableMap.cxx
@@ -0,0 +1,45 @@
+#include "FairDbObjTableMap.h"
+
+ClassImp(FairDbObjTableMap)
+
+FairDbObjTableMap::FairDbObjTableMap()
+  : TObject(),
+    fVersion(0),
+    fTimeStart(),
+    fTimeEnd(),
+    fDetType(),
+    fSimType(),
+    fOwner(NULL)
+{
+}
+
+FairDbObjTableMap::FairDbObjTableMap(const FairDbObjTableMap& from)
+  : TObject(from),
+    fVersion(from.fVersion),
+    fTimeStart(from.fTimeStart),
+    fTimeEnd(from.fTimeEnd),
+    fDetType(from.fDetType),
+    fSimType(from.fSimType),
+    fOwner(from.fOwner)
+{
+}
+
+FairDbObjTableMap& FairDbObjTableMap::operator=(const FairDbObjTableMap& from)
+{
+  if (this == &from) { return *this; }
+
+  TObject::operator=(from);
+  fVersion=from.fVersion;
+  fTimeStart=from.fTimeStart;
+  fTimeEnd=from.fTimeEnd;
+  fDetType=from.fDetType;
+  fSimType=from.fSimType;
+  fOwner=from.fOwner;
+
+  return *this;
+}
+
+FairDbObjTableMap::~FairDbObjTableMap()
+{
+}
+
