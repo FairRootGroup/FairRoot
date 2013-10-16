6a10b35778845a2903fb7bebf7858e451b123098
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:46:38 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22219 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairRegistryElement.h
@@ -0,0 +1,35 @@
+
+#ifndef FAIRREGISTRYELEMENT_H
+#define FAIRREGISTRYELEMENT_H
+
+#include <TObject.h>
+
+#include "Riosfwd.h"                    // for istream, ostream
+#include "Rtypeinfo.h"                  // for type_info
+#include "Rtypes.h"                     // for FairRegistryElement::Class, etc
+
+#include <iosfwd>                       // for istream, ostream
+#include <typeinfo>                     // for type_info
+
+class FairRegistryElement : public TObject
+{
+
+  public:
+
+    FairRegistryElement();
+    virtual ~FairRegistryElement();
+    virtual FairRegistryElement* Dup(void) const = 0;
+
+    virtual std::ostream& PrintStream(std::ostream& os) const = 0;
+    virtual std::istream& ReadStream(std::istream& is) = 0;
+    virtual void Print(Option_t* option="") const = 0;
+
+    virtual const type_info& GetType() const = 0;
+    virtual const char* GetTypeAsString() const = 0;
+
+
+    ClassDef(FairRegistryElement,1)  // class FairRegistryElement
+};
+
+
+#endif  // FAIRREGISTRYELEMENT_H
