0655c803fd355d9ad6df9910ffdbb053c2366571
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:42:24 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22201 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbTableBuffer.h
@@ -0,0 +1,57 @@
+#ifndef FAIRDBTABLEBUFFER_H
+#define FAIRDBTABLEBUFFER_H
+
+#include "Rtypes.h"                     // for UInt_t, Bool_t, etc
+
+#include <string>                       // for string
+
+class FairDbFieldType;
+class FairDbTableMetaData;
+
+class FairDbTableBuffer
+{
+
+  public:
+
+    FairDbTableBuffer(const FairDbTableMetaData* metaData);
+    virtual ~FairDbTableBuffer();
+
+    virtual
+    const FairDbFieldType& ColFieldType(UInt_t col) const;
+    virtual       std::string ColName(UInt_t col) const;
+    virtual
+    const FairDbFieldType& CurColFieldType() const;
+    virtual       std::string CurColName() const;
+    virtual       UInt_t CurColNum() const { return fCurCol;}
+    Bool_t HasRowCounter() const { return fHasRowCounter; }
+    virtual       UInt_t NumCols() const;
+    virtual       std::string TableName() const;
+    virtual       std::string TableNameTc() const;
+    virtual const FairDbTableMetaData*
+    MetaData() const { return fMetaData; }
+
+
+    virtual    void DecrementCurCol() {
+      fCurCol = (fCurCol <= 1) ? 1 : fCurCol -1;
+    }
+
+    virtual    void IncrementCurCol() { ++fCurCol; }
+
+  protected:
+
+    virtual    void ClearCurCol() { fCurCol = 1; }
+
+  private:
+
+    UShort_t fCurCol;
+    Bool_t fHasRowCounter;
+    const FairDbTableMetaData* fMetaData;
+
+    FairDbTableBuffer(const FairDbTableBuffer&);
+    FairDbTableBuffer operator=(const FairDbTableBuffer&);
+
+    ClassDef(FairDbTableBuffer,0)     // Table Row Buffer
+
+};
+
+#endif  // FAIRDBTABLEBUFFER_H
