cb52743a6446828bb1852079b591d77a94db891c
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:42:29 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22202 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbTableBuffer.cxx
@@ -0,0 +1,62 @@
+#include "FairDbTableBuffer.h"
+
+#include "FairDbString.h"               // for ToUpper
+#include "FairDbTableMetaData.h"        // for FairDbTableMetaData, string
+
+class FairDbFieldType;
+
+using std::string;
+
+ClassImp(FairDbTableBuffer)
+
+
+FairDbTableBuffer::FairDbTableBuffer(const FairDbTableMetaData* metaData) :
+  fCurCol(1),
+  fHasRowCounter(kFALSE),
+  fMetaData(metaData)
+{
+  fHasRowCounter = fMetaData->HasRowCounter();
+}
+
+FairDbTableBuffer::~FairDbTableBuffer()
+{
+
+}
+
+const FairDbFieldType& FairDbTableBuffer::ColFieldType(UInt_t col) const
+{
+  return fMetaData->ColFieldType(col);
+}
+
+string FairDbTableBuffer::ColName(UInt_t col) const
+{
+  return fMetaData->ColName(col);
+}
+
+const FairDbFieldType& FairDbTableBuffer::CurColFieldType() const
+{
+  return fMetaData->ColFieldType(fCurCol);
+}
+
+
+string FairDbTableBuffer::CurColName() const
+{
+  return fMetaData->ColName(fCurCol);
+}
+
+
+UInt_t FairDbTableBuffer::NumCols() const
+{
+  return fMetaData->NumCols();
+}
+
+
+string FairDbTableBuffer::TableName() const
+{
+  return FairUtilString::ToUpper(fMetaData->TableName());
+}
+
+string FairDbTableBuffer::TableNameTc() const
+{
+  return fMetaData->TableName();
+}
