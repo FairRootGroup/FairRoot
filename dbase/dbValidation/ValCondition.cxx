6f6935252568c112f3d68c1d6315c1871034249a
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 09:04:58 +0000

    new condition
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22113 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbValidation/ValCondition.cxx
@@ -0,0 +1,79 @@
+#include "ValCondition.h"
+
+#include <stdio.h>                      // for sprintf, printf
+
+ClassImp(ValCondition)
+
+
+ValCondition::ValCondition()
+  :  fDetector(Detector::kUnknown),
+     fDataType(DataType::kUnknown),
+     fTimeStamp()
+{
+  // Default all is unKnown
+}
+
+ValCondition::~ValCondition() { ; }
+
+
+
+std::ostream& operator<<(std::ostream& os, const ValCondition& vldc)
+{
+  if (os.good()) {
+    if (os.tie()) { os.tie()->flush(); }
+    os << vldc.AsString();
+  }
+
+  if (os.flags() & std::ios::unitbuf) { os.flush(); }
+  return os;
+}
+
+
+ValCondition::ValCondition(const Detector::Detector_t& detector,
+                           const DataType::DataType_t mcFlag,
+                           const ValTimeStamp& tstamp)
+  : TObject(),
+    fDetector(detector),
+    fDataType(mcFlag),
+    fTimeStamp(tstamp)
+{
+  // normal constructor
+}
+
+
+const char* ValCondition::AsString(Option_t* option) const
+{
+  static char newstring[80] = " ";
+
+  switch (option[0]) {
+  case 'c':
+  case 'C':
+    sprintf(newstring,"[%c%c %s]",
+            Detector::AsString(GetDetector())[0],
+            DataType::AsString(GetDataType())[0],
+            fTimeStamp.AsString("c"));
+    break;
+  default:
+    sprintf(newstring,"[%6.6s|%6.6s|%s}",
+            Detector::AsString(GetDetector()),
+            DataType::AsString(GetDataType()),
+            fTimeStamp.AsString("c"));
+  }
+
+  return newstring;
+}
+
+
+void ValCondition::Print(Option_t* option) const
+{
+  printf("%s\n",AsString(option));
+}
+
+
+Bool_t ValCondition::IsNull() const
+{
+  return fDetector==Detector::kUnknown && fDataType == DataType::kUnknown;
+}
+
+
+
