3dfc9da9ce4f7822318cf59d8a446a544f98b797
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:51:58 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22239 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbValRecordMap.cxx
@@ -0,0 +1,92 @@
+#include "FairDbValRecordMap.h"
+
+#include "FairDbProxy.h"                // for FairDbProxy
+#include "FairDbResult.h"               // for FairDbResult, etc
+#include "FairDbTableInterface.h"           // for FairDbTableInterface
+#include "FairDbTableInterfaceStore.h"   // for FairDbTableInterfaceStore
+#include "FairDbObjTableMap.h"             // for FairDbObjTableMap
+#include "FairDbValRecord.h"          // for FairDbValRecord
+
+#include <stddef.h>                     // for NULL
+#include <utility>                      // for pair
+
+using std::string;
+using std::map;
+
+ClassImp(FairDbValRecordMap)
+
+
+
+FairDbValRecordMap::FairDbValRecordMap(const string& tableName,UInt_t dbNo,UInt_t seqNo) :
+  fDbNo(dbNo),
+  fResult(NULL),
+  fSeqNoToRec()
+{
+// Get Db proxy for the table.
+  FairDbValRecord pet;
+  const FairDbProxy& proxy = FairDbTableInterfaceStore::Instance()
+                             .GetTableInterface(tableName,&pet)
+                             .GetDBProxy();
+
+// Collect up all validity records.
+  FairDbResultPool* rset = proxy.QueryAllValidities(dbNo,seqNo);
+  fResult = new FairDbResultNonCombo(rset,&pet,0,kFALSE);
+  delete rset;
+
+
+}
+
+
+
+FairDbValRecordMap::~FairDbValRecordMap()
+{
+
+  delete fResult;
+
+}
+
+
+UInt_t FairDbValRecordMap::GetNumRows() const
+{
+
+  return fResult ? fResult->GetNumRows() : 0;
+}
+
+
+const string FairDbValRecordMap::GetTableName() const
+{
+
+  return fResult ? fResult->TableName() : "Unknown";
+}
+
+
+const FairDbValRecord* FairDbValRecordMap::GetObjTableMap(UInt_t rowNum) const
+{
+
+  return fResult ?
+         dynamic_cast<const FairDbValRecord*>(fResult->GetObjTableMap(rowNum))
+         : 0;
+}
+
+
+const FairDbValRecord* FairDbValRecordMap::GetObjTableMapBySeqNo(UInt_t seqNo
+                                                                ) const
+{
+
+  UInt_t numRows = GetNumRows();
+  if ( numRows == 0 ) { return 0; }
+
+// Create look-up table if not yet built.
+
+  if ( fSeqNoToRec.size() == 0 ) {
+    for ( UInt_t irow = 0; irow < numRows; ++irow) {
+      const FairDbValRecord* vrec = GetObjTableMap(irow);
+      fSeqNoToRec[vrec->GetSeqNo()] = vrec;
+    }
+  }
+
+  map<UInt_t,const FairDbValRecord*>::const_iterator itr = fSeqNoToRec.find(seqNo);
+  return ( itr == fSeqNoToRec.end() ) ? 0 : itr->second;
+
+}
+
