6b5ec3c1037eece793ee080d68aaf49670900daf
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:49:13 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22229 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbExtSqlContent.h
@@ -0,0 +1,61 @@
+
+
+#ifndef FAIRDBEXTSQLCONTENT
+#define FAIRDBEXTSQLCONTENT
+
+
+#include "FairDbString.h"               // for FairDbString
+#include "DataType.h"                    // for DataType_t
+#include "ValTimeStamp.h"               // for ValTimeStamp
+#include "db_detector_def.h"            // for Detector, etc
+
+#include "Rtypes.h"                     // for FairDbExtSqlContent::Class, etc
+
+#include <string>                       // for string
+
+class FairDbExtSqlContent : public FairDbString
+{
+
+  public:
+
+
+    enum  IntervalType { kBefore,      kAfter,    kMisses,
+                         kThroughout,  kWithin,   kOverlaps,
+                         kStarts,      kEnds,
+                         kUndefined
+                       };
+
+
+    FairDbExtSqlContent(const std::string& ctxt = "");
+    FairDbExtSqlContent(IntervalType interval,
+                        ValTimeStamp start,
+                        ValTimeStamp end,
+                        Detector::Detector_t det,
+                        DataType::DataType_t sim);
+
+    virtual ~FairDbExtSqlContent();
+
+
+    ValTimeStamp             GetTimeStart() const { return fTimeStart; }
+    ValTimeStamp             GetTimeEnd()   const { return fTimeEnd; }
+    Detector::Detector_t     GetDetector()  const { return fDetType; }
+    DataType::DataType_t       GetDataType()   const { return fSimType; }
+
+
+
+  private:
+
+    IntervalType             fIntervalType;
+    ValTimeStamp             fTimeStart;
+    ValTimeStamp             fTimeEnd;
+    Detector::Detector_t     fDetType;
+    DataType::DataType_t       fSimType;
+
+
+    ClassDef(FairDbExtSqlContent,0)     // SQL for Extended Content Query
+
+};
+
+
+#endif // FAIRDBEXTSQLCONTENT
+
