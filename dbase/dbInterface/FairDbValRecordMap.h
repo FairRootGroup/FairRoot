4a19be12487f00f3d463313617bdc11e6f132263
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:51:42 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22238 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbValRecordMap.h
@@ -0,0 +1,40 @@
+#ifndef FAIRDBVALRECORDMAP
+#define FAIRDBVALRECORDMAP
+
+#include "Rtypes.h"                     // for UInt_t, etc
+
+#include <map>                          // for map
+#include <string>                       // for string
+
+class FairDbResult;
+class FairDbValRecord;
+
+class FairDbValRecordMap
+{
+
+  public:
+    FairDbValRecordMap(const std::string& tableName,UInt_t dbNo,UInt_t seqNo=0);
+    virtual ~FairDbValRecordMap();
+
+    UInt_t GetDbNo() const { return fDbNo; }
+    UInt_t GetNumRows() const;
+    const std::string GetTableName() const;
+    const FairDbValRecord* GetObjTableMap(UInt_t rowNum) const;
+    const FairDbValRecord* GetObjTableMapBySeqNo(UInt_t seqNo) const;
+
+
+  private:
+
+    UInt_t fDbNo;
+    FairDbResult* fResult;
+    mutable std::map<UInt_t,const FairDbValRecord*>fSeqNoToRec;
+
+    FairDbValRecordMap(const FairDbValRecordMap&);
+    FairDbValRecordMap operator=(const FairDbValRecordMap&);
+
+    ClassDef(FairDbValRecordMap,0)     //  A map list of FairDbValRecords
+
+};
+
+
+#endif  // FAIRDBVALRECORDMAP
