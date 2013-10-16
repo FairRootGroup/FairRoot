e27c7f9ea15ab8687f4be339f6fb40c32aacbe23
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:44:27 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22209 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbStopWatch.h
@@ -0,0 +1,47 @@
+
+#ifndef FAIRDBSTOPWATCH
+#define FAIRDBSTOPWATCH
+
+#include "Rtypes.h"                     // for UInt_t, FairDbStopWatch::Class, etc
+#include "TStopwatch.h"                 // for TStopwatch
+
+#include <string>                       // for string
+
+class FairDbStopWatch
+{
+
+  public:
+    FairDbStopWatch();
+    virtual ~FairDbStopWatch();
+
+    void RecBegin(std::string tableName, UInt_t rowSize);
+    void RecEnd(UInt_t numRows);
+    void RecFillAgg(Int_t aggNo);
+    void RecMainQuery();
+    void Resume();
+    void StartSubWatch(UInt_t subWatch);
+    void Suspend();
+
+
+
+  private:
+
+    enum  QueryStage { kPassive, kInitialQuery, kMainQuery };
+    enum { kMaxSubWatch = 4 };
+
+// Data members
+
+    Int_t fCurSubWatch;
+    UInt_t fRowSize;
+    QueryStage fQueryStage;
+    TStopwatch fSubWatches[kMaxSubWatch]; // Partial watches
+    std::string fTableName;   // Current table
+    TStopwatch fWatch;       // StopWatch
+
+    ClassDef(FairDbStopWatch,0)     // Query StopWatch
+
+};
+
+
+#endif // FAIRDBSTOPWATCH
+
