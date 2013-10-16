106cb15d476d11c961692e70c37fb1da5d6e1925
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 11:13:59 +0000

    new dbase
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22266 0381ead4-6506-0410-b988-94b70fbc4730

--- example/Tutorial5/macros/dbconfig.sh
+++ example/Tutorial5/macros/dbconfig.sh
@@ -5,16 +5,16 @@ export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/mysql/lib
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/mysql/lib
 
 # Local MySQL server
-export ENV_TSQL_URL="mysql://localhost/r3b"
-export ENV_TSQL_USER="scott"
-export ENV_TSQL_PSWD="tiger"
+export FAIRDB_TSQL_URL="mysql://localhost/r3b"
+export FAIRDB_TSQL_USER="scott"
+export FAIRDB_TSQL_PSWD="tiger"
 
 # Log File 
 # -- If the string is NULL the default file "fairdbinfo.log"
 # -- will be created in the working directory 
-export ENV_LOGFILE_DB=""
+export FAIRDB_LOGFILE_DB=""
 
 # Local PgSQL server
-#export ENV_TSQL_URL="pgsql://localhost:5432/R3B"
-#export ENV_TSQL_USER="postgres"
-#export ENV_TSQL_PSWD="pasglop1"
+#export FAIRDB_TSQL_URL="pgsql://localhost:5432/R3B"
+#export FAIRDB_TSQL_USER="postgres"
+#export FAIRDB_TSQL_PSWD="pasglop1"
