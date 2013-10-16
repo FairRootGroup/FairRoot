c17f0763f16381358901a5d6db0575a107449804
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:41:57 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22199 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbRollbackTimes.h
@@ -0,0 +1,42 @@
+#ifndef FAIRDBROLLBACKTIMES_H
+#define FAIRDBROLLBACKTIMES_H
+
+#ifndef ROOT_Rtypes
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include "Rtypes.h"                     // for FairDbRollbackTimes::Class, etc
+#endif
+#endif
+
+#include <map>                          // for map, map<>::value_compare
+#include <string>                       // for string, operator<
+
+class FairRegistry;
+
+class FairDbRollbackTimes
+{
+
+  public:
+
+    typedef std::map<std::string,std::string> name_map_t;
+
+    FairDbRollbackTimes();
+    virtual ~FairDbRollbackTimes();
+
+    const std::string& GetDate(const std::string& tableName) const;
+    const std::string& GetType(const std::string& tableName) const;
+    void Show() const;
+
+    void Clear() {fTableToDate.clear(); fTableToType.clear();}
+    void Set(FairRegistry& reg);
+
+  private:
+
+    name_map_t fTableToDate;
+    name_map_t fTableToType;
+
+    ClassDef(FairDbRollbackTimes,0)   // Rollback times using table name.
+
+};
+
+
+#endif  // FAIRDBROLLBACKTIMES_H
