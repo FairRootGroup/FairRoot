f37f6159b4da77b95d7ff80bc56f1269a1d95b62
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:44:58 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22211 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbStopWatchManager.h
@@ -0,0 +1,50 @@
+#ifndef FAIRDBSTOPWATCHMANAGER
+#define FAIRDBSTOPWATCHMANAGER
+
+#include "Rtypes.h"                     // for Bool_t, UInt_t, etc
+
+#include <string>
+#include <list>
+
+class FairDbStopWatch;
+
+class FairDbStopWatchManager
+{
+
+  public:
+
+    FairDbStopWatchManager();
+    virtual ~FairDbStopWatchManager();
+
+    void Enable(Bool_t flag=kTRUE,
+                Bool_t enableSubWatch = kFALSE) {
+      fEnabled = flag;
+      fSubWatchEnabled = enableSubWatch;
+    }
+    void RecBegin(std::string tableName, UInt_t rowSize);
+    void RecEnd(UInt_t numRows);
+    void RecFillAgg(Int_t aggNo);
+    void RecMainQuery();
+    void StartSubWatch(UInt_t subWatch);
+
+
+    static FairDbStopWatchManager gStopWatchManager;
+
+  private:
+
+    FairDbStopWatch* GetCurrent();
+    FairDbStopWatch* Pop();
+    FairDbStopWatch* Push();
+
+
+    Bool_t fEnabled;
+    Bool_t fSubWatchEnabled;
+    std::list<FairDbStopWatch*> fStopWatchs;
+
+    ClassDef(FairDbStopWatchManager,0)    // Query StopWatch
+
+};
+
+
+#endif // FAIRDBSTOPWATCHMANAGER
+
