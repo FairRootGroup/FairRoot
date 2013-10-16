08db9253ef9528a3e85e4727dacdd4fd1bbd37f0
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:56:43 +0000

    new layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22246 0381ead4-6506-0410-b988-94b70fbc4730

--- dbase/FairDBLinkDef.h
+++ dbase/FairDBLinkDef.h
@@ -1,4 +1,3 @@
-//2005/06/23 07:14:26 dbertini Exp $
 
 #ifdef __CINT__
 
@@ -17,9 +16,9 @@
 #pragma link C++ enum          Detector::EDetector;
 #pragma link C++ nestedtypedef Detector::Detector_t;
 
-#pragma link C++ namespace     SimFlag;
-#pragma link C++ enum          SimFlag::ESimFlag;
-#pragma link C++ nestedtypedef SimFlag::SimFlag_t;
+#pragma link C++ namespace     DataType;
+#pragma link C++ enum          DataType::EDataType;
+#pragma link C++ nestedtypedef DataType::DataType_t;
 
 // dbValidation
 #pragma link C++ class ValTimeStamp+;
@@ -32,68 +31,67 @@
 #pragma link C++ function operator>=(const ValTimeStamp&, const ValTimeStamp&);
 #pragma link C++ function operator- (const ValTimeStamp&, const ValTimeStamp&);
 
-#pragma link C++ class ValContext+;
-#pragma link C++ function operator==(const ValContext&, const ValContext&);
-#pragma link C++ function operator!=(const ValContext&, const ValContext&);
-#pragma link C++ function operator< (const ValContext&, const ValContext&);
-#pragma link C++ function operator<<(ostream&, const ValContext&);
+#pragma link C++ class ValCondition+;
+#pragma link C++ function operator==(const ValCondition&, const ValCondition&);
+#pragma link C++ function operator!=(const ValCondition&, const ValCondition&);
+#pragma link C++ function operator< (const ValCondition&, const ValCondition&);
+#pragma link C++ function operator<<(ostream&, const ValCondition&);
 
-#pragma link C++ class ValRange+;
-#pragma link C++ function operator<<(ostream&, const ValRange&);
+#pragma link C++ class ValInterval+;
+#pragma link C++ function operator<<(ostream&, const ValInterval&);
 
-#pragma link C++ class ValValidate;
 
 // dbInterface
-#pragma link C++ class FairDbResultPtr<FairDbLogEntry>+;
+#pragma link C++ class FairDbReader<FairDbLogEntry>+;
 #pragma link C++ class FairDbWriter<FairDbLogEntry>+;
-#pragma link C++ class FairDbResultPtr<FairDbConfigSet>+;
-#pragma link C++ class FairDbWriter<FairDbConfigSet>+;
-#pragma link C++ class FairDbResultPtr<FairDbValidityRec>+;
+#pragma link C++ class FairDbReader<FairDbConfigData>+;
+#pragma link C++ class FairDbWriter<FairDbConfigData>+;
+#pragma link C++ class FairDbReader<FairDbValRecord>+;
 
 #pragma link C++ class FairDb+;
 #pragma link C++ class FairDbCache+;
-#pragma link C++ class FairDbMultConnector+;
-#pragma link C++ class FairDbConfigSet+;
-#pragma link C++ class FairDbConfigStream+;
+#pragma link C++ class FairDbConnectionPool+;
+#pragma link C++ class FairDbConfigData+;
+#pragma link C++ class FairDbConfigBuffer+;
 #pragma link C++ class FairDbConnection+;
-#pragma link C++ class FairDbConnectionMaintainer+;
+#pragma link C++ class FairDbConnectionManager+;
 #pragma link C++ class FairDbProxy+;
 #pragma link C++ class FairDbFieldType+;
 #pragma link C++ class FairDbResultKey::VRecKey+;
 #pragma link C++ class FairDbResultKey+;
 #pragma link C++ class FairDbLogEntry+;
-#pragma link C++ class FairDbOutRowStream+;
+#pragma link C++ class FairDbOutTableBuffer+;
 #pragma link C++ class FairDbResult+;
-#pragma link C++ class FairDbResultAgg+;
-#pragma link C++ class FairDbResultNonAgg+;
-#pragma link C++ class FairDbResultSet+;
-#pragma link C++ class FairDbRollbackDates+;
-#pragma link C++ class FairDbRowStream+;
-#pragma link C++ class FairDbSqlContext+;
-#pragma link C++ class FairDbSqlValPacket+;
+#pragma link C++ class FairDbResultCombo+;
+#pragma link C++ class FairDbResultNonCombo+;
+#pragma link C++ class FairDbResultPool+;
+#pragma link C++ class FairDbRollbackTimes+;
+#pragma link C++ class FairDbTableBuffer+;
+#pragma link C++ class FairDbExtSqlContent+;
+#pragma link C++ class FairDbSqlValidityData+;
 #pragma link C++ class FairDbString+;
 #pragma link C++ class FairDbStreamer+;
 #pragma link C++ class FairDbStatement+;
 #pragma link C++ class FairDbTableMetaData+;
-#pragma link C++ class FairDbTableProxyRegistry+;
-#pragma link C++ class FairDbTableProxy+;
-#pragma link C++ class FairDbTableRow+;
-#pragma link C++ class FairDbSimFlagAssociation+;
-#pragma link C++ class FairDbTimer+;
-#pragma link C++ class FairDbTimerManager+;
-#pragma link C++ class FairDbValidityRec+;
-#pragma link C++ class FairDbValRecSet+;
-#pragma link C++ class FairDbValidityRecBuilder+;
+#pragma link C++ class FairDbTableInterfaceStore+;
+#pragma link C++ class FairDbTableInterface+;
+#pragma link C++ class FairDbObjTableMap+;
+#pragma link C++ class FairDbDataTypeUnion+;
+#pragma link C++ class FairDbStopWatch+;
+#pragma link C++ class FairDbStopWatchManager+;
+#pragma link C++ class FairDbValRecord+;
+#pragma link C++ class FairDbValRecordMap+;
+#pragma link C++ class FairDbValRecordFactory+;
 #pragma link C++ class FairDbException+;
 #pragma link C++ class FairDbExceptionLog+;
 #pragma link C++ class FairDbConfigurable+;
 #pragma link C++ class FairRegistry-;
-#pragma link C++ class FairRegistryItem;
-#pragma link C++ class FairRegistryItemXxx<int>-;
-#pragma link C++ class FairRegistryItemXxx<double>-;
-#pragma link C++ class FairRegistryItemXxx<FairRegistry>-;
-#pragma link C++ class FairRegistryItemXxx<char>-;
-#pragma link C++ class FairRegistryItemXxx<const char*>-;
+#pragma link C++ class FairRegistryElement;
+#pragma link C++ class FairRegistryElementGeneric<int>-;
+#pragma link C++ class FairRegistryElementGeneric<double>-;
+#pragma link C++ class FairRegistryElementGeneric<FairRegistry>-;
+#pragma link C++ class FairRegistryElementGeneric<char>-;
+#pragma link C++ class FairRegistryElementGeneric<const char*>-;
 #pragma link C++ function operator<<(ostream&, const FairRegistry&);
 
 // #### SQL object
