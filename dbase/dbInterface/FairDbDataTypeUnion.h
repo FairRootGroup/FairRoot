cd2d3a92a55506a11d15bc9912762a8dd1061853
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:52:19 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22240 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbDataTypeUnion.h
@@ -0,0 +1,51 @@
+
+#ifndef FAIRDBDATATYPEUNION
+#define FAIRDBDATATYPEUNION
+
+#include "DataType.h"                    // for DataType_t
+
+#include "Rtypes.h"
+
+#include <iosfwd>                       // for ostream
+#include <list>                         // for list
+#include <map>                          // for map, map<>::mapped_type, etc
+
+class FairRegistry;
+
+
+class FairDbDataTypeUnion
+{
+  public:
+
+    typedef std::list<DataType::DataType_t>           SimList_t;
+    typedef std::map<DataType::DataType_t,SimList_t > SimMap_t;
+
+    FairDbDataTypeUnion();
+    virtual ~FairDbDataTypeUnion();
+
+    SimList_t Get(const DataType::DataType_t value)const;
+    void Print(std::ostream& s)const;
+    void Show();
+
+    static const FairDbDataTypeUnion& Instance();
+
+    void Clear() { fUnions.clear(); }
+    void Set(const DataType::DataType_t value, SimList_t list) {
+      fUnions[value] = list;
+    }
+    void Set(FairRegistry& reg);
+
+  private:
+
+    static const FairDbDataTypeUnion* fgInstance;
+
+
+    SimMap_t fUnions;
+
+    ClassDef(FairDbDataTypeUnion,0)  // DataType types Assoc.
+
+};
+
+std::ostream& operator<<(std::ostream& s, const FairDbDataTypeUnion& dataFlagAss);
+
+#endif // FAIRDBDATATYPEUNION
