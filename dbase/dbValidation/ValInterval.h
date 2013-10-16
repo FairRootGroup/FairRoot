4ed113efd92933c6050c0d2f35834e743c590875
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 09:06:44 +0000

    new IOV
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22114 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbValidation/ValInterval.h
@@ -0,0 +1,59 @@
+
+#ifndef VALINTERVAL_H
+#define VALINTERVAL_H
+
+#include "TObject.h"                    // for TObject
+
+#include "ValTimeStamp.h"               // for ValTimeStamp
+
+#include "Riosfwd.h"                    // for ostream
+#include "Rtypes.h"                     // for Int_t, Bool_t, Option_t, etc
+#include "TString.h"                    // for TString
+
+#include <iostream>                     // for ostream
+
+class ValCondition;
+
+class ValInterval : public TObject
+{
+
+  public:
+
+    ValInterval();
+    ValInterval(const Int_t detMask, const Int_t simMask,
+                const ValTimeStamp& tstart, const ValTimeStamp& tend,
+                const TString& source);
+    virtual ~ValInterval();
+
+    const char* AsString(Option_t* option = "") const;
+    Bool_t IsCompatible(const ValCondition& vldc) const;
+    Bool_t IsCompatible(const ValCondition* vldc) const;
+
+    Int_t  GetDetectorMask()    const { return fDetectorMask; }
+    Int_t  GetSimMask()         const { return fSimMask; }
+    ValTimeStamp GetTimeStart() const { return fTimeStart; }
+    ValTimeStamp GetTimeEnd()   const { return fTimeEnd; }
+    TString GetDataSource()     const { return fDataSource; }
+
+    void Print(Option_t* option = "") const;
+
+    void TrimTo(const ValInterval& vldr);
+    void SetTimeStart(const ValTimeStamp& tstart) { fTimeStart = tstart; }
+    void SetTimeEnd(const ValTimeStamp& tend) { fTimeEnd = tend; }
+
+  protected:
+
+    Int_t              fDetectorMask; //  Detector::Detector_t (or compacted)
+    Int_t              fSimMask;      // data/mc conditions (or compacted)
+    ValTimeStamp       fTimeStart;
+    ValTimeStamp       fTimeEnd;
+    TString            fDataSource;
+
+  private:
+
+    ClassDef(ValInterval,1)  // IOV Interval of Validity
+};
+
+std::ostream& operator<<(std::ostream& os, const ValInterval& vldr);
+
+#endif // VALINTERVAL_H
