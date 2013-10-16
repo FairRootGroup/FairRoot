63be546d0fa98d8a21844028a5e686bafc11e385
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:48:19 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22225 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbConfigBuffer.h
@@ -0,0 +1,64 @@
+#ifndef FAIRDBCONFIGBUFFER_H
+#define FAIRDBCONFIGBUFFER_H
+
+#include "FairDb.h"                     // for Version
+#include "FairDbConfigData.h"            // for FairDbConfigData
+#include "FairDbReader.h"               // for FairDbReader
+#include "FairDbValRecord.h"          // for FairDbValRecord
+#include "ValCondition.h"                 // for ValCondition
+
+#include "Riosfwd.h"                    // for ostream
+#include "Rtypes.h"                     // for Bool_t, etc
+
+#include <iosfwd>                       // for ostream
+#include <string>                       // for string
+
+class FairRegistry;
+
+class FairDbConfigBuffer
+{
+
+
+  public:
+
+    static      ValCondition fgDefaultContext;
+
+    FairDbConfigBuffer();
+    FairDbConfigBuffer(const std::string& SoftwName,
+                       const std::string& ConfigName="default",
+                       ValCondition vc = FairDbConfigBuffer::fgDefaultContext,
+                       FairDb::Version task=0,
+                       const std::string& tableName="SOFTWARE_CONF");
+    virtual ~FairDbConfigBuffer();
+
+    const FairDbConfigBuffer& operator>>(FairRegistry* reg);
+    const FairDbConfigData* GetConfigData() const { return fCFSet; }
+
+
+    FairDbConfigBuffer& operator<<(const FairRegistry* reg);
+    Bool_t IsEmpty() { return fCFSet == 0; }
+    FairDbValRecord& GetValidityRec() { return fVRec; }
+    Bool_t Write(UInt_t dbNo = 0,
+                 const std::string& logComment = "",
+                 Bool_t localTest = false);
+
+  private:
+
+    FairDbConfigBuffer(const FairDbConfigBuffer&);
+    FairDbConfigBuffer operator=(const FairDbConfigBuffer&);
+
+
+    const FairDbConfigData* fCFSet;
+    FairDbReader<FairDbConfigData> fCFSetTable;
+    FairDbConfigData fCFSetModified;
+    FairDbValRecord fVRec;
+    std::string    fConfigName;
+    std::string    fSoftwName;
+
+    ClassDef(FairDbConfigBuffer,0)
+
+};
+
+ostream& operator<<(ostream& s, const FairDbConfigBuffer& cfStream);
+
+#endif  // FAIRDBCONFIGBUFFER_H
