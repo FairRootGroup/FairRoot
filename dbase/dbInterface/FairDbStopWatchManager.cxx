4034d45762f416c3d074cea06eb8b299f205208f
Denis Bertini <D.Bertini@gsi.de>
2013-10-16 10:45:03 +0000

    new Layout
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22212 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ dbase/dbInterface/FairDbStopWatchManager.cxx
@@ -0,0 +1,103 @@
+#include "FairDbStopWatchManager.h"
+
+#include "FairDbStopWatch.h"
+
+using std::string;
+
+ClassImp(FairDbStopWatchManager)
+
+
+FairDbStopWatchManager FairDbStopWatchManager::gStopWatchManager;
+
+FairDbStopWatchManager::FairDbStopWatchManager() :
+  fEnabled(kTRUE),
+  fSubWatchEnabled(kFALSE),
+  fStopWatchs()
+{
+}
+
+FairDbStopWatchManager::~FairDbStopWatchManager()
+{
+  while ( this->GetCurrent() ) { this->Pop(); }
+
+}
+
+FairDbStopWatch* FairDbStopWatchManager::GetCurrent()
+{
+
+  return fStopWatchs.empty() ? 0 : *(fStopWatchs.begin());
+}
+
+FairDbStopWatch* FairDbStopWatchManager::Pop()
+{
+  if ( fStopWatchs.empty() ) { return 0; }
+
+  FairDbStopWatch* timer = this->GetCurrent();
+  delete timer;
+  timer = 0;
+  fStopWatchs.pop_front();
+  timer = this->GetCurrent();
+  if ( timer ) { timer->Resume(); }
+  return timer;
+
+}
+
+
+FairDbStopWatch* FairDbStopWatchManager::Push()
+{
+
+  FairDbStopWatch* timer = this->GetCurrent();
+  if ( timer ) { timer->Suspend(); }
+  fStopWatchs.push_front(new FairDbStopWatch);
+  return this->GetCurrent();
+
+}
+
+void FairDbStopWatchManager::RecBegin(string tableName, UInt_t rowSize)
+{
+
+  if ( ! fEnabled ) { return; }
+  FairDbStopWatch* timer = this->Push();
+  timer->RecBegin(tableName, rowSize);
+}
+
+void FairDbStopWatchManager::RecEnd(UInt_t numRows)
+{
+
+  if ( ! fEnabled ) { return; }
+
+
+  FairDbStopWatch* timer = this->GetCurrent();
+  if ( timer ) { timer->RecEnd(numRows); }
+  timer = this->Pop();
+
+}
+
+void FairDbStopWatchManager::RecFillAgg(Int_t aggno)
+{
+  if ( ! fEnabled ) { return; }
+
+
+}
+
+
+void FairDbStopWatchManager::RecMainQuery()
+{
+
+  if ( ! fEnabled ) { return; }
+  FairDbStopWatch* timer = this->GetCurrent();
+  if ( timer ) { timer->RecMainQuery(); }
+
+}
+
+
+void FairDbStopWatchManager::StartSubWatch(UInt_t subWatch)
+{
+
+  if ( ! fEnabled ) { return; }
+  FairDbStopWatch* timer = this->GetCurrent();
+  if ( timer ) { timer->StartSubWatch(subWatch); }
+
+}
+
+
