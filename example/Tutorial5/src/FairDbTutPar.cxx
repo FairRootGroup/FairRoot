695ff716a6f6317918e61876ba80971a16fb6510
Denis Bertini <D.Bertini@gsi.de>
2013-10-23 12:44:41 +0000

    Meta-Data Memory managment added
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22375 0381ead4-6506-0410-b988-94b70fbc4730

--- example/Tutorial5/src/FairDbTutPar.cxx
+++ example/Tutorial5/src/FairDbTutPar.cxx
@@ -1,23 +1,26 @@
 #include "FairDbTutPar.h"
 
-#include "FairDb.h"                     // for GetValDefinition
-#include "FairDbConnection.h"           // for FairDbConnection
-#include "FairDbExceptionLog.h"         // for FairDbExceptionLog
-#include "FairDbConnectionPool.h"        // for FairDbConnectionPool
+//#include "FairDb.h"                     // for GetValDefinition
+//#include "FairDbConnection.h"           // for FairDbConnection
+//#include "FairDbExceptionLog.h"         // for FairDbExceptionLog
+//#include "FairDbConnectionPool.h"        // for FairDbConnectionPool
+
+
+#include "FairDbLogFormat.h"
+#include "FairDbLogService.h"
+
 #include "FairDbOutTableBuffer.h"         // for FairDbOutRowStream
-#include "FairDbReader.h"               // for FairDbReader
-#include "FairDbResult.h"               // for FairDbResultSet
+//#include "FairDbResult.h"               // for FairDbResultSet
 #include "FairDbStatement.h"            // for FairDbStatement
-#include "FairDbTableInterfaceStore.h"   // for FairDbTableInterfaceStore
-#include "FairDbValRecord.h"
-#include "FairDbWriter.h"               // for FairDbWriter
+//#include "FairDbTableInterfaceStore.h"   // for FairDbTableInterfaceStore
+//#include "FairDbValRecord.h"
+//#include "FairDbWriter.h"               // for FairDbWriter
 #include "FairParamList.h"              // for FairParamList
 
 #include "Riosfwd.h"                    // for ostream
 #include "TString.h"                    // for TString
 
 #include <stdlib.h>                     // for exit
-#include <iostream>                     // for operator<<, basic_ostream, etc
 #include <memory>                       // for auto_ptr, etc
 #include <vector>                       // for vector, vector<>::iterator
 
@@ -33,32 +36,35 @@ template class  FairDbReader<FairDbTutPar>;
 template class  FairDbWriter<FairDbTutPar>;
 
 
+
 FairDbTutPar::FairDbTutPar(const char* name, const char* title, const char* context)
   : FairParGenericSet(name,title,context),
     fTopPitch(0.),
     fTopAnchor(0.),
     fTopNrFE(0),
-    fFeType("")
+    fFeType(""),
+    fDbEntry(0),  // Set the default Db Entry to the first slot
+    fParam_Writer(NULL), //  Writer Meta-Class for SQL IO
+    fParam_Reader(NULL), //  Reader Meta-Class for SQL IO
+    fMultConn(FairDbTableInterfaceStore::Instance().fConnectionPool)
 {
-//  clear();
 }
 
 
-string FairDbTutPar::GetTableDefinition(const char* Name)
+FairDbTutPar::~FairDbTutPar()
 {
+  if (fParam_Writer) {
+    delete fParam_Writer;
+    fParam_Writer=NULL;
+  }
 
-  string sql("create table ");
-  if ( Name ) { sql += Name; }
-  else { sql += "FAIRDBTUTPAR"; }
-  sql += "( SEQNO          INT NOT NULL,";
-  sql += "  ROW_ID         INT NOT NULL,";
-  sql += "  TOPPITCH       DOUBLE,";
-  sql += "  TOPANCHOR      DOUBLE,";
-  sql += "  TOPNRFE        INT,";
-  sql += "  FETYPE         TEXT,";
-  sql += "  primary key(SEQNO,ROW_ID))";
-  return sql;
+  if (fParam_Reader) {
+    delete fParam_Reader;
+    fParam_Reader=NULL;
+  }
+
 }
+
 
 void FairDbTutPar::putParams(FairParamList* list)
 {
@@ -84,22 +90,29 @@ Bool_t FairDbTutPar::getParams(FairParamList* list)
 
 void FairDbTutPar::clear()
 {
-  cout << "-I- clear called \n" << endl;
   fTopPitch=fTopAnchor=0.0;
   fTopNrFE=0;
   fFeType="";
+  if (fParam_Writer) { fParam_Writer->Reset(); }
+  if (fParam_Reader) { fParam_Reader->Reset(); }
 }
 
 
-void FairDbTutPar::Print()
+string FairDbTutPar::GetTableDefinition(const char* Name)
 {
-  std::cout<<"Tutorial SQL  Parameters:"<<std::endl;
-  std::cout<<"   Top Pitch    = "<<fTopPitch<<std::endl;
-  std::cout<<"   Top Anchor   = "<<fTopAnchor<< std::endl;
-  std::cout<<"   Nr of Frontends (Top Side)   = "<<fTopNrFE<<std::endl;
-  std::cout<<"   Frontend type name is        = "<<fFeType<<std::endl;
-}
 
+  string sql("create table ");
+  if ( Name ) { sql += Name; }
+  else { sql += "FAIRDBTUTPAR"; }
+  sql += "( SEQNO          INT NOT NULL,";
+  sql += "  ROW_ID         INT NOT NULL,";
+  sql += "  TOPPITCH       DOUBLE,";
+  sql += "  TOPANCHOR      DOUBLE,";
+  sql += "  TOPNRFE        INT,";
+  sql += "  FETYPE         TEXT,";
+  sql += "  primary key(SEQNO,ROW_ID))";
+  return sql;
+}
 
 
 
@@ -118,19 +131,23 @@ void FairDbTutPar::Store(FairDbOutTableBuffer& res_out,
 
 void FairDbTutPar::fill(UInt_t rid)
 {
+  // Get Reader Meta Class
+  fParam_Reader=GetParamReader();
 
+  // Define a Context
   ValTimeStamp ts(rid);
   ValCondition context(Detector::kGfi,DataType::kData,ts);
-  FairDbReader<FairDbTutPar> rsCal(context, GetVersion());
-  Int_t numRows = rsCal.GetNumRows();
 
+  // Activate reading for this Context
+  fParam_Reader->Activate(context, GetVersion());
 
   // By default use the latest row entry
   // (Other rows would correspond to outdated versions)
+  Int_t numRows = fParam_Reader->GetNumRows();
   if ( numRows > 1 ) { numRows = 1; }
 
   for (int i = 0; i < numRows; ++i) {
-    FairDbTutPar* cgd = (FairDbTutPar*) rsCal.GetRow(i);
+    FairDbTutPar* cgd = (FairDbTutPar*) fParam_Reader->GetRow(i);
     if (!cgd) { continue; }
     fTopPitch = cgd->GetTopPitch();
     fTopAnchor =  cgd->GetTopAnchor();
@@ -143,33 +160,30 @@ void FairDbTutPar::fill(UInt_t rid)
 
 void FairDbTutPar::store(UInt_t rid)
 {
-  // In this example we are fixing the database entry point. In the future
-  // a variable entry can be set via the runtime DB directly.
-  Int_t dbEntry = 0;
+
+  // Boolean IO test variable
   Bool_t fail= kFALSE;
 
-  FairDbConnectionPool* fMultConn = FairDbTableInterfaceStore::Instance().fConnectionPool;
-  auto_ptr<FairDbStatement> stmtDbn(fMultConn->CreateStatement(dbEntry));
+  // Create a unique statement on choosen DB entry
+  auto_ptr<FairDbStatement> stmtDbn(fMultConn->CreateStatement(GetDbEntry()));
   if ( ! stmtDbn.get() ) {
-    cout << "-E-  FairDbTutPar::Store()  Cannot create statement for Database_id: " << dbEntry
+    cout << "-E-  FairDbTutPar::Store()  Cannot create statement for Database_id: " << GetDbEntry()
          << "\n    Please check the FAIRDB_TSQL_* environment.  Quitting ... " << endl;
     exit(1);
   }
 
-  // The definition of FairDbTutPar is centralised in the FairDbTutPar class.
-  // The corresponding SQL is executed as follows:
+  // Check if for this DB entry the table already exists.
+  // If not call the corresponding Table Definition Function
   std::vector<std::string> sql_cmds;
   TString atr(GetName());
   atr.ToUpper();
 
-  // Check if for this connection entry the table already exists.
-  // If not call the Class Table Definition function
-  if (! fMultConn->GetConnection(dbEntry)->TableExists("FAIRDBTUTPAR") ) {
+  if (! fMultConn->GetConnection(GetDbEntry())->TableExists("FAIRDBTUTPAR") ) {
     sql_cmds.push_back(FairDb::GetValDefinition("FAIRDBTUTPAR").Data());
     sql_cmds.push_back(FairDbTutPar::GetTableDefinition());
   }
 
-  // Now execute the assemble list of SQL commands.
+  // Packed SQL commands executed internally via SQL processor
   std::vector<std::string>::iterator itr(sql_cmds.begin()), itrEnd(sql_cmds.end());
   while( itr != itrEnd ) {
     std::string& sql_cmd(*itr++);
@@ -183,17 +197,45 @@ void FairDbTutPar::store(UInt_t rid)
 
   // Refresh list of tables in connected database
   // for the choosen DB entry
-  fMultConn->GetConnection(dbEntry)->SetTableExists();
+  fMultConn->GetConnection(GetDbEntry())->SetTableExists();
 
-  FairDbWriter<FairDbTutPar>  aW(this->GetValInterval(rid),
-                                 GetAggregateNo(), // Composite or Simple IO
-                                 GetVersion(),  // Parameter version ( Set via the Container Factory)
-                                 ValTimeStamp(0,0),0,"test parameter", "FAIRDBTUTPAR");
-  aW.SetDbNo(dbEntry);
-  aW.SetLogComment("Test Parameter");
-  aW << (*this);
-  if ( ! aW.Close() ) {
+  // Writer Meta-Class Instance
+  fParam_Writer = GetParamWriter();
+
+
+  // Activate Writer Meta-Class with the proper
+  // Validity Time Interval (run_id encapsulated)
+
+  // Writer Activate() arguments list
+  //                      <Arguments>                   <Type>                  <Comments>
+  //
+  //                      Validity Interval,            ValInterval
+  //                      Composition      ,            Int_t                   set via cont. factory
+  //                      Version          ,            Int_t                   set via cont. factory
+  //                      DbEntry          ,            Int_t                   set via cont. factory
+  //                      LogTitle         ,            std::string             set via cont. factory
+
+  fParam_Writer->Activate(GetValInterval(rid),GetComboNo(), GetVersion(),GetDbEntry(),GetLogTitle());
+
+  // Object to Table mapping
+  *fParam_Writer<< (*this);
+
+  // Check for eventual IO problems
+  if ( !fParam_Writer->Close() ) {
     fail = true;
     cout << "-E- FairDbTutPar::Store() ******** Cannot do IO on class: " << GetName() <<  endl;
   }
+
 }
+
+void FairDbTutPar::Print()
+{
+  std::cout<<"   Tutorial SQL  Parameters:"<<std::endl;
+  std::cout<<"   Top Pitch    = "<<fTopPitch<<std::endl;
+  std::cout<<"   Top Anchor   = "<<fTopAnchor<< std::endl;
+  std::cout<<"   Nr of Frontends (Top Side)   = "<<fTopNrFE<<std::endl;
+  std::cout<<"   Frontend type name is        = "<<fFeType<<std::endl;
+}
+
+
+
