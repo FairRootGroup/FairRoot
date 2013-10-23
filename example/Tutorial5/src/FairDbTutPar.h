43ec6526648c3f958fdc0f9bd849c472a40c638b
Denis Bertini <D.Bertini@gsi.de>
2013-10-23 12:44:47 +0000

    Meta-Data Memory managment added
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22376 0381ead4-6506-0410-b988-94b70fbc4730

--- example/Tutorial5/src/FairDbTutPar.h
+++ example/Tutorial5/src/FairDbTutPar.h
@@ -12,7 +12,14 @@
 
 #include "Rtypes.h"                     // for Double_t, Int_t, UInt_t, etc
 
+#include <iostream>                     // for operator<<, basic_ostream, etc
 #include <string>                       // for string
+
+#include "FairDbWriter.h"
+#include "FairDbReader.h"
+
+
+using namespace std;
 
 class FairDbOutTableBuffer;
 class FairDbResultPool;
@@ -20,13 +27,14 @@ class FairDbObjTableMap;
 class FairDbValRecord;
 class FairParamList;
 
+
 class FairDbTutPar : public FairParGenericSet
 {
   public :
     FairDbTutPar (const char* name="FairDbTutPar",
                   const char* title="Tutorial  parameter",
                   const char* context="TestDefaultContext");
-    virtual ~FairDbTutPar(void) {};
+    virtual ~FairDbTutPar(void);
     void clear(void);
     void   putParams(FairParamList* list);
     Bool_t getParams(FairParamList* list);
@@ -44,17 +52,19 @@ class FairDbTutPar : public FairParGenericSet
     void SetFeType(std::string x)     {fFeType = x;}
 
 
-    // SQL descriptors
+    // Add-ons: SQL descriptors for the parameter class
     virtual std::string GetTableDefinition(const char* Name = 0);
+
     virtual FairDbObjTableMap* CreateObjTableMap() const {
       return new FairDbTutPar();
     }
 
-    // I/O  member functions
+    // Atomic IO (intrinsic)
     virtual void Fill(FairDbResultPool& res_in,
                       const FairDbValRecord* valrec);
     virtual void Store(FairDbOutTableBuffer& res_out,
                        const FairDbValRecord* valrec) const;
+    // Global IO using run_id
     virtual void fill(UInt_t rid);
     virtual void store(UInt_t rid);
 
@@ -65,16 +75,46 @@ class FairDbTutPar : public FairParGenericSet
                           ValTimeStamp(rid));
     }
 
+    // SQL-IO Meta-Class Getters
+    FairDbReader<FairDbTutPar>* GetParamReader();
+    FairDbWriter<FairDbTutPar>* GetParamWriter();
 
   private:
-    // Example Strip Parameters
-    Double_t fTopPitch;     // Strip pitch on top wafer side
-    Double_t fTopAnchor;    // Anchor point of top strip#0
-    Int_t    fTopNrFE;      // Number of FE attached to top wafer side
-    std::string    fFeType;       // Frontend type name
+    // Strip Data Parameters
+    Double_t fTopPitch;         // Strip pitch on top wafer side
+    Double_t fTopAnchor;        // Anchor point of top strip#0
+    Int_t    fTopNrFE;          // Number of FE attached to top wafer side
+    std::string    fFeType;     // Frontend type name
 
-    ClassDef(FairDbTutPar,1); //
+
+    // Database Pool Index
+    Int_t fDbEntry; //!
+    // Parameter Container SQL Writer Meta-Class
+    FairDbWriter<FairDbTutPar>* fParam_Writer; //!
+    // Parameter Container SQL Writer Meta-Class
+    FairDbReader<FairDbTutPar>* fParam_Reader; //!
+    // Connection Pool
+    FairDbConnectionPool* fMultConn;  //!
+
+    ClassDef(FairDbTutPar,1); // Tutorial5 FairDbTutPar Parameter Container example
 };
 
 #endif /* !FAIRDBTUTPAR_H*/
 
+inline  FairDbReader<FairDbTutPar>* FairDbTutPar::GetParamReader()
+{
+  if (fParam_Reader) { return fParam_Reader; }
+  else {
+    fParam_Reader = new  FairDbReader<FairDbTutPar>();
+    return fParam_Reader;
+  }
+}
+
+inline FairDbWriter<FairDbTutPar>* FairDbTutPar::GetParamWriter()
+{
+  if (fParam_Writer) { return fParam_Writer; }
+  else {
+    fParam_Writer = new  FairDbWriter<FairDbTutPar>();
+    return fParam_Writer;
+  }
+}
