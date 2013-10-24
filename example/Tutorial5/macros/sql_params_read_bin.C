87446c1e9de4b93e4aa6a1bc4ca91a944e0d68b8
Denis Bertini <D.Bertini@gsi.de>
2013-10-24 14:03:58 +0000

    Adding Cached Parameter Ownership
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22434 0381ead4-6506-0410-b988-94b70fbc4730

--- example/Tutorial5/macros/sql_params_read_bin.C
+++ example/Tutorial5/macros/sql_params_read_bin.C
@@ -29,7 +29,7 @@ Int_t  sql_params_read_bin()
   // Create a dummy runID using date in UTC from which
   // corresponding  parameters will be initialised
 
-  ValTimeStamp tStamp(2013,10,23,12,28,00);
+  ValTimeStamp tStamp(2013,10,24,09,55,00);
   UInt_t runId = tStamp.GetSec();
   cout << "-I- looking for parameters at runID# " << runId << endl;
   cout << "-I- corresponding  time in runID (UTC) " << tStamp.AsString("c") << endl;
@@ -39,15 +39,20 @@ Int_t  sql_params_read_bin()
   db->initContainers(runId);
 
 
-  // Get the container after initialisation
+ 
+
+  cout << endl;
+  cout << "-I- Initialisation from SQL Database:" << endl;
+  cout << endl;
+
+ // Get the container after initialisation
   // from the RuntimeDB
   FairDbTutParBin* pp1 = (FairDbTutParBin*)(db->getContainer("TUTParBin"));
   pp1->Print();
 
   cout << endl;
-  cout << "\n  -I- SQLDB: RuntimeDB::init from SQL Database done.... \n" << endl;
-  cout << endl;
 
 
+  if (db) delete db;
   return 0;
 }
