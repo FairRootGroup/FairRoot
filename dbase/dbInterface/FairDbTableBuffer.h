14a40e00c4b4229baeead3e4fec609aab915289d
Denis Bertini <D.Bertini@gsi.de>
2013-11-13 11:51:33 +0000

    add sql io features
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@22809 0381ead4-6506-0410-b988-94b70fbc4730

--- dbase/dbInterface/FairDbTableBuffer.h
+++ dbase/dbInterface/FairDbTableBuffer.h
@@ -10,23 +10,30 @@ class FairDbTableMetaData;
 
 class FairDbTableBuffer
 {
-
   public:
 
     FairDbTableBuffer(const FairDbTableMetaData* metaData);
     virtual ~FairDbTableBuffer();
 
-    virtual
-    const FairDbFieldType& ColFieldType(UInt_t col) const;
+    virtual const FairDbFieldType& ColFieldType(UInt_t col) const;
+    virtual const FairDbFieldType& GetColumnFieldType(UInt_t col) const {return ColFieldType(col);}
     virtual       std::string ColName(UInt_t col) const;
-    virtual
-    const FairDbFieldType& CurColFieldType() const;
+    virtual       std::string GetColumnName(UInt_t col) const {return ColName(col);}
+    virtual const FairDbFieldType& CurColFieldType() const;
+    virtual const FairDbFieldType& GetCurrrentColumnFieldType() const {return CurColFieldType();}
     virtual       std::string CurColName() const;
-    virtual       UInt_t CurColNum() const { return fCurCol;}
+    virtual       std::string GetCurrentColumnName() const {return CurColName();}
+    virtual       UInt_t CurColNum() const {return fCurCol;}
+    virtual       UInt_t GetCurrentColumnNo() const {return fCurCol;}
     Bool_t HasRowCounter() const { return fHasRowCounter; }
+    Bool_t HasRowId() const { return fHasRowCounter; }
     virtual       UInt_t NumCols() const;
+    virtual       UInt_t GetNumOfColumns() const {return NumCols();}
     virtual       std::string TableName() const;
+    virtual       std::string GetTableName() const {return TableName();}
     virtual       std::string TableNameTc() const;
+    virtual       std::string GetTableNameTc() const {return TableNameTc();}
+
     virtual const FairDbTableMetaData*
     MetaData() const { return fMetaData; }
 
@@ -51,7 +58,6 @@ class FairDbTableBuffer
     FairDbTableBuffer operator=(const FairDbTableBuffer&);
 
     ClassDef(FairDbTableBuffer,0)     // Table Row Buffer
-
 };
 
 #endif  // FAIRDBTABLEBUFFER_H
