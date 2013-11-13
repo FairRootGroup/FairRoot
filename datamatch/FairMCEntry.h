3053cc3c6ecb39c53f12cc1e6491d8242826c438
Mohammad Al-Turany <m.al-turany@gsi.de>
2013-11-13 14:28:11 +0000

    correct print method again
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/tags/v-13.10@22820 0381ead4-6506-0410-b988-94b70fbc4730

--- datamatch/FairMCEntry.h
+++ datamatch/FairMCEntry.h
@@ -51,7 +51,7 @@ class FairMCEntry : public FairMultiLinkedData
     }
 
     friend std::ostream& operator<< (std::ostream& out, const FairMCEntry& link) {
-      ((FairMultiLinkedData)link).print(out);
+      ((FairMultiLinkedData)link).Print(out);
       return out;
     }
 
