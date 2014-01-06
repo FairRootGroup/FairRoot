369722ca27c824422a58bb385e31cc75e6d01031
Denis Bertini <D.Bertini@gsi.de>
2014-01-06 10:37:21 +0000

    PostGresSQL Support added
    git-svn-id: https://subversion.gsi.de/fairroot/fairbase/trunk@23387 0381ead4-6506-0410-b988-94b70fbc4730

--- dbase/dbInterface/FairDbTableMetaData.cxx
+++ dbase/dbInterface/FairDbTableMetaData.cxx
@@ -55,6 +55,10 @@ void FairDbTableMetaData::ExpandTo(UInt_t colNum)
 const  FairDbTableMetaData::ColumnAttributes&  FairDbTableMetaData::GetAttributes(Int_t colNum) const
 {
   if ( colNum > 0 && colNum <= (signed) fNumCols ) { return fColAttr[colNum-1]; }
+
+  //  cout << "-I- FairDbTableMetaData::GetAttributes : return dummy attributes: col#"
+  //     <<  colNum << " : " << fNumCols << endl;
+
   fgDummy.SetDefault();
   return fgDummy;
 }
@@ -77,22 +81,25 @@ string FairDbTableMetaData::GetToken(const char*& strPtr)
 FairDbTableMetaData::ColumnAttributes& FairDbTableMetaData::SetAttributes(Int_t colNum)
 {
   this->ExpandTo(colNum);
+  //cout << "-I-  FairDbTableMetaData::SetAttributes  Expand to:" << colNum << " : " << fNumCols << endl;
   return const_cast<FairDbTableMetaData::ColumnAttributes&>(this->GetAttributes(colNum));
 }
 
 void FairDbTableMetaData::SetColFieldType(const FairDbFieldType& fieldType,
     Int_t colNum)
 {
+  //cout << "-I-  FairDbTableMetaData::SetColFieldType:" << fieldType.GetConcept() << " : " << colNum << endl;
   ColumnAttributes&  attrib(this->SetAttributes(colNum));
   UInt_t concept     = fieldType.GetConcept();
   attrib.Type        = fieldType;
   attrib.MustDelimit = concept == FairDb::kString || concept == FairDb::kDate || concept == FairDb::kChar;
   attrib.Concept     = concept;
-
 }
 
-void FairDbTableMetaData::SetFromSql(const string& sql)
+void FairDbTableMetaData::SetFromSql(const string& sql, FairDb::DbTypes dbType_target)
 {
+
+  //cout << "-I- <<<<<<  FairDbTableMetaData::SetFromSQL()  called  <<<<<<<" << endl;
 
   TString SqlUpper(sql);
   SqlUpper.ToUpper();
@@ -135,9 +142,13 @@ void FairDbTableMetaData::SetFromSql(const string& sql)
     this->SetColName(name,col);
     this->SetColIsNullable(col);
 
+    // Get type and init precision
     string type = FairDbTableMetaData::GetToken(strPtr);
     int precision = 0;
+
     delim = FairDbTableMetaData::GetToken(strPtr);
+
+    // Check if precision is specified then store it
     if ( delim == "(" ) {
       delim = FairDbTableMetaData::GetToken(strPtr);
       istringstream is(delim);
@@ -145,16 +156,20 @@ void FairDbTableMetaData::SetFromSql(const string& sql)
       delim = FairDbTableMetaData::GetToken(strPtr);
       delim = FairDbTableMetaData::GetToken(strPtr);
     }
+
+
     FairDbFieldType ft(type,precision);
+
     this->SetColFieldType(ft,col);
-    DBLOG("FairDb",FairDbLog::kWarning) << "  Column: " << col << " name " << this->ColName(col)
-                                        << " type " << this->ColFieldType(col).AsString()
-                                        << " precision " << precision << endl;
+    DBLOG("FairDb",FairDbLog::kInfo) << "  Column: " << col << " name " << this->ColName(col)
+                                     << " type " << this->ColFieldType(col).AsString()
+                                     << " precision " << precision << endl;
+
 
 //  Collect optional qualifiers.
-
     while ( delim != ","  &&  delim != ")" ) {
       string opt2 = FairDbTableMetaData::GetToken(strPtr);
+      // cout << "delim " << delim << " opt2 " << opt2 << endl;
       if ( delim == "NOT" && opt2 == "NULL") {
         this->SetColIsNullable(col,false);
         delim = FairDbTableMetaData::GetToken(strPtr);
@@ -165,24 +180,26 @@ void FairDbTableMetaData::SetFromSql(const string& sql)
       } else if ( delim == "UNSIGNED") {
         delim = opt2;
       } else {
-        DBLOG("FairDb",FairDbLog::kWarning) <<"Column: " << col << " name " << name << " type " << ft.AsString()
-                                            << " ignoring unknown option: " << delim << endl;
+        DBLOG("FairDb",FairDbLog::kInfo) <<"Column: " << col << " name " << name << " type " << ft.AsString()
+                                         << " ignoring unknown option: " << delim << endl;
         delim = opt2;
       }
-    }
-  }
+    } // !(while 2)
+
+  } //! (while 1 )
 
 }
 
 
 string FairDbTableMetaData::Sql(FairDb::DbTypes dbType_target) const
 {
+  //cout << "-I- <<<<<<  FairDbTableMetaData::SQL()  called  <<<<<<<" << endl;
 
   Bool_t mainTable = fTableName.substr(fTableName.size()-3,3) != "VAL";
   bool hasRowCounter = false;
-
   string tableName = fTableName;
   FairDbString sql;
+
   sql.GetString() = "";
   sql << "create table " << tableName << "(";
 
@@ -192,17 +209,22 @@ string FairDbTableMetaData::Sql(FairDb::DbTypes dbType_target) const
 
     if ( this->ColName(i) == "ROW_ID" ) { hasRowCounter = true; }
     sql << this->ColName(i) << " " ;
-    sql << this->ColFieldType(i).AsSQLString(dbType_target);
+    // Change Primary key definition in PgSQL
+    if(   dbType_target == FairDb::kPostgreSQL
+          && this->ColName(i) == "SEQNO"
+          && ! mainTable ) {
+      sql << " bigserial primary key" ;
+    } else {
+      sql << this->ColFieldType(i).AsSQLString(dbType_target);
+      if(    dbType_target == FairDb::kMySQL
+             && this->ColName(i) == "SEQNO"
+             && ! mainTable ) { sql << " not null primary key" ; }
 
-    if(    dbType_target == FairDb::kMySQL
-           && this->ColName(i) == "SEQNO"
-           && ! mainTable ) { sql << " not null primary key" ; }
-
-    else if( ! this->ColIsNullable(i)
-             || this->ColName(i) == "SEQNO"
-             || this->ColName(i) == "ROW_ID"
-           ) { sql << " not null" ; }
-
+      else if( ! this->ColIsNullable(i)
+               || this->ColName(i) == "SEQNO"
+               || this->ColName(i) == "ROW_ID"
+             ) { sql << " not null" ; }
+    }
     if (i < numCols) { sql << ", "; }
 
   }
@@ -224,8 +246,30 @@ string FairDbTableMetaData::Sql(FairDb::DbTypes dbType_target) const
     sql << ")";
   }
 
+
+// Deal with PostgreSQL
+  else if (dbType_target == FairDb::kPostgreSQL) {
+    if ( mainTable ) {
+      if ( hasRowCounter ) { sql << ", primary key (SEQNO,ROW_ID)"; }
+    }
+
+    sql << ") ";
+
+    if ( mainTable) {
+      if ( ! hasRowCounter )
+        sql << "; create index " << tableName << "_idx  on "
+            << tableName << "(seqno)";
+    } else {
+      sql << "; create index " << tableName << "VAL_idx2 on "
+          << tableName << "(TIMESTART);";
+      sql << "; create index " << tableName << "VAL_idx3 on "
+          << tableName << "(TIMEEND);";
+    }
+  }
+
 // ORACLE specs
-  else {
+  else if (dbType_target == FairDb::kOracle) {
+
     if ( mainTable ) {
       sql << ", constraint FK_" << fTableName << " foreign key(SEQNO) references "
           << fTableName << "VAL(SEQNO)";
