473c0548fc521d1a765a7eab5647b766d78355cf
Florian Uhlig <f.uhlig@gsi.de>
2014-03-01 13:09:16 +0100

    Update script to clean the complete source tree. Running the script will remove all directories which are unpacked from source packages and clean the two directories which are imported via svn.

--- make_clean.sh
+++ make_clean.sh
@@ -4,20 +4,20 @@
 # installation of the external packages into a separate installation directory
 
 export SIMPATH=$PWD
-export PATH=$PATH:$SIMPATH/tools/root/bin
+
+source scripts/package_versions.sh
 
 # Geant4 - remove complete buid directory
-cd $SIMPATH/transport/geant4
-if [ -d build ]; then
-  rm -rf build
+cd $SIMPATH/transport
+if [ -d geant4 ]; then
+  rm -rf geant4
 fi
 
 # Geant3 - remove lib and temporary directory
-cd $SIMPATH/transport/geant3
-if [ -d lib ]; then
-  rm -rf lib
+cd $SIMPATH/transport
+if [ -d geant3 ]; then
+  rm -rf geant3
 fi
-rm -rf tgt_*
 
 # vgm - remove build directory
 cd $SIMPATH/transport/vgm
@@ -26,32 +26,109 @@ if [ -d build_cmake ]; then
 fi
 
 # geant4_vmc - remove tmp, lib and include directories
-cd $SIMPATH/transport/geant4_vmc
-if [ -d tmp ]; then
-  rm -rf tmp
-fi
-if [ -d lib ]; then
-  rm -rf lib
-fi
-if [ -d include ]; then
-  rm -rf include
+# vgm - remove build directory
+cd $SIMPATH/transport
+if [ -d geant4_vmc ]; then
+  rm -rf geant4_vmc
 fi
 
 # Pluto - remove all unneeded files
-cd $SIMPATH/generators/Pluto.5.37
-make clean
-make pluginclean
+cd $SIMPATH/generators
+if [ -d $PLUTOVERSION ]; then
+  rm -rf $PLUTOVERSION
+fi
+if [ -h pluto ]; then
+  rm -rf pluto
+fi
 
 # Pythia6 - remove build directory
-cd $SIMPATH/generators/pythia6
-if [ -d build ]; then
-  rm -rf build
+cd $SIMPATH/generators
+if [ -d pythia6 ]; then
+  rm -rf pythia6
 fi
 
 # Pythia8 - remove all unneeded files
-cd $SIMPATH/generators/pythia8
-make distclean
+cd $SIMPATH/generators
+if [ -d $PYTHIA8VERSION ]; then
+  rm -rf $PYTHIA8VERSION
+fi
+if [ -h pythia8 ]; then
+  rm -rf pythia8
+fi
+
+# HepMC - remove all unneeded files
+cd $SIMPATH/generators
+if [ -d HepMC-$HEPMCVERSION ]; then
+  rm -rf HepMC-$HEPMCVERSION
+fi
+if [ -d build_HepMC ]; then
+  rm -rf build_HepMC
+fi
+if [ -h HepMC ]; then
+  rm -rf HepMC
+fi
+
+#  Boost remove all unneeded files
+cd $SIMPATH/basics
+if [ -d $BOOSTVERSION ]; then
+  rm -rf $BOOSTVERSION
+fi
+if [ -h boost ]; then
+  rm -rf boost
+fi
+
+#  CMake remove all unneeded files
+cd $SIMPATH/basics
+if [ -d $CMAKEVERSION ]; then
+  rm -rf $CMAKEVERSION
+fi
+if [ -h cmake ]; then
+  rm -rf  cmake
+fi
+
+#  GSL remove all unneeded files
+cd $SIMPATH/basics
+if [ -d $GSLVERSION ]; then
+  rm -rf $GSLVERSION
+fi
+if [ -h gsl ]; then
+  rm -rf gsl
+fi
+
+#  GTest remove all unneeded files
+cd $SIMPATH/basics
+if [ -d $GTESTVERSION ]; then
+  rm -rf $GTESTVERSION
+fi
+if [ -h gtest ]; then
+  rm -rf gtest
+fi
+
+#  ZeroMQ remove all unneeded files
+cd $SIMPATH/basics
+if [ -d zeromq-$ZEROMQVERSION ]; then
+  rm -rf zeromq-$ZEROMQVERSION
+fi
+if [ -h zeromq ]; then
+  rm -rf zeromq
+fi
+
+# millepede 
+cd $SIMPATH/basics/MillepedeII
+if [ -d  build ]; then
+  rm -rf build
+fi
+rm  pede
+rm *.mod
+rm *.o  
+
 
 # Root - remove all unneeded files
-cd $SIMPATH/tools/root
-make distclean
+cd $SIMPATH/tools
+if [ -d root ]; then
+  rm -rf root
+fi
+
+
+set +xv
+exit
