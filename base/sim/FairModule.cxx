bb15618d460bfd61796b1a89a331bc8a0468d474
Florian Uhlig <f.uhlig@gsi.de>
2014-03-12 11:13:44 +0100

    Create dummy material if needed. In newer versions of ROOT (issue was with 5.34/17) also TGeoVolumeAssembly has a medium assigned. Since this medium is neither existing in the GeoManager at time it is needed, nor it is defined in the ASCII media files, it is create on the fly when needed. The same procedure as in TGeoVolume::CreateDummyMedium() is used. This change works at least with ROOT version 5.34/13 and 5.34/17. These are the version used in dec13 release of FairSoft and the master branch in the git repository at this time. The commit fixes #4.

--- base/sim/FairModule.cxx
+++ base/sim/FairModule.cxx
@@ -329,6 +329,7 @@ void FairModule::ConstructRootGeometry()
     /**Now we can add the node to the existing cave */
     Cave->AddNode(v1,0, M);
     /** correction from O. Merle: in case of a TGeoVolume (v1) set the material properly */
+
     AssignMediumAtImport(v1);
     /** now go through the herachy and set the materials properly, this is important becase the CAD converter
      *  produce TGeoVolumes with materials that have only names and no properties
@@ -421,7 +422,31 @@ void FairModule::AssignMediumAtImport(TGeoVolume* v)
   FairGeoBuilder* geobuild  = FairGeoLoader::Instance()->getGeoBuilder();
 
   TGeoMedium* med1=v->GetMedium();
+
+
   if(med1) {
+    // In newer ROOT version also a TGeoVolumeAssembly has a material and medium.
+    // This medium is called dummy and is automatically set when the geometry is constructed.
+    // Since this material and medium is neither in the TGeoManager (at this point) nor in our
+    // ASCII file we have to create it the same way it is done in TGeoVolume::CreateDummyMedium()
+    // In the end the new medium and material has to be added to the TGeomanager, because this is
+    // not done automatically when using the default constructor. For all other constructors the
+    // newly created medium or material is added to the TGeomanger.
+    // Create the medium and material only the first time.
+    TString medName = (TString)(med1->GetName());
+    if ( medName.EqualTo("dummy") && NULL == gGeoManager->GetMedium(medName) ) {
+
+      TGeoMaterial *dummyMaterial = new TGeoMaterial();
+      dummyMaterial->SetName("dummy");
+
+      TGeoMedium* dummyMedium = new TGeoMedium();
+      dummyMedium->SetName("dummy");
+      dummyMedium->SetMaterial(dummyMaterial);
+
+      gGeoManager->GetListOfMedia()->Add(dummyMedium);
+      gGeoManager->AddMaterial(dummyMaterial);
+    }
+
     TGeoMaterial* mat1=v->GetMaterial();
     TGeoMaterial* newMat = gGeoManager->GetMaterial(mat1->GetName());
     if( newMat==0) {
