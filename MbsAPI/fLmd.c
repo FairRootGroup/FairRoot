d0c7e4057002741a40acd7ad8f83afe46ba1e2ef
Mohammad Al-Turany <m.al-turany@gsi.de>
2014-05-30 15:14:36 +0200

    Remove non-used files from MBSAPI and take the freely available source from https://aur.archlinux.org/packages/mbseventapi/

--- MbsAPI/fLmd.c
+++ MbsAPI/fLmd.c
@@ -1,16 +1,3 @@
-// $Id: fLmd.c 595 2010-03-09 06:51:15Z adamczew $
-//-----------------------------------------------------------------------
-//       The GSI Online Offline Object Oriented (Go4) Project
-//         Experiment Data Processing at EE department, GSI
-//-----------------------------------------------------------------------
-// Copyright (C) 2000- GSI Helmholtzzentrum f�r Schwerionenforschung GmbH
-//                     Planckstr. 1, 64291 Darmstadt, Germany
-// Contact:            http://go4.gsi.de
-//-----------------------------------------------------------------------
-// This software can be used under the license agreements as stated
-// in Go4License.txt file which is part of the distribution.
-//-----------------------------------------------------------------------
-
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
--- MbsAPI/typedefs.h
+++ MbsAPI/typedefs.h
@@ -1,16 +1,3 @@
-// $Id: typedefs.h 595 2010-03-09 06:51:15Z adamczew $
-//-----------------------------------------------------------------------
-//       The GSI Online Offline Object Oriented (Go4) Project
-//         Experiment Data Processing at EE department, GSI
-//-----------------------------------------------------------------------
-// Copyright (C) 2000- GSI Helmholtzzentrum für Schwerionenforschung GmbH
-//                     Planckstr. 1, 64291 Darmstadt, Germany
-// Contact:            http://go4.gsi.de
-//-----------------------------------------------------------------------
-// This software can be used under the license agreements as stated
-// in Go4License.txt file which is part of the distribution.
-//-----------------------------------------------------------------------
-
 /* This central include file defines data types for all platforms */
 
 #ifndef TYPEDEF_H
