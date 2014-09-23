ba31286db790cedd92ff832183e0f626c71892ea
Florian Uhlig <f.uhlig@gsi.de>
2014-09-23 16:12:48 +0200

    Extract teh version info from git, since we don't have any longer a subversion repository.

--- cmake/modules/GenerateVersionInfo.cmake
+++ cmake/modules/GenerateVersionInfo.cmake
@@ -8,28 +8,31 @@
  # Configure FairVersion.h
  # ------------------------------
 
- Find_Package(Subversion)
+ Find_Package(Git)
  
- If(Subversion_FOUND AND EXISTS ${SOURCE_DIR}/.svn/ )
-      Subversion_WC_INFO(${SOURCE_DIR} PROJECT)
-      Set(FAIRROOT_SVN_REVISION ${PROJECT_WC_REVISION})
-      Set(FAIRROOT_SVN_BRANCH ${PROJECT_WC_URL})
-      Set(FAIRROOT_SVN_DATE ${PROJECT_WC_LAST_CHANGED_DATE})
-#     String(REGEX MATCH "[^/]+$" FAIRROOT_SVN_BRANCH ${FAIRROOT_SVN_BRANCH})
-      Message(STATUS "FairRoot Revision - ${FAIRROOT_SVN_REVISION} Branch - ${FAIRROOT_SVN_BRANCH}")
+ If(GIT_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/.git" )
+     Execute_Process(COMMAND ${GIT_OMMAND} describe
+                     OUTPUT_VARIABLE FAIRROOT_GIT_VERSION
+                     OUTPUT_STRIP_TRAILING_WHITESPACE
+                    )
+     Execute_Process(COMMAND ${GIT_OMMAND} log -1 --format="%cd"
+                     OUTPUT_VARIABLE FAIRROOT_GIT_DATE
+                     OUTPUT_STRIP_TRAILING_WHITESPACE
+                    )
+      Message(STATUS "FairRoot Version - ${FAIRROOT_GIT_VERSION} from - ${FAIRROOT_GIT_DATE}")
       if(FAIRROOT)
           Configure_File(${FAIRROOT}/scripts/FairVersion.h.tmp ${BINARY_DIR}/FairVersion.h @ONLY)
       else(FAIRROOT)  
           Configure_File(${SOURCE_DIR}/cmake/scripts/FairVersion.h.tmp ${BINARY_DIR}/FairVersion.h @ONLY)
       endif(FAIRROOT)
   
- Else(Subversion_FOUND AND EXISTS ${SOURCE_DIR}/.svn/ )
+ Else()
       if(FAIRROOT)
           Configure_File(${FAIRROOT}/scripts/FairVersion.h.default ${BINARY_DIR}/FairVersion.h COPYONLY)
       else(FAIRROOT) 
           Configure_File(${SOURCE_DIR}/cmake/scripts/FairVersion.h.default ${BINARY_DIR}/FairVersion.h COPYONLY)
       endif(FAIRROOT)
- EndIf(Subversion_FOUND AND EXISTS ${SOURCE_DIR}/.svn/ )
+ EndIf()
 
  Execute_Process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
        ${BINARY_DIR}/FairVersion.h ${INCLUDE_OUTPUT_DIRECTORY}/FairVersion.h)
