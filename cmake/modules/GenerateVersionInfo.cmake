3bad0306f4bfad6d80eb3d518b020530b41447ff
Florian Uhlig <f.uhlig@gsi.de>
2014-09-23 17:18:49 +0200

    Generate FairVersion.h in the correct way and with the correct version. During installation procedure the file is copied to the include directory.

--- cmake/modules/GenerateVersionInfo.cmake
+++ cmake/modules/GenerateVersionInfo.cmake
@@ -9,31 +9,30 @@
  # ------------------------------
 
  Find_Package(Git)
- 
- If(GIT_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/.git" )
-     Execute_Process(COMMAND ${GIT_OMMAND} describe
-                     OUTPUT_VARIABLE FAIRROOT_GIT_VERSION
-                     OUTPUT_STRIP_TRAILING_WHITESPACE
-                    )
-     Execute_Process(COMMAND ${GIT_OMMAND} log -1 --format="%cd"
-                     OUTPUT_VARIABLE FAIRROOT_GIT_DATE
-                     OUTPUT_STRIP_TRAILING_WHITESPACE
-                    )
-      Message(STATUS "FairRoot Version - ${FAIRROOT_GIT_VERSION} from - ${FAIRROOT_GIT_DATE}")
-      if(FAIRROOT)
-          Configure_File(${FAIRROOT}/scripts/FairVersion.h.tmp ${BINARY_DIR}/FairVersion.h @ONLY)
-      else(FAIRROOT)  
-          Configure_File(${SOURCE_DIR}/cmake/scripts/FairVersion.h.tmp ${BINARY_DIR}/FairVersion.h @ONLY)
-      endif(FAIRROOT)
+
+ If(GIT_FOUND AND EXISTS "${SOURCE_DIR}/.git")
+   Execute_Process(COMMAND ${GIT_EXECUTABLE} describe
+                   OUTPUT_VARIABLE FAIRROOT_GIT_VERSION
+                   OUTPUT_STRIP_TRAILING_WHITESPACE
+                   WORKING_DIRECTORY ${SOURCE_DIR}
+                  )
+   Execute_Process(COMMAND ${GIT_EXECUTABLE} log -1 --format=%cd
+                   OUTPUT_VARIABLE FAIRROOT_GIT_DATE
+                   OUTPUT_STRIP_TRAILING_WHITESPACE
+                   WORKING_DIRECTORY ${SOURCE_DIR}
+                  )
+   Message(STATUS "FairRoot Version - ${FAIRROOT_GIT_VERSION} from - ${FAIRROOT_GIT_DATE}")
+   if(FAIRROOT)
+     Configure_File(${FAIRROOT}/scripts/FairVersion.h.tmp ${BINARY_DIR}/FairVersion.h @ONLY)
+   else(FAIRROOT)  
+     Configure_File(${SOURCE_DIR}/cmake/scripts/FairVersion.h.tmp ${BINARY_DIR}/FairVersion.h @ONLY)
+   endif(FAIRROOT)
   
  Else()
-      if(FAIRROOT)
-          Configure_File(${FAIRROOT}/scripts/FairVersion.h.default ${BINARY_DIR}/FairVersion.h COPYONLY)
-      else(FAIRROOT) 
-          Configure_File(${SOURCE_DIR}/cmake/scripts/FairVersion.h.default ${BINARY_DIR}/FairVersion.h COPYONLY)
-      endif(FAIRROOT)
+   if(FAIRROOT)
+     Configure_File(${FAIRROOT}/scripts/FairVersion.h.default ${BINARY_DIR}/FairVersion.h COPYONLY)
+   else(FAIRROOT) 
+     Configure_File(${SOURCE_DIR}/cmake/scripts/FairVersion.h.default ${BINARY_DIR}/FairVersion.h COPYONLY)
+   endif(FAIRROOT)
  EndIf()
-
- Execute_Process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
-       ${BINARY_DIR}/FairVersion.h ${INCLUDE_OUTPUT_DIRECTORY}/FairVersion.h)
 
