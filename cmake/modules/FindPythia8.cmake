d58a0013f9abfe693d6bf451e0fb22c3fc93da14
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2014-12-03 12:36:32 +0100

    Implemented optional modular build: - triggered with FAIRROOT_MODULAR_BUILD option - does not require the environment variables SIMPATH and FAIRROOTPATH

--- cmake/modules/FindPythia8.cmake
+++ cmake/modules/FindPythia8.cmake
@@ -12,6 +12,8 @@
 MESSAGE(STATUS "Looking for PYTHIA8 ...")
 
 FIND_PATH(PYTHIA8_INCLUDE_DIR NAMES Pythia.h PATHS
+  ${PYTHIA8_DIR}/include/Pythia8
+  ${AlFa_DIR}/include/Pythia8
   ${SIMPATH}/include/Pythia8
   ${SIMPATH}/include/pythia8  
   ${SIMPATH}/generators/include/pythia8  
@@ -20,6 +22,8 @@ FIND_PATH(PYTHIA8_INCLUDE_DIR NAMES Pythia.h PATHS
 )
 
 FIND_PATH(PYTHIA8_LIB_DIR  NAMES libpythia8.so PATHS
+  ${PYTHIA8_DIR}/lib
+  ${AlFa_DIR}/lib
   ${SIMPATH}/lib
   ${SIMPATH}/generators/lib
   NO_DEFAULT_PATH
