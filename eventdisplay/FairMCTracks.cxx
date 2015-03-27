edd1323ba6d3a52fa313c038e81bbcf561e8697b
Florian Uhlig <f.uhlig@gsi.de>
2015-03-27 13:14:18 +0100

    Correct log messages.
    Correct the log messages. Use FairLogger instead cout. Fixes #54 @15m.

--- eventdisplay/FairMCTracks.cxx
+++ eventdisplay/FairMCTracks.cxx
@@ -13,6 +13,7 @@
 
 #include "FairEventManager.h"           // for FairEventManager
 #include "FairRootManager.h"            // for FairRootManager
+#include "FairLogger.h"
 
 #include "Riosfwd.h"                    // for ostream
 #include "TClonesArray.h"               // for TClonesArray
@@ -29,8 +30,6 @@
 #include <string.h>                     // for NULL, strcmp
 #include <iostream>                     // for operator<<, basic_ostream, etc
 
-using std::cout;
-using std::endl;
 
 // -----   Default constructor   -------------------------------------------
 FairMCTracks::FairMCTracks()
@@ -66,17 +65,17 @@ FairMCTracks::FairMCTracks(const char* name, Int_t iVerbose)
 // -------------------------------------------------------------------------
 InitStatus FairMCTracks::Init()
 {
-  if(fVerbose>1) { cout<<  "FairMCTracks::Init()" << endl; }
+  LOG(DEBUG) <<  "FairMCTracks::Init()" << FairLogger::endl; 
   FairRootManager* fManager = FairRootManager::Instance();
   fTrackList = (TClonesArray*)fManager->GetObject("GeoTracks");
   if(fTrackList==0) {
-    cout << "FairMCPointDraw::Init()  branch " << GetName() << " Not found! Task will be deactivated "<< endl;
+    LOG(ERROR) << "FairMCTracks::Init()  branch " << GetName() << " Not found! Task will be deactivated "<< FairLogger::endl;
     SetActive(kFALSE);
   }
-  if(fVerbose>2) { cout<<  "FairMCTracks::Init() get track list" << fTrackList<< endl; }
-  if(fVerbose>2) { cout<<  "FairMCTracks::Init()  create propagator" << endl; }
+  LOG(DEBUG1) <<  "FairMCTracks::Init() get track list" << fTrackList << FairLogger::endl; 
+  LOG(DEBUG1) <<  "FairMCTracks::Init()  create propagator" << FairLogger::endl; 
   fEventManager =FairEventManager::Instance();
-  if(fVerbose>2) { cout<<  "FairMCTracks::Init() get instance of FairEventManager " << endl; }
+  LOG(DEBUG1) <<  "FairMCTracks::Init() get instance of FairEventManager " << FairLogger::endl; 
   fEvent = "Current Event";
   MinEnergyLimit=fEventManager->GetEvtMinEnergy();
   MaxEnergyLimit=fEventManager->GetEvtMaxEnergy();
@@ -90,23 +89,23 @@ void FairMCTracks::Exec(Option_t* option)
 
   if (IsActive()) {
 
-    if(fVerbose>1) { cout << " FairMCTracks::Exec "<< endl; }
+    LOG(DEBUG1) << " FairMCTracks::Exec "<< FairLogger::endl; 
     TGeoTrack* tr;
     const Double_t* point;
 
     Reset();
 
     for (Int_t i=0; i<fTrackList->GetEntriesFast(); i++)  {
-      if(fVerbose>2) { cout << "FairMCTracks::Exec "<< i << endl; }
+      LOG(DEBUG3) << "FairMCTracks::Exec "<< i << FairLogger::endl; 
       tr=(TGeoTrack*)fTrackList->At(i);
       TParticle* P=(TParticle*)tr->GetParticle();
       PEnergy=P->Energy();
       MinEnergyLimit=TMath::Min(PEnergy,MinEnergyLimit) ;
       MaxEnergyLimit=TMath::Max(PEnergy,MaxEnergyLimit) ;
-      if(fVerbose>2) { cout << "MinEnergyLimit " << MinEnergyLimit << " MaxEnergyLimit " << MaxEnergyLimit << endl; }
+      LOG(DEBUG3)<< "MinEnergyLimit " << MinEnergyLimit << " MaxEnergyLimit " << MaxEnergyLimit << FairLogger::endl; 
       if (fEventManager->IsPriOnly() && P->GetMother(0)>-1) { continue; }
       if(fEventManager->GetCurrentPDG()!=0 && fEventManager->GetCurrentPDG()!= tr->GetPDG()) { continue; }
-      if(fVerbose>2) { cout << "PEnergy " << PEnergy << " Min "  << fEventManager->GetMinEnergy() << " Max " << fEventManager->GetMaxEnergy() <<endl; }
+      LOG(DEBUG3) << "PEnergy " << PEnergy << " Min "  << fEventManager->GetMinEnergy() << " Max " << fEventManager->GetMaxEnergy() << FairLogger::endl; 
       if( (PEnergy<fEventManager->GetMinEnergy()) || (PEnergy >fEventManager->GetMaxEnergy())) { continue; }
 
       Int_t Np=tr->GetNpoints();
@@ -124,16 +123,16 @@ void FairMCTracks::Exec(Option_t* option)
           TEveVector Mom= TEveVector(P->Px(), P->Py(),P->Pz());
           path->fP=Mom;
         }
-        if(fVerbose>3) { cout << "Path marker added " << path << endl; }
+        LOG(DEBUG4) << "Path marker added " << path << FairLogger::endl; 
 
         track->AddPathMark(*path);
 
-        if(fVerbose>3) { cout << "Path marker added " << path << endl; }
+        LOG(DEBUG4) << "Path marker added " << path << FairLogger::endl; 
 
         delete path;
       }
       fTrList->AddElement(track);
-      if(fVerbose>3) { cout << "track added " << track->GetName() << endl; }
+      LOG(DEBUG3) << "track added " << track->GetName() << FairLogger::endl; 
 
     }
     for (Int_t i=0; i<fEveTrList->GetEntriesFast(); i++) {
