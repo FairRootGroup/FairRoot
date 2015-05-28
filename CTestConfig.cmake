b21cdf41a635226db34386880a1c0f8f45aaf0a7
Florian Uhlig <f.uhlig@gsi.de>
2015-05-28 09:31:48 +0200

    Change drop methode and drop address according to the change on the cdash server.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -10,9 +10,9 @@
 set(CTEST_PROJECT_NAME "FairRoot")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "cdash.gsi.de")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=FairRoot")
+set(CTEST_DROP_LOCATION "/submit.php?project=FairRoot")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 set(CTEST_TESTING_TIMEOUT 60)
