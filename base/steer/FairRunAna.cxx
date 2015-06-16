e5b05b02f6f5858b07c98f86b92292c86b5df4f6
Tobias Stockmanns <t.stockmanns@fz-juelich.de>
2015-06-16 14:27:08 +0200

    Update to convert test beam data with FairRoot

--- base/steer/FairRunAna.cxx
+++ base/steer/FairRunAna.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+f *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
  *                                                                              *
  *              This software is distributed under the terms of the             * 
  *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *  
