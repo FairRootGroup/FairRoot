8c6a633c441fc97faefde4d490d2811ff560fa0e
Dmytro Kresan <D.Kresan@gsi.de>
2015-09-08 14:36:02 +0200

    Added Tutorial 8 - MBS data unpacking.

---
+++ example/Tutorial8/Tutorial8LinkDef.h
@@ -0,0 +1,20 @@
+/********************************************************************************
+ *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             * 
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *  
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
+/////////////////////////////// Tutorial 7 LinkDef ///////////////////////////////
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class FairTut8Unpack+;
+#pragma link C++ class FairTut8RawItem+;
+
+#endif
