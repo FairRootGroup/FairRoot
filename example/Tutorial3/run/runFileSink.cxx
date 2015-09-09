83d64a60c29bfd9d18686799c790aafb6f69bff1
Alexey Rybalchenko <alexryba@gmail.com>
2015-09-09 16:57:04 +0200

    Simplify Tutorial3 structure.
    Use same executable for bin/boost/protobuf/root data format,
    configured now via `--data-format <binary/boost/protobuf/tmessage>`
    command line parameter.

--- example/Tutorial3/run/runFileSink.cxx
+++ example/Tutorial3/run/runFileSink.cxx
@@ -6,7 +6,7 @@
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
 /**
- * runFileSinkProto.cxx
+ * runFileSink.cxx
  *
  * @since 2013-01-21
  * @author A. Rybalchenko
@@ -25,23 +25,43 @@
 #include "zeromq/FairMQTransportFactoryZMQ.h"
 #endif
 
+// data format for the task
 #include "FairTestDetectorHit.h"
+
+// binary data format
+#include "FairTestDetectorPayload.h"
+
+// boost data format
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
+// Google Protocol Buffers data format
 #include "FairTestDetectorPayload.pb.h"
+
+// TMessage data format
+#include "TMessage.h"
 
 using namespace std;
 
-typedef FairTestDetectorHit THit;                    // class to serialize/deserialize
+typedef TestDetectorPayload::Hit TPayloadIn; // binary payload
+typedef boost::archive::binary_iarchive TBoostBinPayload; // boost binary format
+typedef boost::archive::text_iarchive TBoostTextPayload;  // boost text format
 typedef TestDetectorProto::HitPayload TProtoPayload; // protobuf payload
-typedef FairMQFileSink<THit, TProtoPayload> TSink;
+
+typedef FairMQFileSink<FairTestDetectorHit, TPayloadIn> TSinkBin;
+typedef FairMQFileSink<FairTestDetectorHit, TBoostBinPayload> TSinkBoost;
+typedef FairMQFileSink<FairTestDetectorHit, TProtoPayload> TSinkProtobuf;
+typedef FairMQFileSink<FairTestDetectorHit, TMessage> TSinkTMessage;
 
 typedef struct DeviceOptions
 {
     DeviceOptions() :
-        id(), ioThreads(0),
+        id(), ioThreads(0), dataFormat(),
         inputSocketType(), inputBufSize(0), inputMethod(), inputAddress() {}
 
     string id;
     int ioThreads;
+    string dataFormat;
     string inputSocketType;
     int inputBufSize;
     string inputMethod;
@@ -58,6 +78,7 @@ inline bool parse_cmd_line(int _argc, char* _argv[], DeviceOptions* _options)
     desc.add_options()
         ("id", bpo::value<string>()->required(), "Device ID")
         ("io-threads", bpo::value<int>()->default_value(1), "Number of I/O threads")
+        ("data-format", bpo::value<string>()->default_value("binary"), "Data format (binary/boost/protobuf/tmessage)")
         ("input-socket-type", bpo::value<string>()->required(), "Input socket type: sub/pull")
         ("input-buff-size", bpo::value<int>()->required(), "Input buffer size in number of messages (ZeroMQ)/bytes(nanomsg)")
         ("input-method", bpo::value<string>()->required(), "Input method: bind/connect")
@@ -67,7 +88,7 @@ inline bool parse_cmd_line(int _argc, char* _argv[], DeviceOptions* _options)
     bpo::variables_map vm;
     bpo::store(bpo::parse_command_line(_argc, _argv, desc), vm);
 
-    if ( vm.count("help") )
+    if (vm.count("help"))
     {
         LOG(INFO) << "FairMQ File Sink" << endl << desc;
         return false;
@@ -75,45 +96,22 @@ inline bool parse_cmd_line(int _argc, char* _argv[], DeviceOptions* _options)
 
     bpo::notify(vm);
 
-    if ( vm.count("id") )
-        _options->id = vm["id"].as<string>();
-
-    if ( vm.count("io-threads") )
-        _options->ioThreads = vm["io-threads"].as<int>();
-
-    if ( vm.count("input-socket-type") )
-        _options->inputSocketType = vm["input-socket-type"].as<string>();
-
-    if ( vm.count("input-buff-size") )
-        _options->inputBufSize = vm["input-buff-size"].as<int>();
-
-    if ( vm.count("input-method") )
-        _options->inputMethod = vm["input-method"].as<string>();
-
-    if ( vm.count("input-address") )
-        _options->inputAddress = vm["input-address"].as<string>();
+    if (vm.count("id"))                { _options->id              = vm["id"].as<string>(); }
+    if (vm.count("io-threads"))        { _options->ioThreads       = vm["io-threads"].as<int>(); }
+    if (vm.count("data-format"))       { _options->dataFormat      = vm["data-format"].as<string>(); }
+    if (vm.count("input-socket-type")) { _options->inputSocketType = vm["input-socket-type"].as<string>(); }
+    if (vm.count("input-buff-size"))   { _options->inputBufSize    = vm["input-buff-size"].as<int>(); }
+    if (vm.count("input-method"))      { _options->inputMethod     = vm["input-method"].as<string>(); }
+    if (vm.count("input-address"))     { _options->inputAddress    = vm["input-address"].as<string>(); }
 
     return true;
 }
 
-int main(int argc, char** argv)
+template<typename T>
+void runFileSink(const DeviceOptions_t& options)
 {
-    TSink filesink;
+    T filesink;
     filesink.CatchSignals();
-
-    DeviceOptions_t options;
-    try
-    {
-        if (!parse_cmd_line(argc, argv, &options))
-            return 0;
-    }
-    catch (exception& e)
-    {
-        LOG(ERROR) << e.what();
-        return 1;
-    }
-
-    LOG(INFO) << "PID: " << getpid();
 
 #ifdef NANOMSG
     FairMQTransportFactory* transportFactory = new FairMQTransportFactoryNN();
@@ -130,20 +128,46 @@ int main(int argc, char** argv)
 
     filesink.fChannels["data-in"].push_back(channel);
 
-    filesink.SetProperty(TSink::Id, options.id);
-    filesink.SetProperty(TSink::NumIoThreads, options.ioThreads);
+    filesink.SetProperty(T::Id, options.id);
+    filesink.SetProperty(T::NumIoThreads, options.ioThreads);
 
-    filesink.ChangeState(TSink::INIT_DEVICE);
-    filesink.WaitForEndOfState(TSink::INIT_DEVICE);
+    filesink.ChangeState("INIT_DEVICE");
+    filesink.WaitForEndOfState("INIT_DEVICE");
 
-    filesink.ChangeState(TSink::INIT_TASK);
-    filesink.WaitForEndOfState(TSink::INIT_TASK);
+    filesink.ChangeState("INIT_TASK");
+    filesink.WaitForEndOfState("INIT_TASK");
 
     filesink.InitOutputFile(options.id);
 
-    filesink.ChangeState(TSink::RUN);
+    filesink.ChangeState("RUN");
     filesink.InteractiveStateLoop();
+}
 
+int main(int argc, char** argv)
+{
+    DeviceOptions_t options;
+    try
+    {
+        if (!parse_cmd_line(argc, argv, &options))
+            return 0;
+    }
+    catch (exception& e)
+    {
+        LOG(ERROR) << e.what();
+        return 1;
+    }
+
+    LOG(INFO) << "PID: " << getpid();
+
+    if (options.dataFormat == "binary") { runFileSink<TSinkBin>(options); }
+    else if (options.dataFormat == "boost") { runFileSink<TSinkBoost>(options); }
+    else if (options.dataFormat == "protobuf") { runFileSink<TSinkProtobuf>(options); }
+    else if (options.dataFormat == "tmessage") { runFileSink<TSinkTMessage>(options); }
+    else
+    {
+        LOG(ERROR) << "No valid data format provided. (--data-format binary|boost|protobuf|tmessage). ";
+        return 1;
+    }
 
     return 0;
 }
---
+++ example/Tutorial3/run/runTestDetectorSampler.cxx
@@ -0,0 +1,189 @@
+/********************************************************************************
+ *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             * 
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *  
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+/**
+ * runTestDetectorSamplerBin.cxx
+ *
+ * @since 2013-04-29
+ * @author A. Rybalchenko, N. Winckler
+ */
+
+#include <iostream>
+
+#include "boost/program_options.hpp"
+
+#include "FairMQLogger.h"
+#include "FairMQSampler.h"
+
+#ifdef NANOMSG
+#include "nanomsg/FairMQTransportFactoryNN.h"
+#else
+#include "zeromq/FairMQTransportFactoryZMQ.h"
+#endif
+
+#include "TestDetectorDigiLoader.h"
+
+// data format for the task
+#include "FairTestDetectorDigi.h"
+
+// binary data format
+#include "FairTestDetectorPayload.h"
+
+// boost data format
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+
+// Google Protocol Buffers data format
+#include "FairTestDetectorPayload.pb.h"
+
+// TMessage data format
+#include "TMessage.h"
+
+using namespace std;
+
+typedef TestDetectorPayload::Digi TPayloadOut; // binary payload
+typedef boost::archive::binary_oarchive TBoostBinPayloadOut; // boost binary format
+typedef boost::archive::text_oarchive TBoostTextPayloadOut;  // boost text format
+typedef TestDetectorProto::DigiPayload TProtoDigiPayload; // protobuf payload
+
+typedef FairMQSampler<TestDetectorDigiLoader<FairTestDetectorDigi, TPayloadOut>> TSamplerBin;
+typedef FairMQSampler<TestDetectorDigiLoader<FairTestDetectorDigi, TBoostBinPayloadOut>> TSamplerBoost;
+typedef FairMQSampler<TestDetectorDigiLoader<FairTestDetectorDigi, TProtoDigiPayload>> TSamplerProtobuf;
+typedef FairMQSampler<TestDetectorDigiLoader<FairTestDetectorDigi, TMessage>> TSamplerTMessage;
+
+typedef struct DeviceOptions
+{
+    DeviceOptions() :
+        id(), ioThreads(0), dataFormat(), inputFile(), parameterFile(), branch(), eventRate(0),
+        outputSocketType(), outputBufSize(0), outputMethod(), outputAddress() {}
+
+    string id;
+    int ioThreads;
+    string dataFormat;
+    string inputFile;
+    string parameterFile;
+    string branch;
+    int eventRate;
+    string outputSocketType;
+    int outputBufSize;
+    string outputMethod;
+    string outputAddress;
+} DeviceOptions_t;
+
+inline bool parse_cmd_line(int _argc, char* _argv[], DeviceOptions* _options)
+{
+    if (_options == NULL)
+        throw std::runtime_error("Internal error: options' container is empty.");
+
+    namespace bpo = boost::program_options;
+    bpo::options_description desc("Options");
+    desc.add_options()
+        ("id", bpo::value<string>()->required(), "Device ID")
+        ("io-threads", bpo::value<int>()->default_value(1), "Number of I/O threads")
+        ("data-format", bpo::value<string>()->default_value("binary"), "Data format (binary/boost/protobuf/tmessage)")
+        ("input-file", bpo::value<string>()->required(), "Path to the input file")
+        ("parameter-file", bpo::value<string>()->required(), "path to the parameter file")
+        ("branch", bpo::value<string>()->default_value("FairTestDetectorDigi"), "Name of the Branch")
+        ("event-rate", bpo::value<int>()->default_value(0), "Event rate limit in maximum number of events per second")
+        ("output-socket-type", bpo::value<string>()->required(), "Output socket type: pub/push")
+        ("output-buff-size", bpo::value<int>()->required(), "Output buffer size in number of messages (ZeroMQ)/bytes(nanomsg)")
+        ("output-method", bpo::value<string>()->required(), "Output method: bind/connect")
+        ("output-address", bpo::value<string>()->required(), "Output address, e.g.: \"tcp://*:5555\"")
+        ("help", "Print help messages");
+
+    bpo::variables_map vm;
+    bpo::store(bpo::parse_command_line(_argc, _argv, desc), vm);
+
+    if (vm.count("help"))
+    {
+        LOG(INFO) << "FairMQ Test Detector Sampler" << endl << desc;
+        return false;
+    }
+
+    bpo::notify(vm);
+
+    if (vm.count("id"))                 { _options->id               = vm["id"].as<string>(); }
+    if (vm.count("io-threads"))         { _options->ioThreads        = vm["io-threads"].as<int>(); }
+    if (vm.count("data-format"))        { _options->dataFormat       = vm["data-format"].as<string>(); }
+    if (vm.count("input-file"))         { _options->inputFile        = vm["input-file"].as<string>(); }
+    if (vm.count("parameter-file"))     { _options->parameterFile    = vm["parameter-file"].as<string>(); }
+    if (vm.count("branch"))             { _options->branch           = vm["branch"].as<string>(); }
+    if (vm.count("event-rate"))         { _options->eventRate        = vm["event-rate"].as<int>(); }
+    if (vm.count("output-socket-type")) { _options->outputSocketType = vm["output-socket-type"].as<string>(); }
+    if (vm.count("output-buff-size"))   { _options->outputBufSize    = vm["output-buff-size"].as<int>(); }
+    if (vm.count("output-method"))      { _options->outputMethod     = vm["output-method"].as<string>(); }
+    if (vm.count("output-address"))     { _options->outputAddress    = vm["output-address"].as<string>(); }
+
+    return true;
+}
+
+template<typename T>
+void runSampler(const DeviceOptions_t& options)
+{
+    T sampler;
+    sampler.CatchSignals();
+
+#ifdef NANOMSG
+    FairMQTransportFactory* transportFactory = new FairMQTransportFactoryNN();
+#else
+    FairMQTransportFactory* transportFactory = new FairMQTransportFactoryZMQ();
+#endif
+
+    sampler.SetTransport(transportFactory);
+
+    FairMQChannel channel(options.outputSocketType, options.outputMethod, options.outputAddress);
+    channel.UpdateSndBufSize(options.outputBufSize);
+    channel.UpdateRcvBufSize(options.outputBufSize);
+    channel.UpdateRateLogging(1);
+
+    sampler.fChannels["data-out"].push_back(channel);
+
+    sampler.SetProperty(T::Id, options.id);
+    sampler.SetProperty(T::InputFile, options.inputFile);
+    sampler.SetProperty(T::ParFile, options.parameterFile);
+    sampler.SetProperty(T::Branch, options.branch);
+    sampler.SetProperty(T::EventRate, options.eventRate);
+    sampler.SetProperty(T::NumIoThreads, options.ioThreads);
+
+    sampler.ChangeState("INIT_DEVICE");
+    sampler.WaitForEndOfState("INIT_DEVICE");
+
+    sampler.ChangeState("INIT_TASK");
+    sampler.WaitForEndOfState("INIT_TASK");
+
+    sampler.ChangeState("RUN");
+    sampler.InteractiveStateLoop();
+}
+
+int main(int argc, char** argv)
+{
+    DeviceOptions_t options;
+    try
+    {
+        if (!parse_cmd_line(argc, argv, &options))
+            return 0;
+    }
+    catch (exception& e)
+    {
+        LOG(ERROR) << e.what();
+        return 1;
+    }
+
+    LOG(INFO) << "PID: " << getpid();
+
+    if (options.dataFormat == "binary") { runSampler<TSamplerBin>(options); }
+    else if (options.dataFormat == "boost") { runSampler<TSamplerBoost>(options); }
+    else if (options.dataFormat == "protobuf") { runSampler<TSamplerProtobuf>(options); }
+    else if (options.dataFormat == "tmessage") { runSampler<TSamplerTMessage>(options); }
+    else
+    {
+        LOG(ERROR) << "No valid data format provided. (--data-format binary|boost|protobuf|tmessage). ";
+        return 1;
+    }
+
+    return 0;
+}
