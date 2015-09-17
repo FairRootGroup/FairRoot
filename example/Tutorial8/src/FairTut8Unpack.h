5c9b1f9462a1b1b6f614a95e3336ac2cc0a42167
Dmytro Kresan <D.Kresan@gsi.de>
2015-09-17 13:46:32 +0200

    Added THttpServer functionality. Added example task with activation of HTTP server. Disable automatic finish of Run. Add Registerring of custom command. Added doxygen documentation.

--- example/Tutorial8/src/FairTut8Unpack.h
+++ example/Tutorial8/src/FairTut8Unpack.h
@@ -13,33 +13,45 @@
 
 class TClonesArray;
 
+/**
+ * An example unpacker of MBS data.
+ */
 class FairTut8Unpack : public FairUnpack
 {
   public:
-    // Constructor
+    /** Standard Constructor. Input - MBS parameters of the detector. */
     FairTut8Unpack(Short_t type = 94,
                    Short_t subType = 9400,
                    Short_t procId = 10,
                    Short_t subCrate = 1,
                    Short_t control = 3);
 
-    // Destructor
+    /** Destructor. */
     virtual ~FairTut8Unpack();
 
-    // Fair specific
+    /** Initialization. Called once, before the event loop. */
     virtual Bool_t Init();
-    virtual Bool_t DoUnpack(Int_t* data, Int_t size);
-    virtual void Reset();
     
-    inline Int_t GetNHitsTotal() { return fNHitsTotal; }
+    /** Process an MBS sub-event. */
+    virtual Bool_t DoUnpack(Int_t* data, Int_t size);
+    
+    /** Clear the output structures. */
+    virtual void Reset();
+
+    /** Method for controling the functionality. */
+    inline Int_t GetNHitsTotal()
+    {
+        return fNHitsTotal;
+    }
 
   protected:
+    /** Register the output structures. */
     virtual void Register();
 
   private:
-    TClonesArray* fRawData;
-    Int_t fNHits;
-    Int_t fNHitsTotal;
+    TClonesArray* fRawData; /**< Array of output raw items. */
+    Int_t fNHits;           /**< Number of raw items in current event. */
+    Int_t fNHitsTotal;      /**< Total number of raw items. */
 
   public:
     // Class definition
