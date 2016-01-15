24e9e1eefd4d037ca770a02f11308d108d4ab946
Florian Uhlig <f.uhlig@gsi.de>
2016-01-15 12:17:00 +0100

    Allow to set a site name for CDash.
    When set use the environment variable SITE for the CDash site name. This is needed to get a meaningful name
    for the Travis CI machines. Set the site name for Travis to Travis CI.

--- Dart.sh
+++ Dart.sh
@@ -89,10 +89,12 @@ then
   else 
     export number_of_processors=$(cat /proc/cpuinfo | grep processor | wc -l)
   fi
-  export SITE=$(hostname -f)
-  if [ -z $SITE ]; then
-    export SITE=$(uname -n)
-  fi  
+  if test -z $SITE ; then
+    export SITE=$(hostname -f)
+    if [ -z $SITE ]; then
+      export SITE=$(uname -n)
+    fi
+  else
 elif [ "$arch" = "darwin" ];
 then
   if [ "$NCPU" != "" ];
@@ -101,7 +103,9 @@ then
   else 
     export number_of_processors=$(sysctl -n hw.ncpu)
   fi
-  export SITE=$(hostname -s)
+  if test -z $SITE ; then
+    export SITE=$(hostname -s)
+  fi
 fi
 
 echo "************************"
