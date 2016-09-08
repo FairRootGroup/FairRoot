600ea4a32aa16cd064438dd7441c0ccc32fb8294
Radoslaw Karabowicz <r.karabowicz@gsi.de>
2016-09-08 11:51:42 +0200

    MQ/example9: Added Merger that reads data from 3 different input channels.

---
+++ examples/MQ/9-PixelDetector/run/runEx9Merger3Ch.cxx
@@ -0,0 +1,23 @@
+/********************************************************************************
+ *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             * 
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *  
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
+#include "runFairMQDevice.h"
+
+// 9-PixelDetector example
+#include "FairMQEx9Merger3Ch.h"
+
+namespace bpo = boost::program_options;
+
+void addCustomOptions(bpo::options_description& options)
+{
+}
+
+FairMQDevice* getDevice(const FairMQProgOptions& config)
+{
+  return new FairMQEx9Merger3Ch();
+}
