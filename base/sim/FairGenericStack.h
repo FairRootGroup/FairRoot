0e994e7121a5e6968bfded641148d9265ee9ac86
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2017-04-04 13:25:24 +0200

    Code clean-up in the FairGenericStack class: - Removed error-prone dummy implementation of pure virtual functions from the base class - Removed dummy implementation of virtual functions defined in this class, which are called   by the FairRoot framework, and made them pure virtual

--- base/sim/FairGenericStack.h
+++ base/sim/FairGenericStack.h
@@ -6,12 +6,12 @@
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
 // -------------------------------------------------------------------------
-// -----                       FairStack header file                    -----
+// -----                       FairGenericStack header file            -----
 // -----           Created 10/08/04  by D. Bertini                     -----
 // -------------------------------------------------------------------------
 
 
-/** FairStack.h
+/** FairGenericStack.h
  *@author D.Bertini <d.bertini@gsi.de>
  * Generic MC stack class
  **/
@@ -50,30 +50,10 @@ class FairGenericStack : public TVirtualMCStack
     /** Destructor  **/
     virtual ~FairGenericStack();
 
-    /** Virtual method PushTrack.
-     ** Add a TParticle to the stack.
-     *@param toBeDone  Flag for tracking
-     *@param parentID  Index of mother particle
-     *@param pdgCode   Particle type (PDG encoding)
-     *@param px,py,pz  Momentum components at start vertex [GeV]
-     *@param e         Total energy at start vertex [GeV]
-     *@param vx,vy,vz  Coordinates of start vertex [cm]
-     *@param time      Start time of track [s]
-     *@param polx,poly,polz Polarisation vector
-     *@param proc      Production mechanism (VMC encoding)
-     *@param ntr       Track number (filled by the stack)
-     *@param weight    Particle weight
-     *@param is        Generation status code (whatever that means)
-     **/
-    virtual void PushTrack(Int_t toBeDone, Int_t parentID, Int_t pdgCode,
-                           Double_t px, Double_t py, Double_t pz,
-                           Double_t e, Double_t vx, Double_t vy,
-                           Double_t vz, Double_t time, Double_t polx,
-                           Double_t poly, Double_t polz, TMCProcess proc,
-                           Int_t& ntr, Double_t weight, Int_t is);
 
     /** Virtual method PushTrack.
      ** Add a TParticle to the stack.
+     ** This function has an extra argument wrt to the function defined in the base class.
      *@param toBeDone         Flag for tracking
      *@param parentID         Index of mother particle
      *@param pdgCode          Particle type (PDG encoding)
@@ -88,72 +68,32 @@ class FairGenericStack : public TVirtualMCStack
      *@param is               Generation status code (whatever that means)
      *@param secondparentID   used fot the index of mother of primery in the list
      **/
-
     virtual void PushTrack(Int_t toBeDone, Int_t parentID, Int_t pdgCode,
                            Double_t px, Double_t py, Double_t pz,
                            Double_t e, Double_t vx, Double_t vy,
                            Double_t vz, Double_t time, Double_t polx,
                            Double_t poly, Double_t polz, TMCProcess proc,
-                           Int_t& ntr, Double_t weight, Int_t is, Int_t secondparentID);
-
-
-    /** Virtual method PopNextTrack.
-     ** Gets next particle for tracking from the stack.
-     *@param  iTrack  index of popped track
-     *@return Pointer to the TParticle of the track
-     **/
-    virtual TParticle* PopNextTrack(Int_t& iTrack);
-
-
-    /** Virtual method PopPrimaryForTracking.
-     ** Gets primary particle by index for tracking from stack.
-     *@param  iPrim   index of primary particle
-     *@return Pointer to the TParticle of the track
-     **/
-    virtual TParticle* PopPrimaryForTracking(Int_t iPrim);
-
-
-    /** Add a TParticle to the fParticles array **/
-    void AddParticle(TParticle* part);
+                           Int_t& ntr, Double_t weight, Int_t is, Int_t secondparentID) = 0;
 
 
     /** Fill the MCTrack output array, applying filter criteria **/
-    virtual void FillTrackArray();
+    virtual void FillTrackArray() {}
 
 
     /** Update the track index in the MCTracks and MCPoints **/
-    virtual void UpdateTrackIndex(TRefArray* detArray=0);
+    virtual void UpdateTrackIndex(TRefArray* detArray=0) {}
 
 
     /** Set the list of detectors to be used for filltering the stack*/
     void SetDetArrayList(TRefArray* detArray);
 
     /** Resets arrays and stack and deletes particles and tracks **/
-    virtual void Reset();
-
+    virtual void Reset() {}
 
     /** Register the MCTrack array to the Root Manager  **/
-    virtual void Register();
+    virtual void Register() {}
 
-
-    /** Output to screen
-     **@param iVerbose: 0=events summary, 1=track info
-     **/
-    virtual void Print(Option_t* option="") const;
-
-
-    /** Modifiers  **/
-    virtual void SetCurrentTrack(Int_t iTrack);
-
-    /** Accessors **/
-    virtual Int_t GetNtrack() const;   // Total number of tracks
-    virtual Int_t GetNprimary() const; // Number of primaries
-    virtual TParticle* GetCurrentTrack() const;
-    virtual Int_t GetCurrentTrackNumber() const;
-    virtual Int_t GetCurrentParentTrackNumber() const;
-    virtual TParticle* GetParticle(Int_t) const { return NULL; }
-
-    virtual TClonesArray* GetListOfParticles() { return NULL; } 
+    virtual TClonesArray* GetListOfParticles() = 0;
 
     /** Clone this object (used in MT mode only) */
     virtual FairGenericStack* CloneStack() const;
@@ -165,7 +105,7 @@ class FairGenericStack : public TVirtualMCStack
     FairGenericStack& operator=(const FairGenericStack&);
 
     /** Fair Logger */
-    FairLogger*            fLogger;//!
+    FairLogger*  fLogger;//!
 
     /** List of detectors registering hits in the simulation */
     TRefArray*  fDetList;  //!
@@ -177,8 +117,6 @@ class FairGenericStack : public TVirtualMCStack
     Int_t fVerbose;
 
     ClassDef(FairGenericStack,1)
-
-
 };
 
 #endif
