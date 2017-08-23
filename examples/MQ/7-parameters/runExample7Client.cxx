94391936fa7184a12735823626d1cd8da22dee83
Alexey Rybalchenko <alexryba@gmail.com>
2017-08-23 11:12:29 +0200

    Tests for MQ examples

--- examples/MQ/7-parameters/runExample7Client.cxx
+++ examples/MQ/7-parameters/runExample7Client.cxx
@@ -14,7 +14,8 @@ namespace bpo = boost::program_options;
 void addCustomOptions(bpo::options_description& options)
 {
     options.add_options()
-        ("parameter-name", bpo::value<std::string>()->default_value("FairMQExample7ParOne"), "Parameter Name");
+        ("parameter-name", bpo::value<std::string>()->default_value("FairMQExample7ParOne"), "Parameter Name")
+        ("max-iterations", bpo::value<uint64_t>()->default_value(0), "Maximum number of iterations of Run/ConditionalRun/OnData (0 - infinite)");
 }
 
 FairMQDevicePtr getDevice(const FairMQProgOptions& /*config*/)
