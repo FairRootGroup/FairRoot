9e240950755246e28072adaf485aae697b89f33a
Alexey Rybalchenko <alexryba@gmail.com>
2017-09-07 11:42:43 +0200

    Refactor state machine.

--- base/MQ/baseMQtools/baseMQtools.h
+++ base/MQ/baseMQtools/baseMQtools.h
@@ -30,20 +30,20 @@ template<std::size_t>
 struct int_{};
 
 template <class Tuple, size_t Pos>
-std::ostream& print_tuple(std::ostream& out, const Tuple& t, int_<Pos> ) 
+std::ostream& print_tuple(std::ostream& out, const Tuple& t, int_<Pos> )
 {
     out << std::get< std::tuple_size<Tuple>::value-Pos >(t) << ',';
     return print_tuple(out, t, int_<Pos-1>());
 }
 
 template <class Tuple>
-std::ostream& print_tuple(std::ostream& out, const Tuple& t, int_<1> ) 
+std::ostream& print_tuple(std::ostream& out, const Tuple& t, int_<1> )
 {
     return out << std::get<std::tuple_size<Tuple>::value-1>(t);
 }
 
 template <class... Args>
-std::ostream& operator<<(std::ostream& out, const std::tuple<Args...>& t) 
+std::ostream& operator<<(std::ostream& out, const std::tuple<Args...>& t)
 {
     out << '(';
     print_tuple(out, t, int_<sizeof...(Args)>());
