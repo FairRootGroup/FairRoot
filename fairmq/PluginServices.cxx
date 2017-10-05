FairRootGroup/FairRoot

commit f1513f5169d335c17ccdfdb27f0f0c4edbeb8b65

Fix name conflict with FairLogger+FairMQStateMachine

fairmq/PluginServices.cxx
@@ -12,7 +12,7 @@ using namespace fair::mq;
 
 const std::unordered_map<std::string, PluginServices::DeviceState> PluginServices::fkDeviceStateStrMap = {
     {"OK",                  DeviceState::Ok},
-    {"ERROR",               DeviceState::Error},
+    {"Error",               DeviceState::Error},
     {"IDLE",                DeviceState::Idle},
     {"INITIALIZING DEVICE", DeviceState::InitializingDevice},
     {"DEVICE READY",        DeviceState::DeviceReady},
@@ -26,7 +26,7 @@ const std::unordered_map<std::string, PluginServices::DeviceState> PluginService
 };
 const std::unordered_map<PluginServices::DeviceState, std::string, tools::HashEnum<PluginServices::DeviceState>> PluginServices::fkStrDeviceStateMap = {
     {DeviceState::Ok,                 "OK"},
-    {DeviceState::Error,              "ERROR"},
+    {DeviceState::Error,              "Error"},
     {DeviceState::Idle,               "IDLE"},
     {DeviceState::InitializingDevice, "INITIALIZING DEVICE"},
     {DeviceState::DeviceReady,        "DEVICE READY"},
@@ -62,7 +62,7 @@ const std::unordered_map<PluginServices::DeviceStateTransition, std::string, too
 };
 const std::unordered_map<FairMQDevice::State, PluginServices::DeviceState, fair::mq::tools::HashEnum<FairMQDevice::State>> PluginServices::fkDeviceStateMap = {
     {FairMQDevice::OK,                  DeviceState::Ok},
-    {FairMQDevice::ERROR,               DeviceState::Error},
+    {FairMQDevice::Error,               DeviceState::Error},
     {FairMQDevice::IDLE,                DeviceState::Idle},
     {FairMQDevice::INITIALIZING_DEVICE, DeviceState::InitializingDevice},
     {FairMQDevice::DEVICE_READY,        DeviceState::DeviceReady},

fairmq/FairMQStateMachine.h
@@ -357,8 +357,8 @@ struct FairMQFSM_ : public msmf::state_machine_def<FairMQFSM_>
         void operator()(EVT const&, FSM& fsm, SourceState&, TargetState&)
         {
             LOG(STATE) << "Entering ERROR state";
-            fsm.fState = ERROR;
-            fsm.CallStateChangeCallbacks(ERROR);
+            fsm.fState = Error;
+            fsm.CallStateChangeCallbacks(Error);
         }
     };
 
@@ -460,7 +460,7 @@ struct FairMQFSM_ : public msmf::state_machine_def<FairMQFSM_>
     enum State
     {
         OK,
-        ERROR,
+        Error,
         IDLE,
         INITIALIZING_DEVICE,
         DEVICE_READY,
@@ -479,8 +479,8 @@ struct FairMQFSM_ : public msmf::state_machine_def<FairMQFSM_>
         {
             case OK:
                 return "OK";
-            case ERROR:
-                return "ERROR";
+            case Error:
+                return "Error";
             case IDLE:
                 return "IDLE";
             case INITIALIZING_DEVICE:

commit 1cfaffbe40369f60f81b16c45df959faf446eb80

FairMQ: Revert regression introduced by f1513f5169d335c17ccdfdb27f0f0c4edbeb8b65

fairmq/PluginServices.cxx
@@ -13,7 +13,7 @@ using namespace std;
 
 const std::unordered_map<std::string, PluginServices::DeviceState> PluginServices::fkDeviceStateStrMap = {
     {"OK",                  DeviceState::Ok},
-    {"Error",               DeviceState::Error},
+    {"ERROR",               DeviceState::Error},
     {"IDLE",                DeviceState::Idle},
     {"INITIALIZING DEVICE", DeviceState::InitializingDevice},
     {"DEVICE READY",        DeviceState::DeviceReady},
@@ -27,7 +27,7 @@ const std::unordered_map<std::string, PluginServices::DeviceState> PluginService
 };
 const std::unordered_map<PluginServices::DeviceState, std::string, tools::HashEnum<PluginServices::DeviceState>> PluginServices::fkStrDeviceStateMap = {
     {DeviceState::Ok,                 "OK"},
-    {DeviceState::Error,              "Error"},
+    {DeviceState::Error,              "ERROR"},
     {DeviceState::Idle,               "IDLE"},
     {DeviceState::InitializingDevice, "INITIALIZING DEVICE"},
     {DeviceState::DeviceReady,        "DEVICE READY"},