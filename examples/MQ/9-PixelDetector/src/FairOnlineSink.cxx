1fce6c612509a9713684bf49e51f87db44c272ba
Radoslaw Karabowicz <r.karabowicz@gsi.de>
2018-03-23 09:17:47 +0100

    MQ/example9
    Splitted the double inheritance of FairMQSimDevice.
    It is now a singleton inheriting from FairMQDevice only.
    The FairSink functionality of the FairMQSimDevice was moved to FairOnlineSink.

---
+++ examples/MQ/9-PixelDetector/src/FairOnlineSink.cxx
@@ -0,0 +1,86 @@
+/********************************************************************************
+ *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             * 
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *  
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+/**
+ * FairOnlineSink.cxx
+ *
+ * @since 2017.10.24
+ * @author R. Karabowicz
+ */
+
+#include "FairOnlineSink.h"
+  
+#include "FairMQLogger.h"
+#include "FairMQMessage.h"
+#include "FairMQProgOptions.h"
+
+#include "FairMQSimDevice.h"
+#include "FairRootManager.h"
+
+#include "FairEventHeader.h"
+#include "FairModule.h"
+#include "FairPrimaryGenerator.h"
+#include "FairParRootFileIo.h"
+#include "FairParSet.h"
+
+#include "FairMCEventHeader.h"
+
+#include "TROOT.h"
+#include "TRint.h"
+#include "TVirtualMC.h"
+#include "TVirtualMCApplication.h"
+#include "TList.h"
+#include "TObjString.h"
+#include "TObjArray.h"
+#include "TMessage.h"
+
+using namespace std;
+
+FairOnlineSink::FairOnlineSink()
+  : FairSink()
+  , fMQSimDevice(NULL)
+{
+}
+
+FairOnlineSink::~FairOnlineSink()
+{
+}
+
+//_____________________________________________________________________________
+void FairOnlineSink::RegisterImpl(const char* , const char* , void* )
+{
+  return;
+}
+
+//_____________________________________________________________________________
+void FairOnlineSink::RegisterAny(const char* brname, const std::type_info &oi, const std::type_info &pi, void* obj)
+{
+  return;
+}
+
+void  FairOnlineSink::Fill()
+{
+  /// Fill the Root tree.
+  LOG(DEBUG) << "called FairOnlineSink::Fill()!!!!";
+  
+  if ( fMQSimDevice )
+      fMQSimDevice->SendBranches();
+}
+ 
+Bool_t FairOnlineSink::InitSink()
+{
+    fMQSimDevice = FairMQSimDevice::Instance();
+    return kTRUE;
+}
+
+void  FairOnlineSink::Reset()
+{
+}
+
+void  FairOnlineSink::Close()
+{
+}
---
+++ examples/MQ/9-PixelDetector/src/FairOnlineSink.h
@@ -0,0 +1,62 @@
+/********************************************************************************
+ *    Copyright (C) 2017 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             * 
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *  
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
+/**
+ * FairOnlineSink.h
+ *
+ * @since 2018-02-01
+ * @author R. Karabowicz
+ */
+
+#ifndef FAIRONLINESINK_H_
+#define FAIRONLINESINK_H_
+
+#include <string>
+
+#include "FairSink.h"
+
+class FairEventHeader;
+class FairMQSimDevice;
+
+class FairOnlineSink : public FairSink
+{
+ public:
+    FairOnlineSink();
+    virtual ~FairOnlineSink();
+  
+    virtual Bool_t      InitSink();
+    virtual void        Close();
+    virtual void        Reset();
+
+    virtual Sink_Type   GetSinkType() { return kONLINESINK; }
+
+    virtual void        FillEventHeader(FairEventHeader* feh) {}
+
+    virtual void        SetOutTree(TTree* fTree) { return; }
+
+    virtual void        Fill();
+
+    virtual Int_t       Write(const char* name=0, Int_t option=0, Int_t bufsize=0) {return -1;}
+
+    virtual void        RegisterImpl(const char* , const char* , void* );
+    virtual void        RegisterAny(const char* brname, const std::type_info &oi, const std::type_info &pi, void* obj);
+
+    virtual void        WriteFolder() {}
+    virtual bool        CreatePersistentBranchesAny() {return false;}
+
+    virtual void        WriteObject(TObject* f, const char*, Int_t option = 0) {}
+    virtual void        WriteGeometry() {}
+
+ private: 
+    FairMQSimDevice* fMQSimDevice;
+    
+    FairOnlineSink(const FairOnlineSink&);
+    FairOnlineSink& operator=(const FairOnlineSink&);
+};
+
+#endif /* FAIRONLINESIK_H_ */
