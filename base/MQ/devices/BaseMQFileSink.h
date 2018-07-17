f5ca429f3e1dc4e538fce658039356fc152a420e
Alexey Rybalchenko <alexryba@gmail.com>
2018-07-17 15:52:57 +0200

    Fix ambiguous method call in BaseMQFileSink

--- base/MQ/devices/BaseMQFileSink.h
+++ base/MQ/devices/BaseMQFileSink.h
@@ -55,7 +55,7 @@ class BaseMQFileSink : public FairMQDevice, public InputPolicy, public OutputPol
             std::unique_ptr<FairMQMessage> msg(NewMessage());
             if (Receive(msg, fInputChanName) > 0)
             {
-                Deserialize<typename InputPolicy::DeserializerType>(*msg, InputPolicy::fInput); // get data from message.
+                FairMQDevice::Deserialize<typename InputPolicy::DeserializerType>(*msg, InputPolicy::fInput); // get data from message.
                 OutputPolicy::Serialize(InputPolicy::fInput); // put data into output.
                 receivedMsg++;
             }
