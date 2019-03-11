15445bb76a5d2cc3bf46d60bab70fbef4c9e2d54
Mohammad Al-Turany <m.al-turany@gsi.de>
2019-03-11 09:11:06 +0100

    minor fixes

--- cmake/modules/FairMacros.cmake
+++ cmake/modules/FairMacros.cmake
@@ -1,26 +1,12 @@
- ################################################################################
- #    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    #
- #                                                                              #
- #              This software is distributed under the terms of the             #
- #              GNU Lesser General Public Licence (LGPL) version 3,             #
- #                  copied verbatim in the file "LICENSE"                       #
- ################################################################################
-  ###########################################
-  #
-  #       Usefull macros
-  #
-  ###########################################
+################################################################################
+#    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    #
+#                                                                              #
+#              This software is distributed under the terms of the             #
+#              GNU Lesser General Public Licence (LGPL) version 3,             #
+#                  copied verbatim in the file "LICENSE"                       #
+################################################################################
 
-  ###############################################################
-  #
-  # Exchange file extention of LIST from
-  # FILE_EXT1 to FILE_EXT2 and assign the
-  # newly created list to OUTVAR. The input
-  # list LIST is not changed at all
-  # Ex: CHANGE_FILE_EXTENSION(*.cxx *.h TRD_HEADERS "${TRD_SRCS}")
-  #
-  ################################################################
-  # Defines some variables with console color escape sequences
+#Defines some variables with console color escape sequences
   if(NOT WIN32 AND NOT DISABLE_COLOR)
     string(ASCII 27 Esc)
     set(CR       "${Esc}[m")
@@ -41,7 +27,25 @@
     set(BWhite   "${Esc}[1;37m")
   endif()
 
-  MACRO(SUBDIRLIST result curdir)
+################################################################################
+#Sets CMake policies.
+MACRO(set_fairroot_cmake_policies)
+#Find more details to each policy with cmake-- help - policy CMPXXXX
+    foreach(policy
+      CMP0025 # Compiler id for Apple Clang is now AppleClang.
+      CMP0028 # Double colon in target name means ALIAS or IMPORTED target.
+      CMP0042 # MACOSX_RPATH is enabled by default.
+      CMP0048 # The ``project()`` command manages VERSION variables.
+      CMP0054 # Only interpret ``if()`` arguments as variables or keywords when unquoted.
+    )
+      if(POLICY ${policy})
+        cmake_policy(SET ${policy} NEW)
+      endif()
+    endforeach()
+ENDMACRO()
+
+################################################################################
+MACRO(SUBDIRLIST result curdir)
     FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
     SET(dirlist "")
     FOREACH(child ${children})
@@ -50,93 +54,78 @@
       ENDIF()
     ENDFOREACH()
     SET(${result} ${dirlist})
-  ENDMACRO()
+ENDMACRO()
 
-
+################################################################################
+#
+#Exchange file extention of LIST from
+#FILE_EXT1 to FILE_EXT2 and assign the
+#newly created list to OUTVAR.The input
+#list LIST is not changed at all
+#Ex : CHANGE_FILE_EXTENSION(*.cxx *.h TRD_HEADERS "${TRD_SRCS}")
+#
+################################################################################
 MACRO (CHANGE_FILE_EXTENSION FILE_EXT1 FILE_EXT2 OUTVAR LIST)
-
    SET(BLA)
-
    IF (${FILE_EXT1} MATCHES "^[*][.]+.*$")
      STRING(REGEX REPLACE "^[*]+([.].*)$" "\\1" FILE_EXT1_NEW ${FILE_EXT1})
    ENDIF  (${FILE_EXT1} MATCHES "^[*][.]+.*$")
-
    IF (${FILE_EXT2} MATCHES "^[*][.]+.*$")
      STRING(REGEX REPLACE "^[*]+([.].*)" "\\1" FILE_EXT2_NEW ${FILE_EXT2})
    ENDIF  (${FILE_EXT2} MATCHES "^[*][.]+.*$")
-
    foreach (_current_FILE ${LIST})
-
      STRING(REGEX REPLACE "^(.*)${FILE_EXT1_NEW}$" "\\1${FILE_EXT2_NEW}" test ${_current_FILE})
      SET (BLA ${BLA} ${test})
-
    endforeach (_current_FILE ${ARGN})
-
    SET (${OUTVAR} ${BLA})
-
-
-
 ENDMACRO (CHANGE_FILE_EXTENSION)
 
-  ######################################################
-  #
-  # Macro get string with a colon seperated string of
-  # pathes or any other colon sperated list.
-  # First the string is seperated  and the entries are
-  # filled into a list.
-  # Loop over the list and searches for the occurence
-  # of keywords which are provided as a list.
-  # If the keyword occurs this path (entry) is
-  # deleted from the list. Returns the list of pathes
-  # (entries) wich survives the loop.
-  #
-  # PATH: colon separated string of pathes or other
-  #       input entries
-  # LIST_OF_KEYWORDS: list of the keywords which
-  #                   should be excluded in the output
-  # OUTVAR: name of the variable which should be filled
-  #         with the resulting output list
-  #
-  ######################################################
+################################################################################
+#
+# Macro get string with a colon seperated string of
+# pathes or any other colon sperated list.
+# First the string is seperated  and the entries are
+# filled into a list.
+# Loop over the list and searches for the occurence
+# of keywords which are provided as a list.
+# If the keyword occurs this path (entry) is
+# deleted from the list. Returns the list of pathes
+# (entries) wich survives the loop.
+#
+# PATH: colon separated string of pathes or other
+#       input entries
+# LIST_OF_KEYWORDS: list of the keywords which
+#                   should be excluded in the output
+# OUTVAR: name of the variable which should be filled
+#         with the resulting output list
+#
+################################################################################
 
 MACRO (CLEAN_PATH_LIST PATH LIST_OF_KEYWORDS OUTVAR)
-
   SET(BLA "")
-
   STRING(REGEX MATCHALL "[^:]+" PATH1 ${PATH})
-
   FOREACH(_current_PATH ${PATH1})
-
     SET(KEYWORD_FOUND FALSE)
-
     FOREACH(_current_KEYWORD ${LIST_OF_KEYWORDS})
-
       IF (${_current_PATH} MATCHES "${_current_KEYWORD}")
         SET(KEYWORD_FOUND TRUE)
       ENDIF (${_current_PATH} MATCHES "${_current_KEYWORD}")
-
     ENDFOREACH(_current_KEYWORD ${LIST_OF_KEYWORDS})
-
     IF (NOT KEYWORD_FOUND)
       SET(BLA ${BLA} ${_current_PATH})
     ENDIF (NOT KEYWORD_FOUND)
-
   ENDFOREACH(_current_PATH ${PATH1})
-
   UNIQUE(${OUTVAR} "${BLA}")
-
 ENDMACRO (CLEAN_PATH_LIST)
 
-  ##########################################################
-  #
-  # The macro checks if the user wants to build the project
-  # in the source directory and if so stop the execution
-  # of cmake with an error message.
-  #
-  ##########################################################
+#################################################################################
+# The macro checks if the user wants to build the project
+# in the source directory and if so stop the execution
+# of cmake with an error message.
+#
+################################################################################
 
 MACRO (CHECK_OUT_OF_SOURCE_BUILD)
-
 
    STRING(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" insource)
    IF(insource)
@@ -147,38 +136,33 @@ MACRO (CHECK_OUT_OF_SOURCE_BUILD)
 
 ENDMACRO (CHECK_OUT_OF_SOURCE_BUILD)
 
-  ##########################################################
-  #
-  # The macro checks if the build directory is different from the
-  # installation directory. In case both are the same
-  # stop the execution of cmake with an error message.
-  #
-  ##########################################################
+#################################################################################
+# The macro checks if the build directory is different from the
+# installation directory. In case both are the same
+# stop the execution of cmake with an error message.
+#
+################################################################################
 
-Macro (CHECK_INSTALL_DIRECTORY)
-
+MACRO (CHECK_INSTALL_DIRECTORY)
    String(COMPARE EQUAL "${CMAKE_INSTALL_PREFIX}" "${CMAKE_BINARY_DIR}" _same)
    If(_same)
       MESSAGE(FATAL_ERROR "Your build and installation directory is the same one. This option does not work. Please change either your build or your installation directory and rerun cmake.")
    EndIf(_same)
-
 EndMacro (CHECK_INSTALL_DIRECTORY)
+################################################################################
 
-Macro(CHECK_EXTERNAL_PACKAGE_INSTALL_DIR)
+MACRO(CHECK_EXTERNAL_PACKAGE_INSTALL_DIR)
   If(IS_DIRECTORY ${SIMPATH}/bin)
     Set(FAIRSOFT_EXTERN TRUE)
   Else(IS_DIRECTORY ${SIMPATH}/bin)
     Set(FAIRSOFT_EXTERN FALSE)
   EndIf(IS_DIRECTORY ${SIMPATH}/bin)
 EndMacro(CHECK_EXTERNAL_PACKAGE_INSTALL_DIR)
-
-
 MACRO(UNIQUE var_name list)
-
-  #######################################################################
-  # Make the given list have only one instance of each unique element and
-  # store it in var_name.
-  #######################################################################
+################################################################################
+# Make the given list have only one instance of each unique element and
+# store it in var_name.
+################################################################################
 
   SET(unique_tmp "")
   FOREACH(l ${list})
@@ -189,12 +173,10 @@ MACRO(UNIQUE var_name list)
   ENDFOREACH(l)
   SET(${var_name} ${unique_tmp})
 ENDMACRO(UNIQUE)
-
-
-###################################################
+################################################################################
 # Creates a variable which stores the intersection
 # between two lists
-####################################################
+################################################################################
 
 MACRO(INTERSECTION var_name list1 list2)
   # Store the intersection between the two given lists in var_name.
@@ -206,7 +188,7 @@ MACRO(INTERSECTION var_name list1 list2)
   ENDFOREACH(l)
   SET(${var_name} ${intersect_tmp})
 ENDMACRO(INTERSECTION)
-
+################################################################################
 MACRO(REMOVE_FROM_LIST var_name list1 list2)
   # Remove elements in list2 from list1 and store the result in var_name.
   SET(filter_tmp "")
@@ -217,7 +199,7 @@ MACRO(REMOVE_FROM_LIST var_name list1 list2)
   ENDFOREACH(l)
   SET(${var_name} ${filter_tmp})
 ENDMACRO(REMOVE_FROM_LIST)
-
+################################################################################
 MACRO (GENERATE_TEST_SCRIPT SCRIPT_FULL_NAME)
 
   get_filename_component(path_name ${SCRIPT_FULL_NAME} PATH)
@@ -232,10 +214,6 @@ MACRO (GENERATE_TEST_SCRIPT SCRIPT_FULL_NAME)
            ${PROJECT_BINARY_DIR} new_path ${path_name}
           )
   EndIf()
-
-#  Message("${new_path}/${shell_script_name}")
-#  file(MAKE_DIRECTORY ${new_path}/data)
-
   CONVERT_LIST_TO_STRING(${LD_LIBRARY_PATH})
   set(MY_LD_LIBRARY_PATH ${output})
   set(my_script_name ${SCRIPT_FULL_NAME})
@@ -249,15 +227,11 @@ MACRO (GENERATE_TEST_SCRIPT SCRIPT_FULL_NAME)
                    ${new_path}/${shell_script_name}
                   )
   ENDIF(FAIRROOT_FOUND)
-
-
-
   EXEC_PROGRAM(/bin/chmod ARGS "u+x  ${new_path}/${shell_script_name}")
-
 ENDMACRO (GENERATE_TEST_SCRIPT)
+################################################################################
 
 Macro(Generate_Exe_Script _Path _ExeName)
-
 
   Message("PATH: ${_Path}")
   Message("ExeName: ${_ExeName}")
@@ -277,6 +251,7 @@ Macro(Generate_Exe_Script _Path _ExeName)
   EXEC_PROGRAM(/bin/chmod ARGS "u+x  ${new_path}/${shell_script_name}")
 
 EndMacro(Generate_Exe_Script)
+################################################################################
 
 Macro (Generate_Version_Info)
 IF(FAIRROOT_FOUND)
@@ -292,7 +267,6 @@ IF(FAIRROOT_FOUND)
                       )
 ELSE(FAIRROOT_FOUND)
   Add_Custom_Target(svnheader ALL)
-
   Add_Custom_Command(TARGET svnheader
                      COMMAND ${CMAKE_COMMAND} -DSOURCE_DIR=${CMAKE_SOURCE_DIR}
 		     -DBINARY_DIR=${CMAKE_BINARY_DIR}
@@ -302,9 +276,9 @@ ELSE(FAIRROOT_FOUND)
 ENDIF(FAIRROOT_FOUND)
 
 EndMacro (Generate_Version_Info)
+################################################################################
 
 Macro (SetBasicVariables)
-
 IF(FAIRROOT_FOUND)
   Set(BASE_INCLUDE_DIRECTORIES
     ${FAIRROOT_INCLUDE_DIR}
@@ -355,22 +329,13 @@ Set(BASE_LINK_DIRECTORIES
 )
 
 IF(FAIRROOT_FOUND)
-
   Set(FAIRLIBDIR ${FAIRROOT_LIBRARY_DIR})
-
 ELSE(FAIRROOT_FOUND)
-
   Set(FAIRLIBDIR ${CMAKE_BINARY_DIR}/lib)
-
 ENDIF(FAIRROOT_FOUND)
-
 Set(LD_LIBRARY_PATH  ${FAIRLIBDIR} ${LD_LIBRARY_PATH})
-
-
 EndMacro (SetBasicVariables)
-
-
-
+################################################################################
 macro(find_package2 qualifier pkgname)
   cmake_parse_arguments(ARGS "" "VERSION" "COMPONENTS" ${ARGN})
 
@@ -405,8 +370,7 @@ macro(find_package2 qualifier pkgname)
   endif()
 endmacro()
 
-
-
+################################################################################
 function(pad str width char out)
   cmake_parse_arguments(ARGS "" "COLOR" "" ${ARGN})
   string(LENGTH ${str} length)
@@ -422,3 +386,4 @@ function(pad str width char out)
   endif()
   set(${out} ${str} PARENT_SCOPE)
 endfunction()
+################################################################################
