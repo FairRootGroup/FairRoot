2401443f8ec6767d4d80bee6f31218c21cab2837
Dmytro Kresan <d.kresan@gsi.de>
2019-08-07 11:25:12 +0200

    Move implementation of FS to the Generic Stack.

--- examples/common/mcstack/FairStack.cxx
+++ examples/common/mcstack/FairStack.cxx
@@ -43,11 +43,6 @@ FairStack::FairStack(Int_t size)
     fStoreIter(),
     fIndexMap(),
     fIndexIter(),
-    fFSTrackMap(),
-    fFSTrackIter(),
-    fFSMovedIndex(-2),
-    fFSFirstSecondary(-2),
-    fFSNofSecondaries(0),
     fPointsMap(),
     fCurrentTrack(-1),
     fNPrimaries(0),
@@ -529,82 +524,5 @@ void FairStack::SelectTracks()
 
 }
 // -------------------------------------------------------------------------
-
-// -------------------------------------------------------------------------
-void FairStack::FastSimMoveParticleTo(Double_t xx, Double_t yy, Double_t zz, Double_t tt,
-                                      Double_t px, Double_t py, Double_t pz, Double_t en)
-{
-    TLorentzVector curPos;
-    TVirtualMC::GetMC()->TrackPosition(curPos);
-    LOG(debug) << "track is in " << curPos.X() << "," << curPos.Y() << "," << curPos.Z() << ", moving to " << xx << "," << yy << "," << zz;
-    TString curVolName = TString(TVirtualMC::GetMC()->CurrentVolName());
-    LOG(debug) << "gMC says track is in \"" << curVolName << "\" moving particle to \"" << gGeoManager->FindNode(xx,yy,zz)->GetVolume()->GetName() << "\".";
-    if ( curVolName.EqualTo(gGeoManager->FindNode(xx,yy,zz)->GetVolume()->GetName()) ) {
-        LOG(fatal) << "FairStack::FastSimMoveParticleTo(" << xx << "," << yy << "," << zz << ": "
-                   << curVolName << " = " << gGeoManager->FindNode(xx,yy,zz)->GetVolume()->GetName() << ") crashes the simulation.";
-    }
-
-    Int_t tobedone  = 1;
-    Int_t parent    = 0; // do not store it as mother
-    Int_t pdg       = TVirtualMC::GetMC()->TrackPid();
-    Double_t polx   = 0.;
-    Double_t poly   = 0.;
-    Double_t polz   = 0.;
-    TMCProcess proc = kPPrimary;// not important, the track will not be saved
-    Int_t ntr       = 0;    // Track number; to be filled by the stack
-    Int_t status    = 0; // not important, the track will not be saved
-    Double_t weight = 0.;
-
-    PushTrack(tobedone, parent, pdg,
-              px, py, pz, en,
-              xx, yy, zz, tt,
-              polx, poly, polz,
-              proc, ntr, weight, status);
-    fFSMovedIndex = fNParticles-1;
-    Int_t trackID = TVirtualMC::GetMC()->GetStack()->GetCurrentTrackNumber();
-    fFSTrackIter = fFSTrackMap.find(trackID);      // check if this track is not already created by FastSimulation
-    if ( fFSTrackIter != fFSTrackMap.end() )       // indeed the track has been created by the FastSimulation mechanism
-        trackID = fFSTrackIter->second;            // use the ID of the original track
-    fFSTrackMap[ntr] = trackID;
-
-    LOG(debug) << "FairStack::FastSimMoveParticleTo() created track number " << ntr << " to replace track number " << trackID;
-
-    if ( strcmp(TVirtualMC::GetMC()->GetName(),"TGeant3TGeo") == 0 ) {
-        TVirtualMC::GetMC()->StopTrack();
-    }
-}
-// -------------------------------------------------------------------------
-
-// -------------------------------------------------------------------------
-void FairStack::FastSimStopParticle()
-{
-    if ( TVirtualMC::GetMC()->IsTrackStop() )
-        LOG(fatal) << "FairStack::FastSimStopParticle() tries to stop particle that was probably moved!";
-    fFSMovedIndex = -1;
-    TVirtualMC::GetMC()->StopTrack();
-}
-// -------------------------------------------------------------------------
-
-// -------------------------------------------------------------------------
-void FairStack::FastSimPushSecondary(Int_t parentID, Int_t pdgCode,
-                                     Double_t xx, Double_t yy, Double_t zz, Double_t tt,
-                                     Double_t px, Double_t py, Double_t pz, Double_t en,
-                                     Double_t polx, Double_t poly, Double_t polz, TMCProcess proc,
-                                     Double_t weight, Int_t is)
-{
-    Int_t tobedone  = 1;
-
-    Int_t ntr       = 0;    // Track number; to be filled by the stack
-    PushTrack(tobedone, parentID, pdgCode,
-              px, py, pz, en,
-              xx, yy, zz, tt,
-              polx, poly, polz,
-              proc, ntr, weight, is);
-    if ( fFSNofSecondaries == 0 )
-        fFSFirstSecondary = fNParticles-1;
-    fFSNofSecondaries++;
-}
-// -------------------------------------------------------------------------
-
 
 ClassImp(FairStack)
