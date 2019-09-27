a28ead4944bc358a3fbdde5196c593137492ca8e
Radoslaw Karabowicz <r.karabowicz@gsi.de>
2019-09-27 14:57:42 +0200

    G4MT
    FairMCApplication - fix the event numbering when running G4MT in MQ.
    Take the event number from the TVirtualMC in this case.
    
    FairOnlineSink, FairMQSimDevice - added example of how to
    process data added using the RegisterAny mechanism.

--- examples/MQ/pixelDetector/src/FairOnlineSink.h
+++ examples/MQ/pixelDetector/src/FairOnlineSink.h
@@ -62,11 +62,40 @@ class FairOnlineSink : public FairSink
 
     virtual FairSink*   CloneSink();
 
- private:
+    bool                IsPersistentBranchAny(const char *name);
+
+    /* /// Returns a default object for a branch or looks it up when it exists already. */
+    /* /// Returns nullptr when the branch does not exist or looking up with wrong type. */
+    /* /// The returned default object will be filled with data by the framework. */
+    template<typename T>
+        T GetPersistentBranchAny(const char* name) const;
+
+
+ private: 
     FairMQRunDevice* fMQRunDevice;
+
+    // private helper function to emit a warning
+    void EmitPersistentBranchWrongTypeWarning(const char* brname, const char *typen1, const char *typen2) const;
 
     FairOnlineSink(const FairOnlineSink&);
     FairOnlineSink& operator=(const FairOnlineSink&);
 };
+
+// try to retrieve an object address from the registered branches/names
+template<typename T>
+T FairOnlineSink::GetPersistentBranchAny(const char* brname) const {
+  static_assert(std::is_pointer<T>::value, "Return type of GetPersistentBranchAny has to be a pointer");
+  using P = typename std::remove_pointer<T>::type;
+  auto iter = fPersistentBranchesMap.find(brname);
+  if(iter != fPersistentBranchesMap.end()) {
+    // verify type consistency
+    if(typeid(P).hash_code() != iter->second->origtypeinfo.hash_code()) {
+      EmitPersistentBranchWrongTypeWarning(brname, typeid(P).name(), iter->second->origtypeinfo.name());
+      return nullptr;
+    }
+    return static_cast<T>(iter->second->ptraddr);
+  }
+  return nullptr;
+}
 
 #endif /* FAIRONLINESIK_H_ */
