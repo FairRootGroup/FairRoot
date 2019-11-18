74a849074ba70b7a9f58e05e632ff1714dfecf0d
Radoslaw Karabowicz <r.karabowicz@gsi.de>
2019-11-18 09:16:36 +0100

    Propagator example
    The files has not been properly moved before...

---
+++ examples/advanced/propagator/src/FairTutPropContFact.h
@@ -0,0 +1,31 @@
+/********************************************************************************
+ *    Copyright (C) 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+#ifndef FAIRTUTPROPCONTFACT_H_
+#define FAIRTUTPROPCONTFACT_H_
+
+#include "FairContFact.h" // for FairContFact, etc
+
+#include "Rtypes.h" // for ClassDef
+
+class FairParSet;
+
+class FairTutPropContFact : public FairContFact
+{
+  private:
+    void setAllContainers();
+
+  public:
+    FairTutPropContFact();
+    ~FairTutPropContFact()
+    {
+    }
+    FairParSet* createContainer(FairContainer*);
+    ClassDef(FairTutPropContFact, 0) // Factory for all FairTutProp parameter containers
+};
+
+#endif /* FAIRTUTPROPCONTFACT_H_ */
