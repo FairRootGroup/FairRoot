9c3ac8fb82ca4ee9ecb994fc3d33b731cb7d257f
Radoslaw Karabowicz <r.karabowicz@gsi.de>
2019-11-18 09:37:05 +0100

    Propagator example
    TutPropLinkDef.h not moved properly.

---
+++ examples/advanced/propagator/src/TutPropLinkDef.h
@@ -0,0 +1,21 @@
+/********************************************************************************
+ *    Copyright (C) 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             * 
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *  
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class FairTutPropContFact+;
+#pragma link C++ class FairTutPropDet+;
+#pragma link C++ class FairTutPropGeo+;
+#pragma link C++ class FairTutPropGeoPar+;
+#pragma link C++ class FairTutPropPoint+;
+#pragma link C++ class FairTutPropTr+;
+
+#endif
