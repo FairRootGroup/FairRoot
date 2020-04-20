fd60922cd90def543bac8650706a2bc17646824e
Alexey Rybalchenko <alexryba@gmail.com>
2020-04-20 22:08:32 +0200

    Add format check script

---
+++ check-format.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASE_COMMIT=${FAIRROOT_FORMAT_BASE:-HEAD}
+GIT_CLANG_FORMAT_BIN=${FAIRROOT_GIT_CLANG_FORMAT_BIN:-git-clang-format}
+
+FILES=$(git diff --name-only $BASE_COMMIT | grep -E '*\.(h|hpp|c|C|cpp|cxx|tpl)$' | grep -viE '*LinkDef.h$')
+RESULT=$($GIT_CLANG_FORMAT_BIN --commit $BASE_COMMIT --diff $FILES --extensions h,hpp,c,C,cpp,cxx,tpl)
+
+if [ "$RESULT" == "no modified files to format" ] || [ "$RESULT" == "clang-format did not modify any files" ]; then
+    echo "format check passed."
+    exit 0
+else
+    echo "ERROR: format check failed. Suggested changes:"
+    echo "$RESULT"
+    exit 1
+fi
