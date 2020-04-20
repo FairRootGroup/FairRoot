8ee5e3d60e04b38feb1da91a1ac4e84314c0d52a
Alexey Rybalchenko <alexryba@gmail.com>
2020-04-20 16:05:28 +0200

    Add a script to apply clang-format to all src files

---
+++ apply-format.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CLANG_FORMAT_BIN=${FAIRROOT_CLANG_FORMAT_BIN:-clang-format}
+
+FILES=$(find . -type f \( -iname "*.h" ! -iname "*.pb.h" ! -iname "*LinkDef.h" -o -iname "*.hpp" -o -iname "*.cxx" -o -iname "*.cpp" -o -iname "*.tpl" -o -iname "*.c" -o -iname "*.C" \) -not \( -path "./build/*" -o -path "./cmake/*" -prune \))
+
+$CLANG_FORMAT_BIN -i $FILES --verbose
