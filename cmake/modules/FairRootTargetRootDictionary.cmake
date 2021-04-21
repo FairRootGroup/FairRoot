b7f1dcd4dde8357a70c2c80d900a9cfd86e9ad62
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-04-21 16:07:31 +0200

    cmake: Drop GCC_TOOLCHAIN_ROOT variable
    If you really think, you need your toolchains library paths
    in LD_LIBRARY_PATH, then add them to LD_LIBRARY_PATH, in
    the first place.

--- cmake/modules/FairRootTargetRootDictionary.cmake
+++ cmake/modules/FairRootTargetRootDictionary.cmake
@@ -117,11 +117,6 @@ function(fairroot_target_root_dictionary target)
   # Need at least root core library
   # get_filename_component(LD_LIBRARY_PATH ROOT::Core DIRECTORY)
   set(LD_LIBRARY_PATH ${ROOT_LIBRARY_DIR})
-  # and possibly toolchain libs if we are using a toolchain
-  if(DEFINED ENV{GCC_TOOLCHAIN_ROOT})
-    set(LD_LIBRARY_PATH "${LD_LIBRARY_PATH}:$ENV{GCC_TOOLCHAIN_ROOT}/lib")
-    set(LD_LIBRARY_PATH "${LD_LIBRARY_PATH}:$ENV{GCC_TOOLCHAIN_ROOT}/lib64")
-  endif()
 
   set(includeDirs $<TARGET_PROPERTY:${target},INCLUDE_DIRECTORIES>)
 
