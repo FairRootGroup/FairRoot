a334c1b5c92508a228992db84a8ac6df58e4dd9d
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-04-26 20:52:44 +0200

    cmake: Introduce fairroot_library_settings()
    This is an internal function only.  It encapsulates things
    that already happened and one new.
    
    It is meant for all those libraries that get installed and
    are expected to be used by other projects.
    It is explicitly not meant for the examples. Because
    they're not meant to be consumed by other projects.
    
    Already happened:
    * Add the FairRoot::${target} alias
    * Set PROJECT_LIBRARY_PROPERTIES properties
    
    New: fairroot_compile_features()
    
    This sets cxx_std_* on the target.  This isn't very
    granular, but hopefully fits the current development model
    better.
    The value being used depends on CMAKE_CXX_STANDARD or
    PROJECT_MIN_CXX_STANDARD.
    So when exported targets come, consumers will at least use
    the CMAKE_CXX_STANDARD.

---
+++ cmake/private/FairRootTargets.cmake
@@ -0,0 +1,38 @@
+################################################################################
+# Copyright (C) 2021 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH       #
+#                                                                              #
+#              This software is distributed under the terms of the             #
+#              GNU Lesser General Public Licence (LGPL) version 3,             #
+#                  copied verbatim in the file "LICENSE"                       #
+################################################################################
+
+
+# Set the C++ language level on exported targets
+#
+# This is only meaningful on targets that later on are
+# exported.
+# It sets the C++ language level to CMAKE_CXX_STANDARD /
+# PROJECT_MIN_CXX_STANDARD, so that projects that import the
+# target use at least that language level to compile
+# things.
+function(fairroot_compile_features target)
+  if (DEFINED CMAKE_CXX_STANDARD)
+    set(standard ${CMAKE_CXX_STANDARD})
+  elseif(DEFINED PROJECT_MIN_CXX_STANDARD)
+    set(standard ${PROJECT_MIN_CXX_STANDARD})
+  else()
+    set(standard 11)
+  endif()
+  target_compile_features("${target}" INTERFACE cxx_std_${standard})
+endfunction()
+
+
+# Set some generic / general settings on exported targets
+# - Export C++ language level
+# - Set namespaced ALIAS to allow consistent target use
+# - Set some generic PROPERTIES (VERSION, SOVERSION)
+function(fairroot_library_settings target)
+  fairroot_compile_features("${target}")
+  add_library(FairRoot::${target} ALIAS ${target})
+  set_target_properties(${target} PROPERTIES ${PROJECT_LIBRARY_PROPERTIES})
+endfunction()
