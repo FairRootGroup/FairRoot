3759dc18b4b972a663caa554eff2550e4899179c
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-05-06 19:05:34 +0200

    Start Extremely Minimal Config Mode Package
    In the long run, we want to move to a clean config mode
    package for FairRoot. So let's go small steps in that
    direction.
    
    This adds the smallest skeleton.

---
+++ cmake/private/FairRootPackage.cmake
@@ -0,0 +1,32 @@
+################################################################################
+# Copyright (C) 2021 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH       #
+#                                                                              #
+#              This software is distributed under the terms of the             #
+#              GNU Lesser General Public Licence (LGPL) version 3,             #
+#                  copied verbatim in the file "LICENSE"                       #
+################################################################################
+
+include(CMakePackageConfigHelpers)
+
+# Configure/Install CMake package
+function(install_cmake_package)
+  # Correct spelling, in local scope
+  set(PROJECT_NAME FairRoot)
+
+  set(PACKAGE_INSTALL_DESTINATION
+    ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}-${PROJECT_VERSION})
+  configure_package_config_file(
+    "cmake/private/${PROJECT_NAME}Config.cmake.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+    INSTALL_DESTINATION "${PACKAGE_INSTALL_DESTINATION}"
+    PATH_VARS CMAKE_INSTALL_PREFIX)
+  write_basic_package_version_file(
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+    VERSION "${PROJECT_VERSION}"
+    COMPATIBILITY AnyNewerVersion)
+  install(
+    FILES
+      "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+      "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+    DESTINATION "${PACKAGE_INSTALL_DESTINATION}")
+endfunction()
