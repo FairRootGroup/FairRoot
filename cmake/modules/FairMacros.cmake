1b46cd9c683e360d4869f98a17cb448225791b5d
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-05-14 14:04:51 +0200

    Use FairFormattedOutput
    FairCMakeModules now ships with FairFormattedOutput.
    
    Use it for color codes, and its `fair_pad()` instead of the
    previous `pad()`.
    
    Both "old" things have been dropped from the public cmake
    interface of FairRoot.

--- cmake/modules/FairMacros.cmake
+++ cmake/modules/FairMacros.cmake
@@ -45,26 +45,6 @@ string(CONCAT Geant4Data_Variables_csh ${Geant4Data_Variables_csh} "\
 ################################################################################
 ENDMACRO()
 
-#Defines some variables with console color escape sequences
-  if(NOT WIN32 AND NOT DISABLE_COLOR)
-    string(ASCII 27 Esc)
-    set(CR       "${Esc}[m")
-    set(CB       "${Esc}[1m")
-    set(Red      "${Esc}[31m")
-    set(Green    "${Esc}[32m")
-    set(Yellow   "${Esc}[33m")
-    set(Blue     "${Esc}[34m")
-    set(Magenta  "${Esc}[35m")
-    set(Cyan     "${Esc}[36m")
-    set(White    "${Esc}[37m")
-    set(BRed     "${Esc}[1;31m")
-    set(BGreen   "${Esc}[1;32m")
-    set(BYellow  "${Esc}[1;33m")
-    set(BBlue    "${Esc}[1;34m")
-    set(BMagenta "${Esc}[1;35m")
-    set(BCyan    "${Esc}[1;36m")
-    set(BWhite   "${Esc}[1;37m")
-  endif()
 
 ################################################################################
 MACRO(SUBDIRLIST result curdir)
@@ -362,26 +342,6 @@ endif()
 
 EndMacro (SetBasicVariables)
 
-################################################################################
-function(pad str width char out)
-  cmake_parse_arguments(ARGS "LEFT" "COLOR" "" ${ARGN})
-  string(LENGTH ${str} length)
-  if(ARGS_COLOR)
-    math(EXPR padding "${width}-(${length}-10*${ARGS_COLOR})")
-  else()
-    math(EXPR padding "${width}-${length}")
-  endif()
-  if(padding GREATER 0)
-    foreach(i RANGE ${padding})
-      if(ARGS_LEFT)
-        set(str "${char}${str}")
-      else()
-        set(str "${str}${char}")
-      endif()
-    endforeach()
-  endif()
-  set(${out} ${str} PARENT_SCOPE)
-endfunction()
 ################################################################################
 function(generate_package_components)
   join("${PROJECT_PACKAGE_COMPONENTS}" " " COMPS)
