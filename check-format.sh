db596eb7b6389a7fa16f85a08144580434b959f7
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2021-06-24 15:50:15 +0200

    fix check-format.sh: Handle only Files that Exist
    When a commit removes a .cpp or .h file, than
    check-format.sh wants to include it in the list of files to
    be checked and git-clang-format can't handle that.
    
    So filter the file list.

--- check-format.sh
+++ check-format.sh
@@ -4,6 +4,8 @@ BASE_COMMIT=${FAIRROOT_FORMAT_BASE:-HEAD}
 GIT_CLANG_FORMAT_BIN=${FAIRROOT_GIT_CLANG_FORMAT_BIN:-git-clang-format}
 
 FILES=$(git diff --name-only $BASE_COMMIT | grep -E '*\.(h|hpp|c|C|cpp|cxx|tpl)$' | grep -viE '*LinkDef.h$')
+# Include only files that exist
+FILES=$(ls -1 $FILES 2>/dev/null)
 RESULT=$($GIT_CLANG_FORMAT_BIN --commit $BASE_COMMIT --diff $FILES --extensions h,hpp,c,C,cpp,cxx,tpl)
 
 if [ "$RESULT" == "no modified files to format" ] || [ "$RESULT" == "clang-format did not modify any files" ]; then
