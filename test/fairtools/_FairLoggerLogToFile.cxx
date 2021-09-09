45be021dc7875f99ffd328f366dc0cf88603808d
Alexey Rybalchenko <alexryba@gmail.com>
2021-09-09 12:54:38 +0200

    Cleanup FairLogger tests

--- test/fairtools/_FairLoggerLogToFile.cxx
+++
@@ -1,7 +0,0 @@
-#include "_TestFairLogger.h"
-
-TEST(FairToolsTestLogToFile, TestLogFileNotOpen)
-{
-    FairLogger* fLogger = FairLogger::GetLogger();
-    fLogger->Warning(MESSAGE_ORIGIN, "This is a problem");
-}
--- test/fairtools/_FairLoggerNewDeathTest2.cxx
+++
@@ -1,9 +0,0 @@
-#include "_TestFairLoggerNew.h"
-
-TEST(FairToolsTestFatal, TestFatalError1)
-{
-    gLogger->SetScreenStreamToCerr(true);
-    gLogger->SetLogToFile(true);
-    ASSERT_THROW(LOG(fatal) << "This is a another fatal problem", fair::FatalException);
-    // TODO: check if cored dump is written to file
-}
