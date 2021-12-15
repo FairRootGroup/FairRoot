b01bad45abd057bc4c358d09fe8efff0c759574f
Florian Uhlig <f.uhlig@gsi.de>
2021-12-15 10:19:10 +0100

    Add missing streamer
    The streamer is needed to write/read a map<string, TGeoHMatrix> to/from a ROOT
    file. The structure is the input neded by the FairRoot AlignmemtHandler.

---
+++ alignment/AlignmentLinkDef.h
@@ -0,0 +1,18 @@
+/********************************************************************************
+ *    Copyright (C) 2021 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+/* clang-format off */
+#pragma link C++ class map<string, TGeoHMatrix>+;
+/* clang-format on */
+
+#endif
