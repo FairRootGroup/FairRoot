1468b662dafda611c21968a62f261586db3916dc
Dennis Klein <d.klein@gsi.de>
2022-04-08 15:15:00 +0200

    fix: Add (some) missing copyright headers

--- examples/MQ/pixelSimSplit/run/runMQGen.cxx
+++ examples/MQ/pixelSimSplit/run/runMQGen.cxx
@@ -1,4 +1,11 @@
 #include "runFairMQDevice.h"
+/********************************************************************************
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
 
 // MQRunSim
 #include "FairBoxGenerator.h"
--- examples/MQ/serialization/1-simple/Ex1Sink.h
+++ examples/MQ/serialization/1-simple/Ex1Sink.h
@@ -1,3 +1,11 @@
+/********************************************************************************
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
 #ifndef EX1SINK_H
 #define EX1SINK_H
 
