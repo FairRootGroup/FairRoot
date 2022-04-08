2061bec4312376ed7e7f668d31ccf9c89ff232ba
Dennis Klein <d.klein@gsi.de>
2022-04-08 15:15:00 +0200

    fix: Add (some) missing copyright headers

--- examples/MQ/serialization/2-multipart/Ex2Sampler.h
+++ examples/MQ/serialization/2-multipart/Ex2Sampler.h
@@ -1,3 +1,11 @@
+/********************************************************************************
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
 #ifndef EX2SAMPLER_H
 #define EX2SAMPLER_H
 
