231099f4186b2718865731716bf0812e72c34beb
Dennis Klein <d.klein@gsi.de>
2022-04-08 15:30:29 +0200

    feat(Logger): Do not use `FairMQLogger.h` any more
    Since FairMQ v1.4.0 `FairMQLogger.h` was just a wrapper around
    `fairlogger/Logger.h` because all logging features have been factored out to
    FairLogger already.
    See https://github.com/FairRootGroup/FairMQ/blob/v1.4.0/fairmq/FairMQLogger.h

--- examples/MQ/pixelDetector/src/devices/FairMQPixelFileSinkBin.cxx
+++ examples/MQ/pixelDetector/src/devices/FairMQPixelFileSinkBin.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -18,7 +18,6 @@
 #include "PixelHit.h"
 #include "PixelPayload.h"
 
-#include <FairMQLogger.h>
 #include <TClonesArray.h>
 #include <TFile.h>
 #include <TFolder.h>
@@ -27,6 +26,7 @@
 #include <TObject.h>
 #include <TTree.h>
 #include <TVector3.h>
+#include <fairlogger/Logger.h>
 
 using namespace std;
 
