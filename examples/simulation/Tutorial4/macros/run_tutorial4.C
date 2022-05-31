32b25f3249bb02a2b4a9cf380cfd41690895784a
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2022-05-31 17:10:03 +0200

    Tutorial4: Small Fixes
    * Create output directory
    * Use output directory in eventdisplay

--- examples/simulation/Tutorial4/macros/run_tutorial4.C
+++ examples/simulation/Tutorial4/macros/run_tutorial4.C
@@ -1,13 +1,17 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
+
+#include <TObjString.h>
+#include <TStopwatch.h>
+#include <TSystem.h>
+
 void run_tutorial4(Int_t nEvents = 10, TString mcEngine = "TGeant3", Bool_t doAlign = true, Bool_t isMT = false)
 {
-
     TString dir = getenv("VMCWORKDIR");
 
     TString tut_configdir = dir + "/simulation/Tutorial4/gconfig";
@@ -21,6 +25,7 @@ void run_tutorial4(Int_t nEvents = 10, TString mcEngine = "TGeant3", Bool_t doAl
     Double_t theta = 2.;
 
     TString outDir = "./data/";
+    gSystem->MakeDirectory(outDir);
 
     // Output file name
     TString outFile{""};
