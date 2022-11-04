418beb04e101d99ec1a30991d88d4d08f5736e43
Dennis Klein <d.klein@gsi.de>
2022-11-04 23:07:43 +0100

    base: Transform Boost.serialization support into non-intrusive variant
    https://www.boost.org/doc/libs/1_80_0/libs/serialization/doc/tutorial.html#nonintrusiveversion
    
    The splitting of `serialize` into explicit `load`/`save` is necessary,
    because the data members are not public and we have to use the asymmetric
    Getter/Setter interfaces, see
    https://www.boost.org/doc/libs/1_80_0/libs/serialization/doc/tutorial.html#splitting

--- base/event/FairHit.h
+++ base/event/FairHit.h
@@ -1,5 +1,5 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -12,13 +12,6 @@
 
 #include <Rtypes.h>     // for Double_t, Int_t, Double32_t, etc
 #include <TVector3.h>   // for TVector3
-
-namespace boost {
-namespace serialization {
-class access;
-}
-}   // namespace boost
-#include <boost/serialization/base_object.hpp>
 
 /**
  * Abstract base class for reconstructed hits in the FAIR detectors.
@@ -67,23 +60,7 @@ class FairHit : public FairTimeStamp
     /*** Output to screen */
     virtual void Print(const Option_t*) const { ; }
 
-    template<class Archive>
-    void serialize(Archive& ar, const unsigned int)
-    {
-        ar& boost::serialization::base_object<FairTimeStamp>(*this);
-        ar& fDetectorID;
-        ar& fRefIndex;
-        ar& fX;
-        ar& fY;
-        ar& fZ;
-        ar& fDx;
-        ar& fDy;
-        ar& fDz;
-    }
-
   protected:
-    friend class boost::serialization::access;
-
     Double32_t fDx, fDy, fDz;   ///< Errors of position [cm]
     Int_t fRefIndex;            ///< Index of FairMCPoint for this hit
     Int_t fDetectorID;          ///< Detector unique identifier
@@ -124,4 +101,53 @@ inline void FairHit::SetPosition(const TVector3& pos)
     fZ = pos.Z();
 }
 
-#endif
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/split_free.hpp>
+
+namespace boost::serialization {
+
+template<class Archive>
+void load(Archive& ar, FairHit& hit, const unsigned int)
+{
+    ar& base_object<FairTimeStamp>(hit);
+
+    Int_t detectorID = 0;
+    ar& detectorID;
+    hit.SetDetectorID(detectorID);
+
+    Int_t refIndex = 0;
+    ar& refIndex;
+    hit.SetRefIndex(refIndex);
+
+    Double_t x = 0., y = 0., z = 0.;
+    ar& x;
+    ar& y;
+    ar& z;
+    hit.SetXYZ(x, y, z);
+
+    Double_t dx = 0., dy = 0., dz = 0.;
+    ar& dx;
+    ar& dy;
+    ar& dz;
+    hit.SetDxyz(dx, dy, dz);
+}
+
+template<class Archive>
+void save(Archive& ar, FairHit const& hit, const unsigned int)
+{
+    ar& base_object<FairTimeStamp>(hit);
+    ar& hit.GetDetectorID();
+    ar& hit.GetRefIndex();
+    ar& hit.GetX();
+    ar& hit.GetY();
+    ar& hit.GetZ();
+    ar& hit.GetDx();
+    ar& hit.GetDy();
+    ar& hit.GetDz();
+}
+
+}   // namespace boost::serialization
+
+BOOST_SERIALIZATION_SPLIT_FREE(FairHit)
+
+#endif   // FAIRHIT_H
--- base/event/FairMCPoint.h
+++ base/event/FairMCPoint.h
@@ -1,5 +1,5 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -17,13 +17,6 @@
 
 #include <Rtypes.h>     // for Double_t, Double32_t, Int_t, etc
 #include <TVector3.h>   // for TVector3
-
-namespace boost {
-namespace serialization {
-class access;
-}
-}   // namespace boost
-#include <boost/serialization/base_object.hpp>
 
 class FairMCPoint : public FairMultiLinkedData_Interface
 {
@@ -86,27 +79,7 @@ class FairMCPoint : public FairMultiLinkedData_Interface
     /** Output to screen **/
     virtual void Print(const Option_t* opt = 0) const;
 
-    template<class Archive>
-    void serialize(Archive& ar, const unsigned int)
-    {
-        // ar & boost::serialization::base_object<FairMultiLinkedData>(*this);
-        ar& fTrackID;
-        ar& fEventId;
-        ar& fDetectorID;
-        ar& fX;
-        ar& fY;
-        ar& fZ;
-        ar& fPx;
-        ar& fPy;
-        ar& fPz;
-        ar& fTime;
-        ar& fLength;
-        ar& fELoss;
-    }
-
   protected:
-    friend class boost::serialization::access;
-
     Int_t fTrackID;             ///< Track index
     UInt_t fEventId;            ///< MC Event id
     Double32_t fPx, fPy, fPz;   ///< Momentum components [GeV]
@@ -140,4 +113,73 @@ inline void FairMCPoint::SetPosition(const TVector3& pos)
     fZ = pos.Z();
 }
 
-#endif
+// #include <boost/serialization/base_object.hpp>
+#include <boost/serialization/split_free.hpp>
+
+namespace boost::serialization {
+
+template<class Archive>
+void load(Archive& ar, FairMCPoint& mcPoint, const unsigned int)
+{
+    // ar & boost::serialization::base_object<FairMultiLinkedData>(*this);
+
+    Int_t trackID = 0;
+    ar& trackID;
+    mcPoint.SetTrackID(trackID);
+
+    Int_t eventID = 0;
+    ar& eventID;
+    mcPoint.SetEventID(eventID);
+
+    Int_t detectorID = 0;
+    ar& detectorID;
+    mcPoint.SetDetectorID(detectorID);
+
+    Double_t x = 0., y = 0., z = 0.;
+    ar& x;
+    ar& y;
+    ar& z;
+    mcPoint.SetXYZ(x, y, z);
+
+    Double_t px = 0., py = 0., pz = 0.;
+    ar& px;
+    ar& py;
+    ar& pz;
+    mcPoint.SetMomentum(TVector3(px, py, pz));
+
+    Double_t time = 0.;
+    ar& time;
+    mcPoint.SetTime(time);
+
+    Double_t length = 0.;
+    ar& length;
+    mcPoint.SetLength(length);
+
+    Double_t energyLoss = 0.;
+    ar& energyLoss;
+    mcPoint.SetLength(energyLoss);
+}
+
+template<class Archive>
+void save(Archive& ar, FairMCPoint const& mcPoint, const unsigned int)
+{
+    // ar & boost::serialization::base_object<FairMultiLinkedData>(*this);
+    ar& mcPoint.GetTrackID();
+    ar& mcPoint.GetEventID();
+    ar& mcPoint.GetDetectorID();
+    ar& mcPoint.GetX();
+    ar& mcPoint.GetY();
+    ar& mcPoint.GetZ();
+    ar& mcPoint.GetPx();
+    ar& mcPoint.GetPy();
+    ar& mcPoint.GetPz();
+    ar& mcPoint.GetTime();
+    ar& mcPoint.GetLength();
+    ar& mcPoint.GetEnergyLoss();
+}
+
+}   // namespace boost::serialization
+
+BOOST_SERIALIZATION_SPLIT_FREE(FairMCPoint)
+
+#endif   // FAIRMCPOINT_H
