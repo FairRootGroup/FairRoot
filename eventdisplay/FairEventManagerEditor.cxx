7dd7d61a595b399579cc049b4b06586a94c60240
Dennis Klein <d.klein@gsi.de>
2022-11-13 13:35:30 +0100

    fix(eventdisplay): -Wunused-variable
    fix #1258

--- eventdisplay/FairEventManagerEditor.cxx
+++ eventdisplay/FairEventManagerEditor.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -128,7 +128,6 @@ void FairEventManagerEditor::Init()
 
     //=============== graphics =============================
     TGVerticalFrame* scene_conf = CreateEditorTabSubFrame("Graphics");
-    TGHorizontalFrame* transparency_frame = new TGHorizontalFrame(scene_conf);
 
     std::unique_ptr<FairEveTransparencyControl> transparency(
         new FairEveTransparencyControl(scene_conf, "Global transparency"));
