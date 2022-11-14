f2717b74de003c52594a871de1c46e0ad996a682
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2022-11-14 20:19:41 +0100

    Rename FairRoot::Gen to FairRoot::Generators
    (cherry picked from commit 59fe7ca39cc6720f79d97d6879f6d0ea2096fb59)

--- generators/LinkDef.h
+++ generators/LinkDef.h
@@ -1,17 +1,16 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
- *              This software is distributed under the terms of the             * 
- *              GNU Lesser General Public Licence (LGPL) version 3,             *  
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
-// $Id: GenLinkDef.h,v 1.2 2006/09/15 16:53:34 friese Exp $
-
 #ifdef __CINT__
 
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
+// clang-format off
 
 #pragma link C++ class  FairAsciiGenerator+;
 #pragma link C++ class  FairIonGenerator+;
@@ -23,4 +22,5 @@
 #pragma link C++ class  FairYPtGenerator+;
 #pragma link C++ class  FairBaseMCGenerator+;
 
+// clang-format on
 #endif
