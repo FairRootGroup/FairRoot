20902c6cf941655c7a98841f3b9515280b0f02d3
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2023-01-11 21:13:09 +0100

    *ContFact: Drop setAllContainers
    The constructor of nearly all "Container Factories" calls a
    private member function called setAllContainers. And it's
    the only call. And that member function is called from
    nowhere else.
    
    So move the contents into the constructor.
    
    (cherry picked from commit 1f3511bc00c9075ace4b3deed1e58056eeffa3da)

--- examples/MQ/pixelDetector/src/PixelContFact.cxx
+++ examples/MQ/pixelDetector/src/PixelContFact.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -25,11 +25,7 @@ PixelContFact::PixelContFact()
     : FairContFact("PixelContFact", "Factory for parameter containers in libPixel")
 {
     /** Constructor (called when the library is loaded) */
-    setAllContainers();
-}
 
-void PixelContFact::setAllContainers()
-{
     /** Creates the Container objects with all accepted
       contexts and adds them to
       the list of containers for the Pixel library.
