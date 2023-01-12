3aae2bc2cf2b4d527896e2c0ab4eba6bdaf5686e
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2023-01-12 13:12:46 +0100

    Add FairRun::GetEvtHeaderRunId and use it
    Classes derived from FairRun often call GetRunId() on
    fEvtHeader. So let's have a shortcut.
    
    (cherry picked from commit f7091f7d86e8737f28913830713606da488a51cb)

--- base/steer/FairRun.cxx
+++ base/steer/FairRun.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -12,7 +12,6 @@
 
 #include "FairRun.h"
 
-#include "FairEventHeader.h"    // for FairEventHeader
 #include "FairFileHeader.h"     // for FairFileHeader
 #include "FairLinkManager.h"    // for FairLinkManager
 #include "FairLogger.h"         // for FairLogger, MESSAGE_ORIGIN
