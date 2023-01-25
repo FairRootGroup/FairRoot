74f520d203274d0a0fbbac025b896bdcdafd318c
Radoslaw Karabowicz <r.karabowicz@gsi.de>
2023-01-25 12:08:02 +0100

    ex_sim_tutorial1: Add test for Geant4 MT simulation
    Implemented simulation output verification in the run_tutorial1.C macro.

--- examples/simulation/Tutorial1/macros/run_tutorial1.C
+++ examples/simulation/Tutorial1/macros/run_tutorial1.C
@@ -1,60 +1,59 @@
 /********************************************************************************
- * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
 
+#include <TChain.h>
+#include <TRandom3.h>
 #include <TStopwatch.h>
 #include <TString.h>
 #include <TSystem.h>
+#include <array>
+#include <iostream>
 #include <memory>
+#include <string>
 #include <utility>
 
-void run_tutorial1(Int_t nEvents = 10,
-                   TString mcEngine = "TGeant3",
-                   Bool_t isMT = true,
-                   Bool_t loadPostInitConfig = false)
+using std::cout;
+using std::endl;
+using std::string;
+
+int run_tutorial1(int nEvents = 10, string mcEngine = "TGeant3", bool isMT = true, bool loadPostInitConfig = false)
 {
-    TString dir = getenv("VMCWORKDIR");
-    TString tutdir = dir + "/simulation/Tutorial1";
+    string dir = getenv("VMCWORKDIR");
+    string tutdir = dir + "/simulation/Tutorial1";
 
-    TString tut_geomdir = dir + "/common/geometry";
-    gSystem->Setenv("GEOMPATH", tut_geomdir.Data());
+    string tut_geomdir = dir + "/common/geometry";
+    gSystem->Setenv("GEOMPATH", tut_geomdir.c_str());
 
-    TString tut_configdir = dir + "/common/gconfig";
-    gSystem->Setenv("CONFIG_DIR", tut_configdir.Data());
+    string tut_configdir = dir + "/common/gconfig";
+    gSystem->Setenv("CONFIG_DIR", tut_configdir.c_str());
 
-    TString partName[] = {"pions", "eplus", "proton"};
-    Int_t partPdgC[] = {211, 11, 2212};
-    Int_t chosenPart = 0;
+    string partName[] = {"pions", "eplus", "proton"};
+    int partPdgC[] = {211, 11, 2212};
+    auto chosenPart = 0;
 
-    Double_t momentum = 2.;
+    auto momentum = 2.;
 
-    Double_t theta = 0.;
+    auto theta = 0.;
 
-    TString outDir = "./";
+    auto nofPart = 1;
+
+    string outDir = "./";
 
     // Output file name
-    TString outFile = Form("%s/tutorial1_%s_%s.mc_p%1.3f_t%1.0f_n%d.root",
-                           outDir.Data(),
-                           mcEngine.Data(),
-                           partName[chosenPart].Data(),
-                           momentum,
-                           theta,
-                           nEvents);
+    string outFile = outDir + "/tutorial1_" + mcEngine + "_" + partName[chosenPart] + ".mc_p" + std::to_string(momentum)
+                     + "_t" + std::to_string(theta) + "_n" + std::to_string(nEvents) + ".root";
 
     // Parameter file name
-    TString parFile = Form("%s/tutorial1_%s_%s.params_p%1.3f_t%1.0f_n%d.root",
-                           outDir.Data(),
-                           mcEngine.Data(),
-                           partName[chosenPart].Data(),
-                           momentum,
-                           theta,
-                           nEvents);
+    string parFile = outDir + "/tutorial1_" + mcEngine + "_" + partName[chosenPart] + ".params_p"
+                     + std::to_string(momentum) + "_t" + std::to_string(theta) + "_n" + std::to_string(nEvents)
+                     + ".root";
 
-    TString geoFile = "geofile_" + mcEngine + "_full.root";
+    auto geoFile = "geofile_" + mcEngine + "_full.root";
 
     // In general, the following parts need not be touched
     // ========================================================================
@@ -70,12 +69,12 @@ void run_tutorial1(Int_t nEvents = 10,
 
     // -----   Create simulation run   ----------------------------------------
     auto run = std::make_unique<FairRunSim>();
-    run->SetName(mcEngine);   // Transport engine
+    run->SetName(mcEngine.c_str());   // Transport engine
     auto config = std::make_unique<FairGenericVMCConfig>();
     if (loadPostInitConfig)
         config->UsePostInitConfig();
     run->SetSimulationConfig(std::move(config));
-    run->SetIsMT(isMT);                            // Multi-threading mode (Geant4 only)
+    run->SetIsMT(isMT);   // Multi-threading mode (Geant4 only)
     run->SetSink(std::make_unique<FairRootFileSink>(outFile));
     FairRuntimeDb* rtdb = run->GetRuntimeDb();
     // ------------------------------------------------------------------------
@@ -97,7 +96,7 @@ void run_tutorial1(Int_t nEvents = 10,
 
     // -----   Create PrimaryGenerator   --------------------------------------
     FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
-    FairBoxGenerator* boxGen = new FairBoxGenerator(partPdgC[chosenPart], 1);
+    FairBoxGenerator* boxGen = new FairBoxGenerator(partPdgC[chosenPart], nofPart);
 
     boxGen->SetThetaRange(theta, theta + 0.01);
     boxGen->SetPRange(momentum, momentum + 0.01);
@@ -121,7 +120,7 @@ void run_tutorial1(Int_t nEvents = 10,
 
     Bool_t kParameterMerged = kTRUE;
     FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
-    parOut->open(parFile.Data());
+    parOut->open(parFile.c_str());
     rtdb->setOutput(parOut);
     rtdb->saveOutput();
     rtdb->print();
@@ -129,7 +128,7 @@ void run_tutorial1(Int_t nEvents = 10,
 
     // -----   Start run   ----------------------------------------------------
     run->Run(nEvents);
-    run->CreateGeometryFile(geoFile);
+    run->CreateGeometryFile(geoFile.c_str());
     // ------------------------------------------------------------------------
 
     // -----   Finish   -------------------------------------------------------
@@ -160,4 +159,45 @@ void run_tutorial1(Int_t nEvents = 10,
     cout << "Macro finished successfully." << endl;
 
     // ------------------------------------------------------------------------
+
+    TChain chain("cbmsim");
+    if (run->WasMT()) {
+        cout << endl << "MT mode, reading two files: t1 and t2" << endl;
+        chain.Add((outFile.replace(outFile.find(".root"), 5, "_t1.root")).c_str());
+        chain.Add((outFile.replace(outFile.find("_t1.root"), 8, "_t2.root")).c_str());
+    } else {
+        cout << endl << "Read from " << outFile << endl;
+        chain.Add(outFile.c_str());
+    }
+
+    auto nofE = chain.GetEntries();
+    auto nofT = chain.Draw("MCTrack.fPx", "", "goff");
+    auto nofP = chain.Draw("TutorialDetPoint.fX", "", "goff");
+    auto nofB = (chain.GetListOfBranches())->GetEntries();
+    auto nofL = (chain.GetListOfLeaves())->GetEntries();
+    cout << "Output chain has " << nofE << " events, " << nofT << " tracks, " << nofP << " points in " << nofB
+         << " branches, " << nofL << " leaves." << endl;
+    if (nofE < nEvents) {
+        std::cerr << "Not enough events (" << nofE << " < " << nEvents << ") in the output chain." << endl;
+        return 1;
+    }
+    if (nofT < nEvents * nofPart) {
+        std::cerr << "Not enough tracks (" << nofT << " < " << nEvents * nofPart << ") in the output chain." << endl;
+        return 1;
+    }
+    if (nofP < nEvents * nofPart) {
+        std::cerr << "Not enough points (" << nofP << " < " << nEvents * nofPart << ") in the output chain." << endl;
+        return 1;
+    }
+    if (nofB != 3) {
+        std::cerr << "Wrong number of branches (" << nofB << " instead of 3) in the output chain." << endl;
+        return 1;
+    }
+    if (nofL != 46) {
+        std::cerr << "Wrong number of leaves (" << nofL << " instead of 46) in the output chain." << endl;
+        return 1;
+    }
+    cout << "Simulation successful." << endl;
+
+    return 0;
 }
