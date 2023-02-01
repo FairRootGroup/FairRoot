81d8f3c66edf91dfea7fe6844c372d03f6b4aa28
Alexey Rybalchenko <alexryba@gmail.com>
2023-02-01 14:23:43 +0100

    Update copyright header

--- examples/MQ/serialization/ExHeader.h
+++ examples/MQ/serialization/ExHeader.h
@@ -1,3 +1,11 @@
+/********************************************************************************
+ * Copyright (C) 2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH       *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
 #ifndef EXHEADER_H
 #define EXHEADER_H
 
