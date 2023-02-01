28d87c02e714307a77ca2d0723364aa0553ab606
Alexey Rybalchenko <alexryba@gmail.com>
2023-02-01 14:23:43 +0100

    Update copyright header

--- examples/MQ/serialization/SerializationExampleLinkDef.h
+++ examples/MQ/serialization/SerializationExampleLinkDef.h
@@ -1,12 +1,10 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH       *
  *                                                                              *
- *              This software is distributed under the terms of the             * 
- *              GNU Lesser General Public Licence (LGPL) version 3,             *  
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
-
-/////////////////////////////// Tutorial 7 LinkDef ///////////////////////////////
 
 #ifdef __CINT__
 
