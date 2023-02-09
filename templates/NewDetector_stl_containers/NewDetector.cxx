9b79f974e11f87aa4fc7298225e349c63ab4ff04
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2023-02-09 18:52:57 +0100

    chore(templates): Drop ClassImp
    ROOT 6.28 deprecates `ClassImp`. [1]
    In August 2018 there already was a "recommendation is to
    not use".  [2]
    
    Drop it from templates first, so that we do not promote old
    style.
    
    [1]: https://root.cern/doc/v628/Rtypes_8h.html#a3f9bf160bbbb44209e95556960836b96
    [2]: https://root-forum.cern.ch/t/dev-general-what-is-the-purpose-of-declfilename/30283/3

--- templates/NewDetector_stl_containers/NewDetector.cxx
+++ templates/NewDetector_stl_containers/NewDetector.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -248,5 +248,3 @@ Bool_t NewDetector::IsSensitive(const std::string& name)
     }
     return kFALSE;
 }
-
-ClassImp(NewDetector);
--- templates/project_root_containers/passive/MyMagnet.cxx
+++ templates/project_root_containers/passive/MyMagnet.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- *    Copyright (C) 2014 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH    *
+ * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -91,6 +91,7 @@ void MyMagnet::ConstructGeometry()
     top->AddNode(magnet1, 2, m6);
 }
 
-FairModule *MyMagnet::CloneModule() const { return new MyMagnet(*this); }
-
-ClassImp(MyMagnet);
+FairModule* MyMagnet::CloneModule() const
+{
+    return new MyMagnet(*this);
+}
