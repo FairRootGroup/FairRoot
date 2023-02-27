cf069fa72f7375e335212cc9a05b77db2731c4eb
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2023-02-27 12:36:45 +0100

    FairRuntimeDb: Move global into .cxx
    Move `contFactories` from the .h to the .cxx. It's only
    used there.

--- fairroot/parbase/FairRuntimeDb.h
+++ fairroot/parbase/FairRuntimeDb.h
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -18,8 +18,6 @@ class FairLogger;
 class FairParIo;
 class FairParSet;
 class FairRtdbRun;
-
-static TList contFactories;   //! list of container factories
 
 /**
  * \ingroup parbase
