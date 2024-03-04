8755ae931dd3cc4e165af32b02fb3004efd71986
Dennis Klein <d.klein@gsi.de>
2024-03-04 15:42:13 +0100

    test(Base): Add basic test coverage for `FairVolumeList`

---
+++ tests/base/sim/test_FairVolumeList.cxx
@@ -0,0 +1,47 @@
+/********************************************************************************
+ *   Copyright (C) 2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH     *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *              GNU Lesser General Public Licence (LGPL) version 3,             *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
+#include <FairVolume.h>
+#include <FairVolumeList.h>
+#include <TString.h>
+#include <catch2/catch_test_macros.hpp>
+#include <memory>
+#include <string_view>
+
+static auto MakeVolume(std::string_view name)
+{
+    return std::make_unique<FairVolume>(TString{name});
+}
+
+TEST_CASE("FairVolumeList")
+{
+    FairVolumeList volumes;
+
+    SECTION("is a set")
+    {
+        auto a = MakeVolume("a");
+        auto* a_ptr = a.get();
+        auto a_name = TString(a->GetName());
+
+        volumes.addVolume(a.release());
+        volumes.addVolume(MakeVolume("b").release());
+        volumes.addVolume(MakeVolume("a").release());
+
+        REQUIRE(volumes.getEntries() == 2);
+        REQUIRE(volumes.getVolume(&a_name) == a_ptr);
+    }
+
+    SECTION("handles reading a non-existent volume")
+    {
+        TString z = "z";
+        constexpr auto notFound = -111;
+
+        REQUIRE(volumes.getVolume(&z) == nullptr);
+        REQUIRE(volumes.getVolumeId(&z) == notFound);
+    }
+}
