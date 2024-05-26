3c3f455db67298d720cb19a8c3d8cd4b29b204ac
Dennis Klein <d.klein@gsi.de>
2024-05-26 23:57:14 +0200

    fix(templates): Use naming logic from base class
    introduced in 0fb8a6f42be762ee1ea6f0aa9cdfe3e62f8be100

--- templates/NewDetector_stl_containers/NewDetectorGeo.cxx
+++ templates/NewDetector_stl_containers/NewDetectorGeo.cxx
@@ -5,31 +5,5 @@
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
  *                  copied verbatim in the file "LICENSE"                       *
  ********************************************************************************/
+
 #include "NewDetectorGeo.h"
-
-#include "FairGeoNode.h"
-
-// -----   Default constructor   -------------------------------------------
-NewDetectorGeo::NewDetectorGeo()
-    : FairGeoSet("newdetector", 10)
-{}
-
-// -------------------------------------------------------------------------
-
-const char* NewDetectorGeo::getModuleName(Int_t m)
-{
-    /** Returns the module name of NewDetector number m
-      Setting NewDetector here means that all modules names in the
-      ASCII file should start with NewDetector otherwise they will
-      not be constructed
-  */
-    sprintf(modName, "NewDetector%i", m + 1);
-    return modName;
-}
-
-const char* NewDetectorGeo::getEleName(Int_t m)
-{
-    /** Returns the element name of Det number m */
-    sprintf(eleName, "NewDetector%i", m + 1);
-    return eleName;
-}
--- templates/NewDetector_stl_containers/NewDetectorGeo.h
+++ templates/NewDetector_stl_containers/NewDetectorGeo.h
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -12,26 +12,13 @@
 
 class NewDetectorGeo : public FairGeoSet
 {
-  protected:
-    char modName[20]{"NewDetector"};   // name of module
-    char eleName[20]{"NewDetector"};   // substring for elements in module
   public:
-    NewDetectorGeo();
+    NewDetectorGeo()
+        : FairGeoSet("NewDetector", 10)
+    {}
     ~NewDetectorGeo() override = default;
-    const char* getModuleName(Int_t) override;
-    const char* getEleName(Int_t) override;
-    inline Int_t getModNumInMod(const TString&) override;
+
     ClassDefOverride(NewDetectorGeo, 1);
 };
-
-inline Int_t NewDetectorGeo::getModNumInMod(const TString& name)
-{
-    /** returns the module index from module name
-   ?? in name[??] has to be the length of the detector name in the
-   .geo file. For example if all nodes in this file starts with
-   newdetector ?? has to be 11.
-  */
-    return (Int_t)(name[11] - '0') - 1;   //
-}
 
 #endif
