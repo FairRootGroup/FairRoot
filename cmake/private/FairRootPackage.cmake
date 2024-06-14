b892c2eb0304ba468282c64f2f47d0c95c74d0df
Dennis Klein <d.klein@gsi.de>
2024-06-14 11:43:04 +0200

    build: Export `FairRoot_CMAKE_MODULES_DIR`
    Co-authored-by: @ChristianTackeGSI
    (cherry picked from commit 60baa2dec401c5b2c66f4b585c3d3b60c474bce2)

--- cmake/private/FairRootPackage.cmake
+++ cmake/private/FairRootPackage.cmake
@@ -1,5 +1,5 @@
 ################################################################################
-# Copyright (C) 2021 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH       #
+# Copyright (C) 2021-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  #
 #                                                                              #
 #              This software is distributed under the terms of the             #
 #              GNU Lesser General Public Licence (LGPL) version 3,             #
@@ -25,7 +25,8 @@ function(install_cmake_package)
     PATH_VARS
       CMAKE_INSTALL_PREFIX
       CMAKE_INSTALL_FULL_BINDIR
-      PACKAGE_INSTALL_DESTINATION)
+      PACKAGE_INSTALL_DESTINATION
+      PROJECT_CMAKE_MODULES_DIR)
   write_basic_package_version_file(
     "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
     VERSION "${PROJECT_VERSION}"
