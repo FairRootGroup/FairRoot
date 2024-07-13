52836e5878a1c9e5e0ba9aec37111bfcba7379be
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2024-07-13 22:04:18 +0200

    refactor: Remove unneeded FairRootManager references

--- examples/advanced/Tutorial3/MQ/processorTask/MQRecoTask.h
+++ examples/advanced/Tutorial3/MQ/processorTask/MQRecoTask.h
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -15,9 +15,6 @@
 #include "FairTestDetectorRecoTask.h"
 #include "Payload.h"
 #include "ProcessorTask.h"
-
-// FairRoot
-#include "FairRootManager.h"
 
 // ROOT
 #include <TClonesArray.h>
--- fairroot/base/event/FairEventBuilderManager.cxx
+++ fairroot/base/event/FairEventBuilderManager.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -132,12 +132,6 @@ void FairEventBuilderManager::AddEventBuilder(FairEventBuilder* eventBuilder)
 
 InitStatus FairEventBuilderManager::Init()
 {
-    // Get input array
-    FairRootManager* ioman = FairRootManager::Instance();
-    if (!ioman) {
-        Fatal("Init", "No FairRootManager");
-    }
-
     cout << "*** FairEventBuilderManager. " << fEventBuilders.size() << " event builders registered." << endl;
 
     for (UInt_t ieb = 0; ieb < fEventBuilders.size(); ieb++) {
