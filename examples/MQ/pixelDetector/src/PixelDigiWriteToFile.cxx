3bde303291dec47787033f564dcffd03257488ab
Christian Tacke <58549698+ChristianTackeGSI@users.noreply.github.com>
2024-07-13 22:03:59 +0200

    refactor(examples): Drop empty Reset methods

--- examples/MQ/pixelDetector/src/PixelDigiWriteToFile.cxx
+++ examples/MQ/pixelDetector/src/PixelDigiWriteToFile.cxx
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2014-2023 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -38,17 +38,12 @@ PixelDigiWriteToFile::PixelDigiWriteToFile(const char* name, Int_t iVerbose)
     , fRunId(0)
     , fMCEntryNo(0)
     , fPartNo(0)
+{}
 
-{
-    Reset();
-}
-
-PixelDigiWriteToFile::~PixelDigiWriteToFile() { Reset(); }
+PixelDigiWriteToFile::~PixelDigiWriteToFile() = default;
 
 void PixelDigiWriteToFile::Exec(Option_t* /*opt*/)
 {
-    Reset();
-
     Int_t nofDigis = fDigis->GetEntriesFast();
 
     for (Int_t ifile = 0; ifile < fNofOutputFiles; ifile++) {
--- examples/MQ/pixelDetector/src/PixelDigiWriteToFile.h
+++ examples/MQ/pixelDetector/src/PixelDigiWriteToFile.h
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (C) 2014-2022 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
+ * Copyright (C) 2014-2024 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH  *
  *                                                                              *
  *              This software is distributed under the terms of the             *
  *              GNU Lesser General Public Licence (LGPL) version 3,             *
@@ -63,9 +63,6 @@ class PixelDigiWriteToFile : public FairTask
 
     /** Reinitialisation **/
     InitStatus ReInit() override;
-
-    /** Reset eventwise counters **/
-    void Reset() {}
 
     /** Finish at the end of each event **/
     void Finish() override;
